dcf0664ab91753c4055643a2b386330f77d57654
Martin Kuba <mkuba@newrelic.com>
2021-05-06 09:38:14 -0700

    Fix for interaction waiting on setTimeout without callback (#31)
    Fix for SPA interactions waiting for timer that has no callback

---
+++ tests/functional/spa/timers.test.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+const testDriver = require('../../../tools/jil')
+const querypack = require('@newrelic/nr-querypack')
+
+const supported = testDriver.Matcher.withFeature('addEventListener')
+
+testDriver.test('incorrect timer', supported, function (t, browser, router) {
+  let rumPromise = router.expectRum()
+  let eventsPromise = router.expectEvents()
+  let loadPromise = browser.safeGet(router.assetURL('spa/incorrect-timer.html', { loader: 'spa' }))
+
+  Promise.all([eventsPromise, rumPromise, loadPromise])
+    .then(([eventsResult]) => {
+      let eventPromise = router.expectEvents()
+      let domPromise = browser.elementByCssSelector('body').click()
+      return Promise.all([eventPromise, domPromise]).then(([eventData, domData]) => {
+        return eventData
+      })
+    })
+    .then(({query, body}) => {
+      let interactionTree = querypack.decode(body && body.length ? body : query.e)[0]
+      t.equal(interactionTree.category, 'Route change', 'gout route change harvest call')
+      t.end()
+    })
+    .catch(fail)
+
+  function fail(err) {
+    t.error(err)
+    t.end()
+  }
+})

7a6fc9902279f3109de047fa0784c84065bcbc39
Jordan <insomniacrampage@hotmail.com>
2021-05-11 15:15:35 -0700

    fix whitespace issue on eslint-ignored file

--- post-installer.js
+++ post-installer.js
@@ -11,33 +11,33 @@ print('Installed Root Package')
 npm_install_recursive(root)
 
 function print(msg){
-    console.log(`\n================================\n${msg}\n================================\n`)
+  console.log(`\n================================\n${msg}\n================================\n`)
 }
 
 // Recurses into a folder
 function npm_install_recursive(folder){
-    const has_package_json = fs.existsSync(path.join(folder, 'package.json'))
-    // If there is `package.json` in this folder then perform `npm install`.
-    if (has_package_json && folder !== root){
-        npm_install(folder)
-    }
-    // Recurse into subfolders
-    for (let subfolder of subfolders(folder)){
-        npm_install_recursive(subfolder)
-    }
+  const has_package_json = fs.existsSync(path.join(folder, 'package.json'))
+  // If there is `package.json` in this folder then perform `npm install`.
+  if (has_package_json && folder !== root){
+    npm_install(folder)
+  }
+  // Recurse into subfolders
+  for (let subfolder of subfolders(folder)){
+    npm_install_recursive(subfolder)
+  }
 }
 
 // Performs `npm install`
 function npm_install(folder) {
-    const cmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-    print(`Installing ./${path.relative(root, folder)}`)
-    child_process.execSync(`${cmd} ci`, { cwd: folder, env: process.env, stdio: 'inherit' })
+  const cmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+  print(`Installing ./${path.relative(root, folder)}`)
+  child_process.execSync(`${cmd} ci`, { cwd: folder, env: process.env, stdio: 'inherit' })
 }
 
 // Lists subfolders in a folder
 function subfolders(folder) {
-    return fs.readdirSync(folder)
-        .filter(subfolder => fs.statSync(path.join(folder, subfolder)).isDirectory())
-        .filter(subfolder => subfolder !== 'node_modules' && subfolder[0] !== '.')
-        .map(subfolder => path.join(folder, subfolder))
+  return fs.readdirSync(folder)
+    .filter(subfolder => fs.statSync(path.join(folder, subfolder)).isDirectory())
+    .filter(subfolder => subfolder !== 'node_modules' && subfolder[0] !== '.')
+    .map(subfolder => path.join(folder, subfolder))
 }

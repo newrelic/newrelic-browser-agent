f04e2b93d138bf9dd0e35edf19e1c70a8fb1572a
Martin Kuba <mkuba@newrelic.com>
2021-05-20 16:10:50 -0700

    updated config test that checks for default URL value to include version number (#54)

--- tests/functional/config.test.js
+++ tests/functional/config.test.js
@@ -12,11 +12,20 @@ testDriver.test('empty string for agent key', function (t, browser, router) {
     agent: ''
   }
 
+  let expectedUrl = 'js-agent.newrelic.com/nr.min.js'
+  if (process.env.VERSION) {
+    expectedUrl = `js-agent.newrelic.com/nr-${process.env.VERSION}.min.js`
+  }
+
   browser
     .get(router.assetURL('instrumented.html', {config}))
     .safeEval('NREUM.info.agent', function (err, agentUrl) {
       if (err) t.fail(err)
-      t.equal(agentUrl, 'js-agent.newrelic.com/nr.min.js', 'agent url should use default value')
+      if (process.env.VERSION) {
+        t.equal(agentUrl, expectedUrl, 'agent url should have default value with the correct version')
+      } else {
+        t.equal(agentUrl, expectedUrl, 'agent url should have the default value')
+      }
     })
     .catch(fail)
 

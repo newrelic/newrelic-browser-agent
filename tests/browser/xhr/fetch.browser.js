28acde4f32b9950a70f60b5f6c7c379c29ef35b7
Martin Kuba <mkuba@newrelic.com>
2021-05-28 16:55:47 -0700

    more tests for fetch metrics

--- tests/browser/xhr/fetch.browser.js
+++ tests/browser/xhr/fetch.browser.js
@@ -4,9 +4,8 @@
  */
 
 const test = require('../../../tools/jil/browser-test')
-const register = require('../../../agent/register-handler')
-const drain = require('../../../agent/drain')
 const ee = require('ee')
+var handleEE = ee.get('handle')
 
 require('../../../feature/xhr/instrument')
 require('../../../feature/err/instrument')
@@ -18,36 +17,76 @@ let assetServerSSLPort = NREUM.info.assetServerSSLPort
 let assetServerPort = proto === 'http:' ? assetServerHTTPPort : assetServerSSLPort
 let assetServerHostname = window.location.host.split(':')[0]
 
-test('basic fetch call', function(t) {
-  if (!window.fetch) {
-    t.pass('fetch is not supported in this browser')
-    t.end()
-    return
+var testCases = [
+  {
+    name: 'basic fetch call',
+    invoke: function() {
+      window.fetch('/json')
+    },
+    check: function(t, params, metrics, start) {
+      t.equals(params.method, 'GET', 'method')
+      t.equals(params.status, 200, 'status')
+      t.equals(params.host, assetServerHostname + ':' + assetServerPort, 'host')
+      t.equals(params.pathname, '/json', 'pathname')
+      t.equals(metrics.txSize, 0, 'request size')
+      t.equals(metrics.rxSize, 14, 'response size')
+      t.ok(metrics.duration > 1, 'duration is a positive number')
+      t.ok(start > 0, 'start is a positive number')
+    }
+  }, {
+    name: 'fetch with Request parameter',
+    invoke: function() {
+      var request = new Request('/json')
+      window.fetch(request)
+    },
+    check: function(t, params, metrics, start) {
+      t.equals(params.method, 'GET', 'method')
+      t.equals(params.status, 200, 'status')
+      t.equals(params.host, assetServerHostname + ':' + assetServerPort, 'host')
+      t.equals(params.pathname, '/json', 'pathname')
+      t.equals(metrics.txSize, 0, 'request size')
+      t.equals(metrics.rxSize, 14, 'response size')
+      t.ok(metrics.duration > 1, 'duration is a positive number')
+      t.ok(start > 0, 'start is a positive number')
+    }
+  }, {
+    name: 'fetch with URL parameter',
+    invoke: function() {
+      var request = new URL('http://' + assetServerHostname + ':' + assetServerPort + '/json')
+      window.fetch(request)
+    },
+    check: function(t, params, metrics, start) {
+      t.equals(params.method, 'GET', 'method')
+      t.equals(params.status, 200, 'status')
+      t.equals(params.host, assetServerHostname + ':' + assetServerPort, 'host')
+      t.equals(params.pathname, '/json', 'pathname')
+      t.equals(metrics.txSize, 0, 'request size')
+      t.equals(metrics.rxSize, 14, 'response size')
+      t.ok(metrics.duration > 1, 'duration is a positive number')
+      t.ok(start > 0, 'start is a positive number')
+    }
   }
+]
 
-  ee.emit('feat-err', [])
+ee.emit('feat-err', [])
+if (!window.NREUM) NREUM = {}
+if (!NREUM.loader_config) NREUM.loader_config = {}
 
-  if (!window.NREUM) NREUM = {}
-  if (!NREUM.loader_config) NREUM.loader_config = {}
+for (let testCase of testCases) {
+  test(testCase.name, function(t) {
+    if (!window.fetch) {
+      t.pass('fetch is not supported in this browser')
+      t.end()
+      return
+    }
 
-  window.fetch('/json')
+    handleEE.addEventListener('xhr', validate)
+    testCase.invoke()
 
-  register('xhr', function(params, metrics, start) {
-    require('../../../feature/xhr/aggregate')(params, metrics, start)
-
-    t.equals(params.method, 'GET', 'method')
-    t.equals(params.status, 200, 'status')
-    t.equals(params.host, assetServerHostname + ':' + assetServerPort, 'host')
-    t.equals(params.pathname, '/json', 'pathname')
-
-    t.equals(metrics.txSize, 0, 'request size')
-    t.equals(metrics.rxSize, 14, 'response size')
-    t.ok(metrics.duration > 1, 'duration is a positive number')
-
-    t.ok(start > 0, 'start is a positive number')
-
-    t.end()
+    function validate(params, metrics, start) {
+      testCase.check(t, params, metrics, start)
+      t.end()
+      handleEE.removeEventListener('xhr', validate)
+    }
   })
-
-  drain('feature')
-})
+}

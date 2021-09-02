890ca644a1e56dfe8867f59a2cf58e71bf17f291
Martin Kuba <mkuba@newrelic.com>
2021-09-01 17:50:46 -0700

    simplified test to only check for one resource from second harvest

--- tests/functional/stn/resource-harvest.test.js
+++ tests/functional/stn/resource-harvest.test.js
@@ -26,54 +26,33 @@ testDriver.test('session trace resources', supported, function (t, browser, rout
   let loadPromise = browser.safeGet(assetURL)
   let rumPromise = router.expectRum()
   let resourcePromise = router.expectResources()
-  let firstBody
 
   Promise.all([resourcePromise, loadPromise, rumPromise]).then(([result]) => {
     t.equal(result.res.statusCode, 200, 'server responded with 200')
 
-    // first harvest is complete and should be a resource entry in second harvest
-    firstBody = result.body
-
     // trigger an XHR call after
-    let domPromise = browser
+    var clickPromise = browser
       .elementById('trigger')
       .click()
 
-    return domPromise
-  }).then(result => {
-    // capture resourceBuffer after /json call and before second harvest request is made
-    return browser.eval("window.performance.getEntriesByType('resource')")
-  }).then(evalResult => {
     resourcePromise = router.expectResources()
-    return Promise.all([evalResult, resourcePromise])
-  }).then(result => {
-    const resourceBuffer = result[0]
-    const secondBody = result[1].body
+
+    return Promise.all([resourcePromise, clickPromise])
+  }).then(([result]) => {
+    const body = result.body
 
     t.equal(router.seenRequests.resources, 2, 'got two harvest requests')
-    t.equal(result[1].res.statusCode, 200, 'server responded with 200')
+    t.equal(result.res.statusCode, 200, 'server responded with 200')
 
-    const firstParsed = JSON.parse(firstBody).res
-    const secondParsed = JSON.parse(secondBody).res
-    const harvestBody = firstParsed.concat(secondParsed)
+    const parsed = JSON.parse(body).res
+    const harvestBody = parsed
     const resources = harvestBody.filter(function (node) { return node.t === 'resource' })
 
-    t.ok(resources.length >= 1, 'there is at least one resource node')
-    t.equal(resources.length, resourceBuffer.length, 'STN captured same number of resources as ResourceTiming API')
+    t.ok(resources.length > 1, 'there is at least one resource node')
 
-    const identical = resources.every(resource => {
-      const match = resourceBuffer.find(bufResource => {
-        const startTime = bufResource.fetchStart | 0
-        const endTime = bufResource.responseEnd | 0
-        return startTime === resource.s && endTime === resource.e
-      })
-
-      t.ok(match, 'found a match for ' + resource.n + ' resource with source ' + resource.o)
-
-      return match
-    })
-
-    t.ok(identical, 'STN resource nodes match the ResourceTiming API buffer')
+    var url = 'http://' + router.router.assetServer.host + ':' + router.router.assetServer.port + '/json'
+    const found = resources.find(element => element.o === url)
+    t.ok(!!found, 'expected resource was found')
 
     t.end()
   }).catch(fail)

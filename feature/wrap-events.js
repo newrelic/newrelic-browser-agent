31db6754d1ff98e26c2b7f612d17da0527a820c1
Martin Kuba <mkuba@newrelic.com>
2021-11-03 13:44:58 -0700

    Handle addEventListener with null function argument (#139)
    * handle addEventListener with null instead of function argument
    
    * objects are valid too

--- feature/wrap-events.js
+++ feature/wrap-events.js
@@ -25,6 +25,11 @@ if ('getPrototypeOf' in Object) {
 
 ee.on(ADD_EVENT_LISTENER + '-start', function (args, target) {
   var originalListener = args[1]
+  if (originalListener === null ||
+    (typeof originalListener !== 'function' && typeof originalListener !== 'object')
+  ) {
+    return
+  }
 
   var wrapped = getOrSet(originalListener, 'nr@wrapped', function () {
     var listener = {

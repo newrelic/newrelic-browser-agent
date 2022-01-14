1af255adb44b952cab4580a3361a596b2270dbdd
Martin Kuba <mkuba@newrelic.com>
2022-01-14 13:59:08 -0800

    changed artifact name to match what mock-bam-server can server right now

--- packages/agent-loader/webpack.config.js
+++ packages/agent-loader/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'nr-loader-webpack.js',
+    filename: 'nr-loader-spa.js',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {

1f1e90c5e69f73ba57cff275d2f0442d63be4cc6
Jordan <insomniacrampage@hotmail.com>
2022-01-31 13:20:13 -0800

    add try-catch blocks for compat with ie8

--- loader/framework-detection.js
+++ loader/framework-detection.js
@@ -22,32 +22,47 @@ function getFrameworks() {
     if (window.Meteor) frameworks.push(FRAMEWORKS.METEOR)
     if (window.Zepto) frameworks.push(FRAMEWORKS.ZEPTO)
     if (window.jQuery) frameworks.push(FRAMEWORKS.JQUERY)
-    return frameworks
   } catch (err) {
     // not supported?
   }
+  return frameworks
 }
 
 function detectReact() {
-  if (!!window.React || !!window.ReactDOM || !!window.ReactRedux) return true
-  if (document.querySelector('[data-reactroot], [data-reactid]')) return true
-  var divs = document.querySelectorAll('body > div')
-  for (var i = 0; i < divs.length; i++) {
-    if (Object.keys(divs[i]).indexOf('_reactRootContainer') >= 0) return true
+  try {
+    if (!!window.React || !!window.ReactDOM || !!window.ReactRedux) return true
+    if (document.querySelector('[data-reactroot], [data-reactid]')) return true
+    var divs = document.querySelectorAll('body > div')
+    for (var i = 0; i < divs.length; i++) {
+      if (Object.keys(divs[i]).indexOf('_reactRootContainer') >= 0) return true
+    }
+    return false
+  } catch (err) {
+    // not supported?
+    return false
   }
-  return false
 }
 
 function detectAngularJs() {
-  if (window.angular) return true
-  if (document.querySelector('.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]')) return true
-  if (document.querySelector('script[src*="angular.js"], script[src*="angular.min.js"]')) return true
-  return false
+  try {
+    if (window.angular) return true
+    if (document.querySelector('.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]')) return true
+    if (document.querySelector('script[src*="angular.js"], script[src*="angular.min.js"]')) return true
+    return false
+  } catch (err) {
+    // not supported?
+    return false
+  }
 }
 
 function detectAngular() {
-  if (window.hasOwnProperty('ng') && window.ng.hasOwnProperty('coreTokens') && window.ng.coreTokens.hasOwnProperty('NgZone')) return true
-  return !!document.querySelectorAll('[ng-version]').length
+  try {
+    if (window.hasOwnProperty('ng') && window.ng.hasOwnProperty('coreTokens') && window.ng.coreTokens.hasOwnProperty('NgZone')) return true
+    return !!document.querySelectorAll('[ng-version]').length
+  } catch (err) {
+    // not supported?
+    return false
+  }
 }
 
 module.exports = getFrameworks()

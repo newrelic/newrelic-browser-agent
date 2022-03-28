180850dccdef0dbbddf18ee68f7ebdf7fc45c2ae
aubreymasten <aubreymasten@users.noreply.github.com>
2022-03-28 13:27:18 -0700

    cleanup

--- agent/bel-serializer.js
+++ agent/bel-serializer.js
@@ -22,13 +22,6 @@ function nullable (val, fn, comma) {
     : '!'
 }
 
-obj = {
-
-  param: 'helllo'
-}
-
-obj.regex('l', `l'`)
-
 function numeric (n, noDefault) {
   if (noDefault) {
     return Math.floor(n).toString(36)

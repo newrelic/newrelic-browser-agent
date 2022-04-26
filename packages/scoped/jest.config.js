54f09948d6cbb593ead933f0f61977d89f3bc6dd
Jordan <insomniacrampage@hotmail.com>
2022-04-26 13:23:36 -0700

    cleanup jest config

--- packages/scoped/jest.config.js
+++ packages/scoped/jest.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    transform: {
-      '^.+\\.(ts|tsx)?$': 'ts-jest',
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    collectCoverage: true,
-    // collectCoverageFrom: ["./dist/**"],
-    // "coverageThreshold": {
-    //   "global": {
-    //     "lines": 90
-    //   }
-    // }
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  collectCoverage: true
+}

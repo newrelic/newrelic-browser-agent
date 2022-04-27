b844260168992e66c25b59965d9b274425db01cd
Jordan <insomniacrampage@hotmail.com>
2022-04-26 17:02:16 -0700

    fix issue with package building

--- packages/scoped/webpack.config.js
+++ packages/scoped/webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 const webpack = require('webpack')
 const pkg = require('./package.json')
 
@@ -8,24 +8,25 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-   library: {
-     name: 'nrBrowserCore',
-     type: 'umd',
-   },
+    library: {
+      name: 'nrBrowserCore',
+      type: 'umd'
+    }
   },
+
   plugins: [
     new Dotenv({
-      path: path.resolve(__dirname, './.env'),
+      path: path.resolve(__dirname, './.env')
     }),
     new webpack.DefinePlugin({
       'process.env.SUBPATH': JSON.stringify(process.env.SUBPATH || ''),
-      'process.env.VERSION': JSON.stringify(pkg.version || `-${process.env.VERSION}` || ''),
+      'process.env.VERSION': JSON.stringify(pkg.version || ''),
       'process.env.BUILD': JSON.stringify(process.env.BUILD || 'spa'),
-      'process.env.DEBUG': JSON.stringify(process.env.DEBUG || false),
-    }),
+      'process.env.DEBUG': JSON.stringify(process.env.DEBUG || false)
+    })
   ],
   optimization: {
     minimize: false
   },
-  devtool: 'source-map',
-};
+  devtool: 'source-map'
+}

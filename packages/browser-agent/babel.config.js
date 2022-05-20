89948316dcc10907f9704a37ee625e791a61d63e
Jordan <insomniacrampage@hotmail.com>
2022-05-19 21:56:41 -0700

    workarounds

--- packages/browser-agent/babel.config.js
+++ packages/browser-agent/babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    presets: ['@babel/preset-env', "minify"]
+    presets: ['@babel/preset-env'],
+    "env": {
+        "production": {
+            "presets": ["minify"]
+        }
+    },
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods'
+    ],
 }

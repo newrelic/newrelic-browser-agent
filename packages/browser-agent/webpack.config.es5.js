65caa0492d42608671c945fe114219594d4fcaef
Jordan <insomniacrampage@hotmail.com>
2022-05-20 10:07:39 -0700

    build es5 through webpack

---
+++ packages/browser-agent/webpack.config.es5.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+const webpack = require('webpack')
+const pkg = require('./package.json')
+
+module.exports = {
+  entry: './dist/bundled/es6/index.js',
+  target: ['web', 'es5'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/bundled/es5'),
+    filename: 'index.js',
+    library: {
+      name: 'NRBA',
+      type: 'umd'
+    },
+    chunkFormat: 'module',
+  },
+
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env')
+    }),
+    new webpack.DefinePlugin({
+      'process.env.SUBPATH': JSON.stringify(process.env.SUBPATH || ''),
+      'process.env.VERSION': JSON.stringify(pkg.version || ''),
+      'process.env.BUILD': JSON.stringify(process.env.BUILD || 'spa'),
+      'process.env.DEBUG': JSON.stringify(process.env.DEBUG || false)
+    })
+  ],
+  optimization: {
+    minimize: true
+  },
+  devtool: 'source-map',
+  mode: 'production'
+}

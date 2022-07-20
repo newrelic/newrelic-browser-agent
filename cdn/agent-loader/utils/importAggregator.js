7488a7df0cd073fc3b89b4c15bab7cd580cd4d3b
C Li <cli@newrelic.com>
2022-07-20 14:27:49 -0500

    Fixed stageAggregator cb causing faulty racing, propagate errors for aggregators import, and add unit tests for failing imports.

--- cdn/agent-loader/utils/importAggregator.js
+++ cdn/agent-loader/utils/importAggregator.js
@@ -1,17 +1,23 @@
 
-import { onWindowLoad } from '@newrelic/browser-agent-core/common/window/load'
+import {onWindowLoad} from '@newrelic/browser-agent-core/common/window/load'
+import {ee} from '@newrelic/browser-agent-core/common/event-emitter/contextual-ee'
 
 let loadFired = 0
 
 export function stageAggregator(loaderType, useTimeout, ms) {
     if (useTimeout) setTimeout(() => importAggregator(loaderType), ms || 1000) // used just for local testing injecting the script after window load...
-    else onWindowLoad(importAggregator(loaderType)) // inject the aggregator when window load event fires
+    else onWindowLoad(() => importAggregator(loaderType)) // inject the aggregator when window load event fires
 }
 
 function importAggregator(loaderType) {
-    setTimeout(async () => {
+    (async () => {
         if (loadFired++) return
-        const { aggregator } = await import('../../agent-aggregator/aggregator')
-        aggregator(loaderType)
-    }, 0)
+        try {
+            const { aggregator } = await import('../../agent-aggregator/aggregator')
+            await aggregator(loaderType)
+        } catch (err) {
+            console.error("Failed to successfully load all aggregators. Aborting...\n", err);
+            ee.abort(); // halt event emitter primary functions
+        }
+    })();
 }

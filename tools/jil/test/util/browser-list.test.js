8e837f9335f4f443c169096238adf28be1d1df98
C Li <cli@newrelic.com>
2022-07-21 17:48:56 -0500

    Update JIL to accept "latest-X" prev versions in CLI

--- tools/jil/test/util/browser-list.test.js
+++ tools/jil/test/util/browser-list.test.js
@@ -5,8 +5,20 @@
 
 var tape = require('tape')
 var browsers = require('../../util/browsers.json')
-var browserList = require('../../util/browser-list').default
+var browserList = require('../../util/browser-list')
 var BrowserSpec = require('../../util/browser-list').BrowserSpec
+const latestVersStringRe = require('../../util/browser-list').latestVersStringRe;
+
+tape("Regex for latest versions works", function(t) {
+  t.ok(latestVersStringRe.test("latest"), "latest -- should pass");
+  t.notOk(latestVersStringRe.test("latest-0"), "latest-0 -- should not pass");
+  t.ok(latestVersStringRe.test("latest-1"), "latest-1 -- should pass");
+  t.ok(latestVersStringRe.test("latest-9"), "latest-9 -- should pass");
+  t.notOk(latestVersStringRe.test("latest-10"), "latest-10 -- should not pass");
+  t.notOk(latestVersStringRe.test("latest-"), "latest- -- should not pass");
+  t.notOk(latestVersStringRe.test("latest-X"), "latest-X -- should not pass");
+  t.end();
+})
 
 tape('returns BrowserSpec instances', function (t) {
   var browserSpecs = browserList('*@*')
@@ -15,8 +27,8 @@ tape('returns BrowserSpec instances', function (t) {
   t.end()
 })
 
-tape('returns only browsers defined in config file', (t) => {
-  var browserSpecs = browserList('chrome@56')
+tape('returns only browsers defined in config file', (t) => { // "config file" == browsers.json
+  var browserSpecs = browserList('chrome@latest')
   t.ok(browserSpecs.length === 1, 'got defined browser')
 
   browserSpecs = browserList('chrome@55')
@@ -58,23 +70,34 @@ tape('beta returns labeled `beta` if defined', function (t) {
 
 tape('*@beta returns all browser versions labeled `beta`', function (t) {
   var browserSpecs = browserList('*@beta')
-  t.equal(browserSpecs.length, 2)
+  t.equal(browserSpecs.length, 1)
   browserSpecs.forEach(b => {
     t.equal(b.version, 'beta', 'got the right one')
   })
   t.end()
 })
 
-tape('latest returns labeled `latest` if defined', function (t) {
+/**
+ * Note: Part of test is a bit redundant after the 'returns only browsers defined in config file' test above.
+ */
+tape('latest returns labeled `latest` and -1 if defined', function (t) {
   var browserSpecs = browserList('chrome@latest')
   t.equal(browserSpecs.length, 1, 'got one')
-  t.equal(browserSpecs[0].version, 'latest', 'got the right one')
+  t.equal(browserSpecs[0].version, 'latest', 'got the right one (latest)')
+
+  browserSpecs = browserList('chrome@latest-1')
+  t.equal(browserSpecs.length, 1, 'got one')
+  t.equal(browserSpecs[0].version, 'latest-1', 'got the right one (latest-1)')
   t.end()
 })
 
-tape('latest returns biggest version if `latest` label not defined', function (t) {
+tape('latest returns biggest version if `latest` and previous -X labels not defined', function (t) {
   var browserSpecs = browserList('ie@latest')
   t.equal(browserSpecs.length, 1, 'got one')
-  t.equal(browserSpecs[0].version, '11', 'got the right one')
+  t.equal(browserSpecs[0].version, '11', 'got the right one (ie-11)')
+
+  browserSpecs = browserList('chrome@latest-9')
+  t.equal(browserSpecs.length, 1, 'got one')
+  t.equal(browserSpecs[0].version, 'beta', 'got the right one (chrome-beta)')
   t.end()
 })

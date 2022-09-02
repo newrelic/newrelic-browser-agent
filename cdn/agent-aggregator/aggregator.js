4f5385a5aabe2f4095f70720b5dedc8d5f5bbe02
Jordan Porter <insomniacrampage@hotmail.com>
2022-09-02 10:04:27 -0700

    reconfigure config setting to allow nr.info to be placed on page before or after the script block (#274)

--- cdn/agent-aggregator/aggregator.js
+++ cdn/agent-aggregator/aggregator.js
@@ -1,11 +1,19 @@
-import { getRuntime } from '@newrelic/browser-agent-core/common/config/config'
+import { getRuntime, setInfo, setConfiguration, setLoaderConfig } from '@newrelic/browser-agent-core/common/config/config'
 import { importFeatures } from './util/features'
 import { activateFeatures, activatedFeatures } from '@newrelic/browser-agent-core/common/util/feature-flags'
-import { addToNREUM, gosNREUMInitializedAgents } from '@newrelic/browser-agent-core/common/window/nreum'
+import { addToNREUM, gosCDN, gosNREUMInitializedAgents } from '@newrelic/browser-agent-core/common/window/nreum'
 import agentIdentifier from '../shared/agentIdentifier'
 import { initializeAPI } from './util/api'
 
 export function aggregator(build) {
+  // do this again in case they are using a custom build that has 
+  // nr.info below the main agent script in some way
+  const nr = gosCDN()
+  setInfo(agentIdentifier, nr.info)
+  setConfiguration(agentIdentifier, nr.init)
+  setLoaderConfig(agentIdentifier, nr.loader_config)
+  // don't set runtime again no matter what, it is always set in the loader stage.
+
   const autorun = typeof (getRuntime(agentIdentifier).autorun) !== 'undefined' ? getRuntime(agentIdentifier).autorun : true
 
   initializeAPI(agentIdentifier)

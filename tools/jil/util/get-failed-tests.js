4f911d307d3a76006bb41cd169b5c03c84c6f40b
Jordan Porter <silentsummitsoftware@gmail.com>
2022-09-27 11:35:53 -0400

    update polyfill test and post check

--- tools/jil/util/get-failed-tests.js
+++ tools/jil/util/get-failed-tests.js
@@ -7,7 +7,21 @@ if (!BUILD_NUMBER) process.exit(1)
 
 const queryNR = async () => {
     console.log("---- FETCHING --", BUILD_NUMBER, " ----")
-    const body = {query: `{
+    const latest = getGQLResults(await getDataFromNRQL(`{
+        actor {
+            account(id: 1672072) {
+               nrql(query: "FROM JilTest SELECT latest(remaining) where build = '${BUILD_NUMBER}' FACET browserName, browserVersion, platformName, platformVersion LIMIT 50 SINCE 7 DAYS AGO") {
+                  results
+               }
+            }
+         }
+    }`))
+    if (latest.some(x => !!x['latest.remaining'])) {
+        console.log("all tests did not finish....")
+        console.log(latest)
+        process.exit(1)
+    }
+    const failures = await getDataFromNRQL(`{
         actor {
             account(id: 1672072) {
                nrql(query: "FROM JilTest SELECT * where build = '${BUILD_NUMBER}' AND passed is false and retryRun is true and retry = 2 SINCE 7 days ago") {
@@ -15,16 +29,7 @@ const queryNR = async () => {
                }
             }
          }
-    }`}
-    const resp = await fetch("https://api.newrelic.com/graphql", {
-        body: JSON.stringify(body),
-        headers: {
-            "Api-Key": `${NRQL_API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        method: "POST"
-    })
-    const json = await resp.json()
+    }`)
     const failedTests = {
         chrome: {},
         firefox: {},
@@ -34,7 +39,7 @@ const queryNR = async () => {
         ios: {}
 
     }
-    json?.data?.actor?.account?.nrql?.results.forEach(x => {
+    failures?.data?.actor?.account?.nrql?.results.forEach(x => {
         failedTests[x.browserName.toLowerCase()][x.browserVersion] = failedTests[x.browserName.toLowerCase()][x.browserVersion] ? failedTests[x.browserName.toLowerCase()][x.browserVersion] : new Set()
         failedTests[x.browserName.toLowerCase()][x.browserVersion].add(x.testFileName)
     })
@@ -42,7 +47,7 @@ const queryNR = async () => {
     console.log("---- FAILED TESTS ----")
     console.log(failedTests)
     var isValid = true
-    var out = json?.data?.actor?.account?.nrql?.results?.length ? 'node --max-old-space-size=8192 ./tools/jil/bin/cli.js -f merged -s -t 85000 -b ' : ''
+    var out = failures?.data?.actor?.account?.nrql?.results?.length ? 'node --max-old-space-size=8192 ./tools/jil/bin/cli.js -f merged -s -t 85000 -b ' : ''
     Object.entries(failedTests).forEach(([key, val]) => {
         if (!val) return
         isValid = false
@@ -67,3 +72,21 @@ const queryNR = async () => {
 }
 
 queryNR()
+
+async function getDataFromNRQL(nrqlString){
+    const body = {query: nrqlString}
+    const resp = await fetch("https://api.newrelic.com/graphql", {
+        body: JSON.stringify(body),
+        headers: {
+            "Api-Key": `${NRQL_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        method: "POST"
+    })
+    const json = await resp.json()
+    return json
+}
+
+function getGQLResults(gql){
+    return gql?.data?.actor?.account?.nrql?.results
+}

78d58d36efe3c206be1c7021e1b5562e165ed73c
Jordan Porter <silentsummitsoftware@gmail.com>
2022-10-31 11:34:18 -0400

    fetch open PRs from github api and use that to check CDN for PR builds

--- tools/scripts/upload-ab-to-s3.js
+++ tools/scripts/upload-ab-to-s3.js
@@ -1,5 +1,5 @@
 const request = require('request')
-const {connectToS3, uploadToS3} = require('./s3')
+const { connectToS3, uploadToS3 } = require('./s3')
 
 var argv = require('yargs')
   .usage('$0 [options]')
@@ -9,46 +9,66 @@ var argv = require('yargs')
   .describe('env', 'NR Internal Environment')
   .default('env', 'dev')
 
-  .string('config')
-  .describe('NR internal config block')
+  .string('current')
+  .describe('current', 'current/stable build (defaults to -current)')
+  .default('current', 'https://js-agent.newrelic.com/nr-loader-spa-current.min.js')
 
-  .string('files')
-  .describe('comma delimited string of CDN file paths')
+  .string('next')
+  .describe('next', 'next version to compare to stable version (defaults to /dev)')
+  .default('next', 'https://js-agent.newrelic.com/dev/nr-loader-spa.min.js')
+
+  .string('config')
+  .describe('config', 'NR internal config block')
 
   .string('bucket')
   .describe('bucket', 'S3 bucket name')
 
   .string('role')
   .describe('role', 'S3 role ARN')
+
+  .boolean('dry')
+  .default('dry', false)
   .argv
 
 
-const { env, files, config, bucket, role } = argv
+const { env, config, bucket, role, current, next, dry } = argv
 
 let counter = 0
 
-if (!env || !files || !config || !bucket || !role) {
+if (!env || !config || !bucket || !role) {
   console.log("missing required param")
   process.exit(1)
 }
 
-const filePaths = files.split(",")
-Promise.all(filePaths.map(fp => getFile(fp))).then((contents) => {
-  let output = `window.NREUM=${config};`
-  contents.forEach(([url, res, body]) => { output += wrapAgent(body) })
-  output += randomExecutor(contents.length)
+(async function () {
+  const filePaths = [
+    current,
+    next,
+    ...(env === 'dev' && await getOpenPrNums()).map(num => `https://js-agent.newrelic.com/pr/PR${num}`),
+  ]
 
-  const filename = `internal/${env}.js`
+  Promise.all(filePaths.map(fp => getFile(fp))).then((contents) => {
+    contents = contents.filter(([url, res])=> res.statusCode === 200)
+    console.log(`found ${contents.length} valid PR builds in CDN`)
+    let output = `window.NREUM=${config};`
+    contents.forEach(([url, res, body]) => { output += wrapAgent(body) })
+    output += randomExecutor(contents.length)
 
-  connectToS3(role).then(async () => {
-    const uploads = await uploadToS3(filename, output, bucket)
-    console.log(`Successfully uploaded ${filename} to S3`)
-    process.exit(0)
+    const filename = `internal/${env}.js`
+
+    connectToS3(role, dry).then(async () => {
+      const uploads = await uploadToS3(filename, output, bucket, dry)
+      console.log(`Successfully uploaded ${filename} to S3`)
+      process.exit(0)
+    }).catch(err => {
+      console.log(err)
+      process.exit(1)
+    })
   }).catch(err => {
-    console.log(err)
-    process.exit(1)
+    console.log("error getting all files... ", err)
   })
-})
+
+})()
 
 function wrapAgent(agent) {
   const agentNo = counter++
@@ -87,6 +107,27 @@ function getFile(path) {
         return
       }
       resolve([path, res, body])
+    })
+  })
+}
+
+function getOpenPrNums() {
+  var opts = {
+    uri: 'https://api.github.com/repos/newrelic/newrelic-browser-agent/pulls?state=open',
+    method: 'GET',
+    headers: {
+      'User-Agent': 'metal-messiah'
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    request(opts, (err, res, body) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      const prNums = JSON.parse(body).map(pr => pr.number)
+      resolve(prNums)
     })
   })
 }

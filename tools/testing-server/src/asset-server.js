b42b3614cb277c80f5f937d10c385888ce1c8914
bjfield <25965987+bjfield@users.noreply.github.com>
2023-01-06 11:05:12 -0800

    Update babel targets and tests to match support statement (NEWRELIC-5417) (#357)
    * Update babel targets to match support statement (last 10 including mobile).
    * Filter sauce browsers based on supported browsers list.
    * Fetch updated list of browserslist targets (`npx browserslist@latest --update-db`).
    * Round down Safari versions: too few SauceLabs configs available.
    * Evenly sample 4 of each browser, incl. 1st, last.
    * Enable testing for Android 9.0.
    * Enable android functional/unit tests in workflow.

--- tools/testing-server/src/asset-server.js
+++ tools/testing-server/src/asset-server.js
@@ -315,7 +315,9 @@ class BrowserifyTransform extends AssetTransform {
                 "last 10 Chrome versions",
                 "last 10 Safari versions",
                 "last 10 Firefox versions",
-                "last 10 Edge versions"
+                "last 10 Edge versions",
+                "last 10 ChromeAndroid versions",
+                "last 10 iOS versions"
               ]
             }
           }]

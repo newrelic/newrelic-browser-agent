da9ac271ceb8e44943fa5131ab785443b37e2655
Jordan Porter <silentsummitsoftware@gmail.com>
2023-01-12 21:56:28 -0500

    use setToken response flags to kill instantiated features

--- packages/browser-agent-core/src/features/ajax/aggregate/index.js
+++ packages/browser-agent-core/src/features/ajax/aggregate/index.js
@@ -79,6 +79,7 @@ export class Aggregate extends FeatureBase {
 
       handle('bstXhrAgg', ['xhr', hash, params, metrics], undefined, undefined, ee)
 
+      if (this.blocked) return
       // store as metric
       aggregator.store('xhr', hash, params, metrics)
 

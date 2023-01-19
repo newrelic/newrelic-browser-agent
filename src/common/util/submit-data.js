c2c40ee2a0b3d8fb2aa069fe36d10b7924a26de0
Jordan Porter <silentsummitsoftware@gmail.com>
2023-01-19 12:09:25 -0500

    Bind navigator to sendBeacon to prevent illegal invocation errors on some browsers and envs

--- src/common/util/submit-data.js
+++ src/common/util/submit-data.js
@@ -2,7 +2,7 @@
  * Copyright 2020 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-
+import globalScope from "./global-scope"
 export const submitData = {}
 
 /**
@@ -53,5 +53,8 @@ submitData.img = function img (url) {
  * Do NOT call this function outside of a guaranteed web window environment.
  */
 submitData.beacon = function (url, body) {
-  return window.navigator.sendBeacon(url, body)
+  // Navigator has to be bound to ensure it does not error in some browsers
+  // https://xgwang.me/posts/you-may-not-know-beacon/#it-may-throw-error%2C-be-sure-to-catch
+  const send = globalScope.navigator.sendBeacon.bind(navigator)
+  return send(url, body)
 }

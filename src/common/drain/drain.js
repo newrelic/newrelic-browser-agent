1cb4a5c1daa88d106418511ffa910dd31c3afa67
Jordan Porter <silentsummitsoftware@gmail.com>
2023-01-21 11:34:54 -0500

    call drain only after window is loaded, to match previous behavior

--- src/common/drain/drain.js
+++ src/common/drain/drain.js
@@ -6,33 +6,44 @@
 import { ee } from '../event-emitter/contextual-ee'
 import { mapOwn } from '../util/map-own'
 import { registerHandler as defaultRegister } from '../event-emitter/register-handler'
+import { isBrowserScope } from '../util/global-scope'
+import { onWindowLoad } from '../window/load'
 
 
-// calls will need to update to call this more directly so we can explicitly pass in the ee and handler
+/**
+ * Drain buffered events out of the EE. Each feature **should** have its events bucket into a "group".  The feature should drain this group explicitly
+ * @param {string} agentIdentifier 
+ * @param {string} group 
+ */
 export function drain(agentIdentifier, group = 'feature') {
-  const baseEE = agentIdentifier ? ee.get(agentIdentifier) : ee
-  const handlers = defaultRegister.handlers
-  if (!baseEE.backlog || !handlers) return
+  if (isBrowserScope) onWindowLoad(drainGroup)
+  else drainGroup()
 
-  var bufferedEventsInGroup = baseEE.backlog[group]
-  var groupHandlers = handlers[group]
-  if (groupHandlers) {
-    // don't cache length, buffer can grow while processing
-    for (var i = 0; bufferedEventsInGroup && i < bufferedEventsInGroup.length; ++i) { // eslint-disable-line no-unmodified-loop-condition
-      emitEvent(bufferedEventsInGroup[i], groupHandlers)
+  function drainGroup() {
+    const baseEE = agentIdentifier ? ee.get(agentIdentifier) : ee
+    const handlers = defaultRegister.handlers
+    if (!baseEE.backlog || !handlers) return
+
+    var bufferedEventsInGroup = baseEE.backlog[group]
+    var groupHandlers = handlers[group]
+    if (groupHandlers) {
+      // don't cache length, buffer can grow while processing
+      for (var i = 0; bufferedEventsInGroup && i < bufferedEventsInGroup.length; ++i) { // eslint-disable-line no-unmodified-loop-condition
+        emitEvent(bufferedEventsInGroup[i], groupHandlers)
+      }
+
+      mapOwn(groupHandlers, function (eventType, handlerRegistrationList) {
+        mapOwn(handlerRegistrationList, function (i, registration) {
+          // registration is an array of: [targetEE, eventHandler]
+          registration[0].on(eventType, registration[1])
+        })
+      })
     }
 
-    mapOwn(groupHandlers, function (eventType, handlerRegistrationList) {
-      mapOwn(handlerRegistrationList, function (i, registration) {
-        // registration is an array of: [targetEE, eventHandler]
-        registration[0].on(eventType, registration[1])
-      })
-    })
+    delete handlers[group]
+    // Keep the group as a property so we know it was created and drained
+    baseEE.backlog[group] = null
   }
-
-  delete handlers[group]
-  // Keep the group as a property so we know it was created and drained
-  baseEE.backlog[group] = null
 }
 
 function emitEvent(evt, groupHandlers) {

2481c99bd62b69376c1e8abb5c8ba3f4d3e29331
Jordan Porter <silentsummitsoftware@gmail.com>
2023-02-03 11:25:24 -0500

    replace stringify with built-in and circular logic

--- src/cdn/polyfills.js
+++ src/cdn/polyfills.js
@@ -14,3 +14,30 @@ import 'core-js/stable/object/values'
 import 'core-js/stable/map'
 import 'core-js/stable/reflect'
 import 'core-js/stable/set'
+import 'core-js/stable/weak-set'
+
+// promise
+// ie - none
+// safari - 7+
+// ios - 8+
+// chrome - 33+
+// edge - 12+
+// ff - 29+
+
+// not in use currently.... but be aware this feature of promises has much different benchmark support
+// promise.any, promise.allSettled
+// ie - none
+// safari - 14+
+// ios - 14+
+// chrome - 85+
+// edge - 105+
+// ff - 79+
+
+// making note because this feature of promises has different benchmark support
+// promise.any
+// ie - none
+// safari - 8
+// ios - 8+
+// chrome - 32+
+// edge - 12+
+// ff - 29+

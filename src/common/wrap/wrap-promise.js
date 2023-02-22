ed3b0011e823f01ae7b3b9c973858a6bd699beaf
Chunwai Li <cli@newrelic.com>
2023-02-21 18:07:32 -0600

    Modify original promise prototype instead

--- src/common/wrap/wrap-promise.js
+++ src/common/wrap/wrap-promise.js
@@ -5,7 +5,7 @@
 /**
  * This module is used by: spa
  */
-import { createWrapperWithEmitter as wrapFn } from './wrap-function'
+import { createWrapperWithEmitter as wrapFn, flag, unwrapFunction } from './wrap-function'
 import { ee as baseEE, getOrSetContext } from '../event-emitter/contextual-ee'
 import { originals } from '../config/config'
 import { globalScope } from '../util/global-scope'
@@ -36,26 +36,23 @@ export function wrapPromise (sharedEE) {
     })
     WrappedPromise.toString = function () { return prevPromiseObj.toString() }
 
-    // Adds support for instanceof comparison for async functions
-    if (typeof Symbol === 'function' && typeof Symbol.hasInstance === 'symbol') {
-      Object.defineProperty(WrappedPromise, Symbol.hasInstance, {
-        value: function (instance) {
-          return instance instanceof prevPromiseObj
-        }
-      })
-    }
-
+    /**
+     * This constitutes the global used when calling "Promise.staticMethod" or chaining off a "new Promise()" object.
+     * @param {Function} executor - to be executed by the original Promise constructor
+     * @returns A new WrappedPromise object prototyped off the original.
+     */
     function WrappedPromise (executor) {
       var ctx = promiseEE.context()
       var wrappedExecutor = promiseWrapper(executor, 'executor-', ctx, null, false)
 
-      const newCustomPromiseInst = Reflect.construct(prevPromiseObj, [wrappedExecutor], WrappedPromise) // extend the orig Promise constructor as super
+      const newCustomPromiseInst = Reflect.construct(prevPromiseObj, [wrappedExecutor], WrappedPromise) // new Promises will use WrappedPromise.prototype as theirs prototype
 
       promiseEE.context(newCustomPromiseInst).getCtx = function () {
         return ctx
       }
       return newCustomPromiseInst
     }
+
     // Make WrappedPromise inherit statics from the orig Promise.
     Object.setPrototypeOf(WrappedPromise, prevPromiseObj);
 
@@ -79,7 +76,6 @@ export function wrapPromise (sharedEE) {
         }
       }
     });
-
     ['resolve', 'reject'].forEach(function (method) {
       const prevStaticFn = prevPromiseObj[method]
       WrappedPromise[method] = function (val) { // and the same for ".resolve" and ".reject"
@@ -92,23 +88,32 @@ export function wrapPromise (sharedEE) {
       }
     })
 
-    // Mirror the orig Promise's prototype (catch, try, then, etc.) but with our own methods.
-    WrappedPromise.prototype = Object.create(prevPromiseObj.prototype)
-    WrappedPromise.prototype.constructor = WrappedPromise // calls to "new Promise()" should use our custom Promise
-    WrappedPromise.prototype.then = function (...args) {
+    /*
+     * Ideally, we create a new WrappedPromise.prototype chained off the original Promise's so that we don't alter it.
+     * However, there's no way to make the (native) promise returned from async functions use our WrappedPromise,
+     * so we have to modify the original prototype in essence. This ensures that "async()"" promise functions and runs the
+     * same instance methods as "new Promise()", and also that instanceof async() is the global Promise (see GH issue #409).
+     *  In addition, this affects the fetch() promise too.
+     */
+    WrappedPromise.prototype = prevPromiseObj.prototype
+
+    // Note that this wrapping affects the same originals.PR (prototype) object.
+    const prevPromiseOrigThen = prevPromiseObj.prototype.then
+    prevPromiseObj.prototype.then = function wrappedThen (...args) {
       var originalThis = this
       var ctx = getContext(originalThis)
       ctx.promise = originalThis
       args[0] = promiseWrapper(args[0], 'cb-', ctx, null, false)
       args[1] = promiseWrapper(args[1], 'cb-', ctx, null, false)
 
-      const origFnCallWithThis = prevPromiseObj.prototype.then.apply(this, args)
+      const origFnCallWithThis = prevPromiseOrigThen.apply(this, args)
 
       ctx.nextPromise = origFnCallWithThis
       promiseEE.emit('propagate', [originalThis, true], origFnCallWithThis, false, false)
 
       return origFnCallWithThis
     }
+    prevPromiseObj.prototype.then[flag] = prevPromiseOrigThen
 
     promiseEE.on('executor-start', function (args) {
       args[0] = promiseWrapper(args[0], 'resolve-', this, null, false)

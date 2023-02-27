20f4cd0eb1b39f1f086dfd6e018244ad35d33910
Jordan Porter <silentsummitsoftware@gmail.com>
2023-02-27 17:46:33 -0500

    fix tests

--- src/features/metrics/instrument/index.js
+++ src/features/metrics/instrument/index.js
@@ -7,7 +7,7 @@ export class Instrument extends InstrumentBase {
   static featureName = FEATURE_NAME
   constructor (agentIdentifier, aggregator, auto = true) {
     super(agentIdentifier, aggregator, FEATURE_NAME, auto)
-    insertSupportMetrics(tag => handle(SUPPORTABILITY_METRIC_CHANNEL, [tag], undefined, FEATURE_NAMES.metrics, ee))
+    insertSupportMetrics(tag => handle(SUPPORTABILITY_METRIC_CHANNEL, [tag], undefined, FEATURE_NAMES.metrics, this.ee))
     this.importAggregator()
   }
 }

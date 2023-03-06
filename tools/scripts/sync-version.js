6ff073b9b24337769fc9e561727b90fbbeed0a54
Jordan Porter <silentsummitsoftware@gmail.com>
2023-03-06 16:00:06 -0500

    update version requirements locally

--- tools/scripts/sync-version.js
+++ tools/scripts/sync-version.js
@@ -2,7 +2,4 @@ const pkg = require('../../package.json')
 const fs = require('fs')
 const path = require('path')
 
-fetch('https://raw.githubusercontent.com/newrelic/newrelic-browser-agent/main/package.json').then(r => r.json()).then(({ version }) => {
-  if (version === pkg.version) throw new Error("package.json version hasn't been updated.")
-  fs.writeFileSync(path.join(__dirname, '../../', 'VERSION'), pkg.version)
-})
+fs.writeFileSync(path.join(__dirname, '../../', 'VERSION'), pkg.version)

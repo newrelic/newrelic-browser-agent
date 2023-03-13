de0de263b0f4a745198e023bed6d283563a9c4c8
Jordan Porter <silentsummitsoftware@gmail.com>
2023-03-13 17:42:46 -0400

    fix version sync

--- tools/scripts/sync-version.js
+++ tools/scripts/sync-version.js
@@ -2,7 +2,7 @@ const pkg = require('../../package.json')
 const fs = require('fs')
 const path = require('path')
 
-const cdn_version = fs.readFileSync(path.join(__dirname, '../../', 'VERSION'))
-const new_pkg_version = `0.1.${cdn_version}`
+const cdn_version = fs.readFileSync(path.join(__dirname, '../../', 'VERSION'), 'utf-8')
+const new_pkg_version = `0.${cdn_version.split('.').slice(0, 2).join('.')}`
 pkg.version = new_pkg_version.replace(/^\s+|\s+$/g, '')
 fs.writeFileSync(path.join(__dirname, '../../', 'package.json'), JSON.stringify(pkg, null, 2))

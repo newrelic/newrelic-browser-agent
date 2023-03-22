2a04b4e3b69bed014291e529e6d825b358f71215
Jordan Porter <silentsummitsoftware@gmail.com>
2023-03-22 13:28:40 -0400

    update sync-version to match

--- tools/scripts/sync-version.js
+++ tools/scripts/sync-version.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 const path = require('path')
 
 const cdn_version = fs.readFileSync(path.join(__dirname, '../../', 'VERSION'), 'utf-8')
-const new_pkg_version = `0.${cdn_version.split('.').slice(0, 2).join('.')}`
-pkg.version = new_pkg_version.replace(/^\s+|\s+$/g, '')
-fs.writeFileSync(path.join(__dirname, '../../', 'package.json'), JSON.stringify(pkg, null, 2))
+const [major, minor, patch] = cdn_version.split('.')
+const new_pkg_version = `0.${major}.${minor}${patch && patch > 0 ? `-rc.${patch}` : ''}`
+if (pkg.version !== new_pkg_version) {
+  pkg.version = new_pkg_version.replace(/^\s+|\s+$/g, '')
+  fs.writeFileSync(path.join(__dirname, '../../', 'package.json'), JSON.stringify(pkg, null, 2))
+} else console.log('versions match!', pkg.version, '===', new_pkg_version)

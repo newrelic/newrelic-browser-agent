26214e312f8e54dbfd38dcb17cdec776145bb031
Patrick Housley <phousley@newrelic.com>
2023-04-06 15:41:42 -0500

    chore: unit tests for url utilities

--- src/common/url/protocol.js
+++ src/common/url/protocol.js
@@ -2,18 +2,8 @@
  * Copyright 2020 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import { getGlobalScope } from '../util/global-scope'
+import { globalScope } from '../util/global-scope'
 
-export const protocol = {
-  isFileProtocol: isFileProtocol,
-  supportabilityMetricSent: false
-}
-
-function isFileProtocol () {
-  let isFile = Boolean(getGlobalScope()?.location?.protocol === 'file:')
-  if (isFile) {
-    //metrics.recordSupportability('Generic/FileProtocol/Detected') -- may be implemented later? Probably make sure it's once per window
-    protocol.supportabilityMetricSent = true
-  }
-  return isFile
+export function isFileProtocol () {
+  return Boolean(globalScope?.location?.protocol === 'file:')
 }

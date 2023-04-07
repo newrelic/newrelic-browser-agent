76737817eeb32d47be89c0493067bd6beb558d76
Patrick Housley <patrickhousley@users.noreply.github.com>
2023-04-07 15:26:27 -0500

    Update src/common/url/clean-url.test.js
    Co-authored-by: bjfield <25965987+bjfield@users.noreply.github.com>

--- src/common/url/clean-url.test.js
+++ src/common/url/clean-url.test.js
@@ -20,6 +20,6 @@ test.each([
   ['http://domain.com/path?query=5#fragment#dumb', 'http://domain.com/path#fragment#dumb'],
   ['http://domain.com/path?query=5#fragment#dumb?additional_query', 'http://domain.com/path#fragment#dumb'],
   ['http://domain.com/path?query=5#fragment/silly/dumber#dumbest?additional_query=silly#what_is_this_even', 'http://domain.com/path#fragment/silly/dumber#dumbest']
-])('cleanURL should retain hash', (input, expected) => {
+])('cleanURL should retain hash if second argument is true', (input, expected) => {
   expect(cleanURL(input, true)).toEqual(expected)
 })

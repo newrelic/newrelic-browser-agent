727dc0a38560490c18cbb7948e46e7094f7255e1
Jordan Porter <silentsummitsoftware@gmail.com>
2023-04-12 22:27:41 -0400

    fix failing test

--- tests/browser/protocol.browser.js
+++ tests/browser/protocol.browser.js
@@ -4,7 +4,7 @@
  */
 
 var jil = require('../../tools/jil/driver/browser.js')
-import { protocol } from '../../src/common/url/protocol'
+import { isFileProtocol } from '../../src/common/url/protocol'
 import { setScope, resetScope } from '../../src/common/util/global-scope'
 
 var fileLocation = {
@@ -21,11 +21,10 @@ var fileLocation = {
 jil.browserTest('isFileProtocol returns coorectly when detecting file protocol', function (t) {
   setScope({ location: fileLocation })
 
-  t.ok(protocol.isFileProtocol(), 'Returned true when protocol is file protocol')
-  t.ok(protocol.supportabilityMetricSent, 'isFileProtocol should send supportability metric if file protocol is detected')
+  t.ok(isFileProtocol(), 'Returned true when protocol is file protocol')
 
   resetScope()
 
-  t.ok(!protocol.isFileProtocol(), 'Returned false when protocol is not file protocol')
+  t.ok(!isFileProtocol(), 'Returned false when protocol is not file protocol')
   t.end()
 })

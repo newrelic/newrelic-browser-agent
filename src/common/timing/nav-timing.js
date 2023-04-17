966f508ed519e491318d97ef512d4c98f0b0279d
Jordan Porter <silentsummitsoftware@gmail.com>
2023-04-17 11:30:09 -0400

    cleanup fn name

--- src/common/timing/nav-timing.js
+++ src/common/timing/nav-timing.js
@@ -30,38 +30,38 @@ export var navTimingValues = []
 export function addPT (offset, pt, v = {}) {
   if (!pt) return
   v.of = offset
-  addLit(0, v, 'n')
-  addLit(pt[UNLOAD_EVENT + START], v, 'u')
-  addLit(pt[REDIRECT + START], v, 'r')
-  addLit(pt[UNLOAD_EVENT + END], v, 'ue')
-  addLit(pt[REDIRECT + END], v, 're')
-  addLit(pt['fetch' + START], v, 'f')
-  addLit(pt[DOMAIN_LOOKUP + START], v, 'dn')
-  addLit(pt[DOMAIN_LOOKUP + END], v, 'dne')
-  addLit(pt['c' + ONNECT + START], v, 'c')
-  addLit(pt['secureC' + ONNECT + 'ion' + START], v, 's')
-  addLit(pt['c' + ONNECT + END], v, 'ce')
-  addLit(pt[REQUEST + START], v, 'rq')
-  addLit(pt[RESPONSE + START], v, 'rp')
-  addLit(pt[RESPONSE + END], v, 'rpe')
-  addLit(pt.domLoading, v, 'dl')
-  addLit(pt.domInteractive, v, 'di')
-  addLit(pt[DOM_CONTENT_LOAD_EVENT + START], v, 'ds')
-  addLit(pt[DOM_CONTENT_LOAD_EVENT + END], v, 'de')
-  addLit(pt.domComplete, v, 'dc')
-  addLit(pt[LOAD_EVENT + START], v, 'l')
-  addLit(pt[LOAD_EVENT + END], v, 'le')
+  handleValue(0, v, 'n')
+  handleValue(pt[UNLOAD_EVENT + START], v, 'u')
+  handleValue(pt[REDIRECT + START], v, 'r')
+  handleValue(pt[UNLOAD_EVENT + END], v, 'ue')
+  handleValue(pt[REDIRECT + END], v, 're')
+  handleValue(pt['fetch' + START], v, 'f')
+  handleValue(pt[DOMAIN_LOOKUP + START], v, 'dn')
+  handleValue(pt[DOMAIN_LOOKUP + END], v, 'dne')
+  handleValue(pt['c' + ONNECT + START], v, 'c')
+  handleValue(pt['secureC' + ONNECT + 'ion' + START], v, 's')
+  handleValue(pt['c' + ONNECT + END], v, 'ce')
+  handleValue(pt[REQUEST + START], v, 'rq')
+  handleValue(pt[RESPONSE + START], v, 'rp')
+  handleValue(pt[RESPONSE + END], v, 'rpe')
+  handleValue(pt.domLoading, v, 'dl')
+  handleValue(pt.domInteractive, v, 'di')
+  handleValue(pt[DOM_CONTENT_LOAD_EVENT + START], v, 'ds')
+  handleValue(pt[DOM_CONTENT_LOAD_EVENT + END], v, 'de')
+  handleValue(pt.domComplete, v, 'dc')
+  handleValue(pt[LOAD_EVENT + START], v, 'l')
+  handleValue(pt[LOAD_EVENT + END], v, 'le')
   return v
 }
 
 // Add Performance Navigation values to the given object
 export function addPN (pn, v) {
-  addRel(pn.type, v, 'ty')
-  addRel(pn.redirectCount, v, 'rc')
+  handleValue(pn.type, v, 'ty')
+  handleValue(pn.redirectCount, v, 'rc')
   return v
 }
 
-function addLit (value, obj, prop) {
+function handleValue (value, obj, prop) {
   if (typeof (value) === 'number' && (value >= 0)) {
     obj[prop] = Math.round(value)
   }

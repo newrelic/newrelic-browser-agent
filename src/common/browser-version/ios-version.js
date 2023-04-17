d3d6f0cd65b2fc4e5961d4e933b883bf11abf210
Patrick Housley <phousley@newrelic.com>
2023-04-17 11:29:37 -0500

    chore: fix no-tab lint rule

--- src/common/browser-version/ios-version.js
+++ src/common/browser-version/ios-version.js
@@ -4,7 +4,8 @@ export const isiOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent)
 
 // Shared Web Workers introduced in iOS 16.0+ and n/a in 15.6-
 export const iOS_below16 = isiOS && Boolean(typeof SharedWorker === 'undefined')
-/* ^ It was discovered in Safari 14 (https://bugs.webkit.org/show_bug.cgi?id=225305) that the buffered flag in PerformanceObserver
-	did not work. This affects our onFCP metric in particular since web-vitals uses that flag to retrieve paint timing entries.
-	This was fixed in v16+.
+/**
+ * ^ It was discovered in Safari 14 (https://bugs.webkit.org/show_bug.cgi?id=225305) that the buffered flag in PerformanceObserver
+ * did not work. This affects our onFCP metric in particular since web-vitals uses that flag to retrieve paint timing entries.
+ * This was fixed in v16+.
 */

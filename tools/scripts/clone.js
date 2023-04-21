81eb6128da6e1f51610092d0b98bc31cddb90fb4
Jordan Porter <silentsummitsoftware@gmail.com>
2023-04-21 15:05:32 -0400

    fix double semicolon issue

--- tools/scripts/clone.js
+++ tools/scripts/clone.js
@@ -4,12 +4,7 @@ const path = require('path')
 const buildDir = path.resolve(__dirname, '../../build/')
 const builtFileNames = fs.readdirSync(buildDir)
 
-function prependSemicolon (fileName, text) {
-  const newText = ';' + text
-  return fs.promises.writeFile(`${buildDir}/${fileName}`, newText)
-}
-
-(async () => {
+async function run () {
   const files = await Promise.all(builtFileNames.map(fileName => {
     return fs.promises.readFile(`${buildDir}/${fileName}`, 'utf-8')
   }))
@@ -22,7 +17,7 @@ function prependSemicolon (fileName, text) {
       if (!pieces[1]) return Promise.resolve()
       pieces[1] = pieces[1].map((v, i) => i === 1 || i === 2 ? 'x' : v).join('.')
       const major = pieces.join('-')
-      return prependSemicolon(major, content)
+      return fs.promises.writeFile(`${buildDir}/${major}`, content)
     }
     return Promise.resolve()
   }))
@@ -35,11 +30,13 @@ function prependSemicolon (fileName, text) {
       if (!pieces[1]) return Promise.resolve()
       pieces[1] = pieces[1].map((v, i) => i === 2 ? 'x' : v).join('.')
       const minor = pieces.join('-')
-      return prependSemicolon(minor, content)
+      return fs.promises.writeFile(`${buildDir}/${minor}`, content)
     }
     return Promise.resolve()
   }))
 
   console.log(`Processed ${fuzzyMajor.length} files for major fuzzy versions`)
   console.log(`Processed ${fuzzyMinor.length} files for minor fuzzy versions`)
-})()
+}
+
+run()

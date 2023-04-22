d65fd229bbf89bb88f3fc4326c9741d17cd3db46
Ben <bfield@newrelic.com>
2023-04-21 21:06:36 -0700

    Update functional test benchmarks

--- tests/specs/console-performance.e2e.js
+++ tests/specs/console-performance.e2e.js
@@ -14,17 +14,17 @@ describe('console method observation', () => {
     // Similar to very-large (1st percentile largest per day) console.log payloads observed in supportability metrics
     large: {
       size: 500 * 1024 * 1024, // 500 MB; maximum string length in Chrome is 536.8 million characters
-      time_limit: 3024 // ms
+      time_limit: 29 // ms
     },
     // Similar to ordinary-large (99th percentile largest per day) console.log payloads observed in supportability metrics
     medium: {
       size: 2 * 1024 * 1024, // 2 MB
-      time_limit: 52 // ms
+      time_limit: 20 // ms
     },
     // A good-sized console.log payload of a length suitable for reading by a human
     small: {
       size: 1 * 1024, // 1 KB
-      time_limit: 3 // ms
+      time_limit: 4 // ms
     }
   }
 
@@ -33,14 +33,14 @@ describe('console method observation', () => {
 
     | Browser        | Large | Medium | Small |
     |----------------|-------|--------|-------|
-    | firefox@latest |   864 |     11 |     2 |
-    | chrome@latest  |  2750 |     16 |     2 |
-    | safari@latest  |  2438 |     19 |     2 |
-    | edge@latest    |  1898 |     19 |     2 |
-    | ios@latest     |  2084 |     18 |     2 |
-    | ie@11          |   866 |     47 |     2 |
+    | firefox@latest |    16 |     16 |     3 |
+    | chrome@latest  |     7 |      5 |     2 |
+    | safari@latest  |     7 |      4 |     3 |
+    | edge@latest    |     6 |      5 |     2 |
+    | ios@latest     |    12 |      7 |     2 |
+    | ie@11          |    26 |     18 |     2 |
     |----------------|-------|--------|-------|
-    | largest + 10%  |  3024 |     52 |     3 |
+    | largest + 10%  |    29 |     20 |     4 |
 
     Time limits in testConfig are set to 10% more than the largest value for any browser, rounded up to the millisecond.
   */

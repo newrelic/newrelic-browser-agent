b9a57121e1756e56170d6aa3d64cc661465a293e
Ben <bfield@newrelic.com>
2023-04-24 09:27:41 -0700

    Add jest tests for dataSize

---
+++ src/common/util/data-size.test.js
@@ -0,0 +1,50 @@
+import { dataSize } from './data-size'
+
+describe('dataSize', () => {
+  test('returns length of string', () => {
+    const str = 'Hello, world!'
+    expect(dataSize(str)).toBe(str.length)
+  })
+
+  test('returns undefined for non-object, number, or empty string', () => {
+    expect(dataSize(Infinity)).toBeUndefined()
+    expect(dataSize(12345)).toBeUndefined()
+    expect(dataSize('')).toBeUndefined()
+  })
+
+  test('returns byte length of ArrayBuffer object', () => {
+    const buffer = new ArrayBuffer(8)
+    expect(dataSize(buffer)).toBe(8)
+  })
+
+  test('returns size of Blob object', () => {
+    const blob = new Blob(['Hello, world!'], { type: 'text/plain' })
+    expect(dataSize(blob)).toBe(blob.size)
+  })
+
+  test('returns undefined for FormData object', () => {
+    const formData = new FormData()
+    expect(dataSize(formData)).toBeUndefined()
+  })
+
+  test('returns length of JSON string representation of object', () => {
+    const obj = {
+      str: 'Hello, world!',
+      num: 12345,
+      nestedObj: {
+        arr: [1, 2, 3]
+      }
+    }
+    const expectedSize = JSON.stringify(obj).length
+    expect(dataSize(obj)).toBe(expectedSize)
+  })
+
+  test('returns undefined for object with toJSON method that throws an error', () => {
+    const obj = {
+      toJSON: () => {
+        throw new Error('Error in toJSON')
+      }
+    }
+    expect(dataSize(obj)).toBeUndefined()
+  })
+})

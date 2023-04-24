e7727ccaa7aa52c16ded67dfbfa053699650dc08
Ben <bfield@newrelic.com>
2023-04-24 08:00:44 -0700

    Consolidate duplicate calcs on arrays/objects

--- src/common/util/approximate-size.js
+++ src/common/util/approximate-size.js
@@ -10,36 +10,37 @@ export function approximateSize (object) {
   const stack = [object]
   let size = 0
   while (stack.length) {
-    const value = stack.pop()
-    if (value === null || value === true) size += 4
-    else if (value === false) size += 5
-    else if (typeof value === 'string') size += value.length + 2 // + ""
-    else if (typeof value === 'number') size += String(value).length
-    else if (value instanceof Date) size += 26 // ISO format + ""
-    else if (typeof value === 'object' && !seen.has(value)) {
-      // assuming zero bytes for circular values (but does count the key)
-      seen.add(value)
-      if (Array.isArray(value)) { // arrays and other iterables
-        size += 2 // []
+    try {
+      const value = stack.pop()
+      if (value === null || value === true) size += 4
+      else if (value === false) size += 5
+      else if (typeof value === 'string') size += value.length + 2 // + ""
+      else if (typeof value === 'number') size += String(value).length
+      else if (value instanceof Date) size += 26 // ISO format + ""
+      else if (typeof value === 'object' && !seen.has(value)) {
+        // assuming zero bytes for circular values (but does count the key)
+        seen.add(value)
+        size += 2 // [] or {}
         let commas = -1
-        for (const v of value) {
-          stack.push(v)
-          commas++
+        if (Array.isArray(value)) { // arrays and other iterables
+          for (const v of value) {
+            stack.push(v)
+            commas++
+          }
+        } else {
+          const keys = Object.keys(value)
+          if (keys && keys.length) {
+            keys.forEach(k => {
+              size += k.length + 3 // "":
+              stack.push(value[k])
+              commas++
+            })
+          }
         }
         if (commas > 0) size += commas
-      } else {
-        const keys = Object.keys(value)
-        if (keys && keys.length) {
-          size += 2 // {}
-          let commas = -1
-          keys.forEach(k => {
-            size += k.length + 3 // "":
-            stack.push(value[k])
-            commas++
-          })
-          if (commas > 0) size += commas
-        } else size += 2 // {}
       }
+    } catch (_) {
+      continue
     }
   }
 

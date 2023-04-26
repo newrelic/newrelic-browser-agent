ea49f72d56253db61cdcca44e7d6f0af7badabe3
Patrick Housley <patrickhousley@users.noreply.github.com>
2023-04-26 07:42:23 -0500

    fix: npm package version reporting (#484)
    Updates the npm package so it reports the non-pre-release version number in calls to the BAM server.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 const process = require('process')
+const fs = require('fs')
 
 module.exports = function (api) {
   api.cache(true)
 
   if (!process.env.BUILD_VERSION) {
-    process.env.BUILD_VERSION = require('./package.json').version
+    process.env.BUILD_VERSION = process.env.VERSION_OVERRIDE || fs.readFileSync('./VERSION', 'utf-8')?.trim()
   }
 
   const presets = [

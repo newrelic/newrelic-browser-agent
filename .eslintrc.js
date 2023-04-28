f659cec68aec218b4ddc18046318f018bde44686
Patrick Housley <phousley@newrelic.com>
2023-04-28 12:59:42 -0500

    chore: enable no-prototype-builtins lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -107,7 +107,6 @@ module.exports = {
     'valid-typeof': 'off',
     'no-undef': 'off',
     'no-control-regex': 'off',
-    'no-prototype-builtins': 'off',
     'no-eval': 'off',
     'no-return-assign': 'off',
 

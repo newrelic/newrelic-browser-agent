689d735ae7545d1c9b97f94ee9ccdb0a6575a1dd
Jordan Porter <silentsummitsoftware@gmail.com>
2023-05-01 17:37:57 -0400

    cleanup console and comment

--- src/common/session/session-entity.js
+++ src/common/session/session-entity.js
@@ -12,7 +12,7 @@ import { wrapEvents } from '../wrap'
 export class SessionEntity {
   /**
    * Create a self-managing Session Entity. This entity is scoped to the agent identifier which triggered it, allowing for multiple simultaneous session objects to exist.
-   * The key is inteded to be unique, if not, existing entities in the same agent ID will overlap and overwrite.
+   * There is one "namespace" an agent can store data in LS -- NRBA_{key}. If there are two agents on one page, and they both use the same key, they could overwrite each other since they would both use the same namespace in LS by default.
    * The value can be overridden in the constructor, but will default to a unique 16 character hex string
    * expiresMs and inactiveMs are used to "expire" the session, but can be overridden in the constructor. Pass 0 to disable expiration timers.
    */

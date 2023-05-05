66c511893b5000bbb09ec4765f8f0297c269e0c8
Patrick Housley <phousley@newrelic.com>
2023-05-05 12:10:48 -0500

    fix all browsers list

--- tools/browsers-lists/sauce-browsers.mjs
+++ tools/browsers-lists/sauce-browsers.mjs
@@ -26,53 +26,6 @@ const supportedBrowsers = ['chrome', 'edge', 'firefox', 'safari', 'android', 'io
 })()
 
 /**
- * Reduces the full array of SauceLabs browser-platform definitions to at most 4 eligible versions for each browser we support.
- * @param {[Object]} sauceBrowsers - An array of SauceLabs browser-platform definitions.
- * @returns {Object} - A set of SauceLabs browser-platform definitions eligible for testing.
- */
-function getBrowsers (sauceBrowsers, sample = 4) {
-  return supportedBrowsers.reduce((aggregator, name) => {
-    const filteredBrowsersList = sauceBrowsers
-      // Filter browsers lists for the browsers we want in this iteration
-      .filter(platformSelector(browserName(name)))
-      // Order browsers list by version number in ascending order
-      .sort((a, b) => Number(a.short_version) - Number(b.short_version))
-      // Remove duplicate version numbers.
-      .reduce((aggregator, sauceBrowser) => {
-        if (!aggregator[sauceBrowser.short_version]) {
-          aggregator[sauceBrowser.short_version] = sauceBrowser
-        }
-
-        return aggregator
-      }, {})
-
-    // Sample the version numbers
-    aggregator[name] = evenlySampleArray(Object.values(filteredBrowsersList), sample)
-      // Construct the metadata needed for the connection to saucelabs
-      .map(sauceBrowser => ({
-        browserName: mobileBrowserName(sauceBrowser),
-        platformName: mobilePlatformName(sauceBrowser),
-        platform: mobilePlatformName(sauceBrowser),
-        ...(sauceBrowser.automation_backend !== 'appium' && {
-          version: sauceBrowser.short_version,
-          browserVersion: sauceBrowser.short_version
-        }),
-        ...(sauceBrowser.device && { device: sauceBrowser.device }),
-        ...(sauceBrowser.automation_backend === 'appium' && { 'appium:deviceName': sauceBrowser.long_name }),
-        ...(sauceBrowser.automation_backend === 'appium' && { 'appium:platformVersion': sauceBrowser.short_version }),
-        ...(sauceBrowser.automation_backend === 'appium' && { 'appium:automationName': sauceBrowser.api_name === 'android' ? 'UiAutomator2' : 'XCUITest' }),
-        ...(sauceBrowser.automation_backend === 'appium' && {
-          'sauce:options': {
-            appiumVersion: sauceBrowser.recommended_backend_version
-          }
-        })
-      }))
-
-    return aggregator
-  }, {})
-}
-
-/**
  * Translates the name of a supported browser to the format used by SauceLabs browser-platform definitions.
  * @param {string} name - The name of a supported browser.
  * @returns {string} The SauceLabs `api_name` associated with the specified browser.
@@ -89,55 +42,14 @@ const browserName = name => {
 }
 
 /**
- * Returns a filter function to include SauceLabs browser-platform definitions with versions only in a certain range, if specified.
- * @param {string} desiredBrowser - The name of the browser (e.g., `safari`).
- * @returns {function} A filter function.
+ * Uses browserslist to deterine the minimum supported version based on a particular query.
+ * @param {string} query - A {@link https://browsersl.ist|properly formatted} browserslist query.
+ * @returns {number} The smallest version number of browsers matching the specified query.
  */
-function platformSelector (desiredBrowser) {
-  return (sauceBrowser) => {
-    if (hasKnownConnectionIssue(sauceBrowser)) return false
-    if (sauceBrowser.api_name !== desiredBrowser) return false
-    if (isNaN(Number(sauceBrowser.short_version))) return false
-    if (sauceBrowser.short_version < minSupportedVersion(desiredBrowser)) return false
-    if (sauceBrowser.short_version > maxSupportedVersion(desiredBrowser)) return false
-
-    switch (desiredBrowser) {
-      case 'iphone':
-      case 'ipad':
-      case 'android':
-        if (sauceBrowser.automation_backend !== 'appium') return false
-        break
-        // NOTE: the following platform limitation per browser is FRAGILE -- will have to update this in the future!
-      case 'firefox':
-        if (sauceBrowser.os !== 'Windows 10') return false // we're only testing FF on Win10
-        break
-      case 'MicrosoftEdge':
-      case 'chrome':
-        if (!sauceBrowser.os.startsWith('Windows 1')) return false // exclude Linux, Mac, and pre-Win10
-        break
-        // 'safari' will only ever be on MacOS
-      case 'safari':
-        if (sauceBrowser.short_version == 12 && sauceBrowser.os == 'Mac 10.13') return false // this OS+safari combo has issues with functional/XHR tests
-        break
-    }
-    return true
-  }
-}
-
-/**
- * May be used to exclude browsers known to have SauceLabs connection issues. For example, for a problem browser the
- * Sauce Connect Proxy might report "The environment you requested was unavailable. Infrastructure Error -- The Sauce
- * VMs failed to start the browser or device."
- * @param {*} sauceBrowser - A SauceLabs browser-platform definition object.
- * @returns {boolean} `true` if SauceLabs has a known issue connecting to the given browser.
- */
-function hasKnownConnectionIssue (sauceBrowser) {
-  switch (sauceBrowser.api_name) {
-    case 'firefox':
-      return ['59', '57', '49', '47'].includes(sauceBrowser.short_version)
-    default:
-      return false
-  }
+const browserslistMinVersion = query => {
+  const list = browserslist(query)
+  const version = list[list.length - 1].split(' ')[1] // browserslist returns id version pairs like 'ios_saf 16.1'
+  return Number(version.split('-')[0]) // versions might be a range (e.g. 14.0-14.4), and we want the low end.
 }
 
 /**
@@ -186,14 +98,97 @@ const maxSupportedVersion = apiName => {
 }
 
 /**
- * Uses browserslist to deterine the minimum supported version based on a particular query.
- * @param {string} query - A {@link https://browsersl.ist|properly formatted} browserslist query.
- * @returns {number} The smallest version number of browsers matching the specified query.
+ * Reduces the full array of SauceLabs browser-platform definitions to at most 4 eligible versions for each browser we support.
+ * @param {[Object]} sauceBrowsers - An array of SauceLabs browser-platform definitions.
+ * @returns {Object} - A set of SauceLabs browser-platform definitions eligible for testing.
  */
-const browserslistMinVersion = query => {
-  const list = browserslist(query)
-  const version = list[list.length - 1].split(' ')[1] // browserslist returns id version pairs like 'ios_saf 16.1'
-  return Number(version.split('-')[0]) // versions might be a range (e.g. 14.0-14.4), and we want the low end.
+function getBrowsers (sauceBrowsers, sample = 4) {
+  return supportedBrowsers.reduce((aggregator, name) => {
+    const desiredBrowserName = browserName(name)
+    const filteredBrowsersList = sauceBrowsers
+      // Filter browsers lists for the browsers we want in this iteration
+      .filter(sample === Infinity
+        ? platformSelector(desiredBrowserName)
+        : platformSelector(desiredBrowserName, minSupportedVersion(desiredBrowserName), maxSupportedVersion(desiredBrowserName))
+      )
+      // Order browsers list by version number in ascending order
+      .sort((a, b) => Number(a.short_version) - Number(b.short_version))
+      // Remove duplicate version numbers.
+      .reduce((aggregator, sauceBrowser) => {
+        if (!aggregator[sauceBrowser.short_version]) {
+          aggregator[sauceBrowser.short_version] = sauceBrowser
+        }
+
+        return aggregator
+      }, {})
+
+    // Sample the version numbers
+    aggregator[name] = evenlySampleArray(Object.values(filteredBrowsersList), sample)
+      // Construct the metadata needed for the connection to saucelabs
+      .map(sauceBrowser => {
+        const browserName = mobileBrowserName(sauceBrowser)
+        const platformName = mobilePlatformName(sauceBrowser)
+        return {
+          browserName,
+          platformName,
+          platform: platformName,
+          version: sauceBrowser.short_version,
+          ...(sauceBrowser.automation_backend !== 'appium' && {
+            browserVersion: sauceBrowser.short_version
+          }),
+          ...(sauceBrowser.device && { device: sauceBrowser.device }),
+          ...(sauceBrowser.automation_backend === 'appium' && { 'appium:deviceName': sauceBrowser.long_name }),
+          ...(sauceBrowser.automation_backend === 'appium' && { 'appium:platformVersion': sauceBrowser.short_version }),
+          ...(sauceBrowser.automation_backend === 'appium' && { 'appium:automationName': sauceBrowser.api_name === 'android' ? 'UiAutomator2' : 'XCUITest' }),
+          ...(sauceBrowser.automation_backend === 'appium' && {
+            'sauce:options': {
+              appiumVersion: sauceBrowser.recommended_backend_version
+            }
+          }),
+          ...(browserName.toLowerCase() === 'safari' && { acceptInsecureCerts: false })
+        }
+      })
+
+    return aggregator
+  }, {})
+}
+
+/**
+ * Returns a filter function to include SauceLabs browser-platform definitions with versions only in a certain range, if specified.
+ * @param {string} desiredBrowser - The name of the browser (e.g., `safari`).
+ * @param {number} minVersion - The minimum browser version to include (e.g., `14`).
+ * @param {number} maxVersion - The maximum browser version to include (e.g., `16`).
+ * @returns {function} A filter function.
+ */
+function platformSelector (desiredBrowser, minVersion = 0, maxVersion = Infinity) {
+  return (sauceBrowser) => {
+    if (hasKnownConnectionIssue(sauceBrowser)) return false
+    if (sauceBrowser.api_name !== desiredBrowser) return false
+    if (isNaN(Number(sauceBrowser.short_version))) return false
+    if (sauceBrowser.short_version < minVersion) return false
+    if (sauceBrowser.short_version > maxVersion) return false
+
+    switch (desiredBrowser) {
+      case 'iphone':
+      case 'ipad':
+      case 'android':
+        if (sauceBrowser.automation_backend !== 'appium') return false
+        break
+        // NOTE: the following platform limitation per browser is FRAGILE -- will have to update this in the future!
+      case 'firefox':
+        if (sauceBrowser.os !== 'Windows 10') return false // we're only testing FF on Win10
+        break
+      case 'MicrosoftEdge':
+      case 'chrome':
+        if (!sauceBrowser.os.startsWith('Windows 1')) return false // exclude Linux, Mac, and pre-Win10
+        break
+        // 'safari' will only ever be on MacOS
+      case 'safari':
+        if (sauceBrowser.short_version == 12 && sauceBrowser.os == 'Mac 10.13') return false // this OS+safari combo has issues with functional/XHR tests
+        break
+    }
+    return true
+  }
 }
 
 /**
@@ -245,12 +240,25 @@ function mobilePlatformName (sauceBrowser) {
     case 'iphone':
     case 'ipad':
       return 'iOS'
-    case 'safari':
-      // return browser.os.replace('Mac', 'macOS')
-      return sauceBrowser.os
     case 'android':
       return 'Android'
     default:
       return sauceBrowser.os
+  }
+}
+
+/**
+ * May be used to exclude browsers known to have SauceLabs connection issues. For example, for a problem browser the
+ * Sauce Connect Proxy might report "The environment you requested was unavailable. Infrastructure Error -- The Sauce
+ * VMs failed to start the browser or device."
+ * @param {*} sauceBrowser - A SauceLabs browser-platform definition object.
+ * @returns {boolean} `true` if SauceLabs has a known issue connecting to the given browser.
+ */
+function hasKnownConnectionIssue (sauceBrowser) {
+  switch (sauceBrowser.api_name) {
+    case 'firefox':
+      return ['59', '57', '49', '47'].includes(sauceBrowser.short_version)
+    default:
+      return false
   }
 }

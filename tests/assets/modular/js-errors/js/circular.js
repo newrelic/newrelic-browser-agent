0a6a9aca2658dc68c22d2c21e1859650817accb6
Ben <bfield@newrelic.com>
2023-05-09 06:44:03 -0700

    Remove unused 'modular' test assets

--- tests/assets/modular/js-errors/js/circular.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2020 New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-var ouroboros = {}
-ouroboros.ouroboros = ouroboros
-var e = new Error('asdf'); e.message = ouroboros; throw e

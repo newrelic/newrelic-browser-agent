755bcae06f1804b668227a92b8598980e4acd200
Patrick Housley <phousley@newrelic.com>
2023-05-15 12:38:16 -0500

    fixing tests

--- tests/assets/worker/js/module-worker.js
+++ tests/assets/worker/js/module-worker.js
@@ -3,8 +3,10 @@
 onmessage = function (e) {
   if (e.data.type === 'startAgent') {
     self.NREUM = e.data.payload
-    importScripts('/web-worker-agent') // JIL's endpoint to fetch the single bundle web worker pkg
-    self.postMessage({ type: 'ready' })
+    // Module workers do not support importScripts, need to import directly
+    import('../../../../../../../../web-worker-agent').then(() => {
+      self.postMessage({ type: 'ready' })
+    })
   } else if (e.data.type === 'command') {
     // Let errors go unhandled so bad commands crashes the tests for troubleshooting.
     let retVal = eval(e.data.fn) // run the literal string cmd

f9587cb604759e397740cf4478daf1b0261e7ded
Patrick Housley <phousley@newrelic.com>
2023-05-16 16:27:13 -0500

    addressing PR comments

---
+++ tools/browsers-lists/extended-debugging.mjs
@@ -0,0 +1,19 @@
+import browsersAll from './browsers-all.json' assert { type: "json" }
+
+export default function browserSupportsExtendedDebugging ({ browserName, browserVersion, version }) {
+  if (!['chrome', 'firefox'].includes(browserName)) {
+    return false
+  }
+
+  if (browserVersion === 'latest') {
+    return true
+  }
+
+  if (browserName === 'firefox' && (Number(browserVersion) >= 53 || Number(version) >= 53)) {
+    return true
+  }
+
+  const latestChrome = browsersAll.chrome
+    .reduce((aggregator, sauceBrowser) => aggregator = Math.max(Number(sauceBrowser.version), Number(sauceBrowser.browserVersion), aggregator), 0)
+  return (Number(browserVersion) || Number(version)) >= latestChrome - 2
+}

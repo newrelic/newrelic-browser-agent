145d9c292fcfaa8c4292475e4a39bce33fd2ca48
Patrick Housley <phousley@newrelic.com>
2023-05-20 15:08:28 -0500

    use default config

--- tests/specs/third-party-compatibility/run-test.js
+++ tests/specs/third-party-compatibility/run-test.js
@@ -1,15 +1,4 @@
-const config = {
-  loader: 'spa',
-  init: {
-    ajax: { deny_list: [], harvestTimeSeconds: 5 },
-    jserrors: { harvestTimeSeconds: 5 },
-    metrics: { harvestTimeSeconds: 5 },
-    page_action: { harvestTimeSeconds: 5 },
-    page_view_timing: { harvestTimeSeconds: 5 },
-    session_trace: { harvestTimeSeconds: 5 },
-    spa: { harvestTimeSeconds: 5 }
-  }
-}
+import { config } from '../../util/defaults'
 
 export default async function runTest ({
   browser,

a167f6d4203ebc824072782a9246d978cf2c8a2f
Jordan Porter <silentsummitsoftware@gmail.com>
2023-05-21 19:10:46 -0400

    fix ie test

--- tools/testing-server/plugins/browser-scripts/browser-scripts-transform.js
+++ tools/testing-server/plugins/browser-scripts/browser-scripts-transform.js
@@ -4,53 +4,60 @@ const preprocessify = require('preprocessify')
 const fse = require('fs-extra')
 const path = require('path')
 
-function processScript (scriptPath, enablePolyfills) {
-  return new Promise((resolve, reject) => {
+async function processScript (scriptPath, enablePolyfills) {
+  if (!scriptPath) return ''
+  try {
     const reRoutedPath = 'tests/assets/scripts/tests' + scriptPath.split('/tests').pop()
-    fse.readFile(path.join(process.cwd(), reRoutedPath)).then(f => {
-      resolve(f)
-    }).catch(() => {
-      browserify(scriptPath)
-        .transform('babelify', {
-          envName: 'test',
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                loose: true,
-                targets: {
-                  browsers: enablePolyfills
-                    ? ['ie >= 11']
-                    : [
-                        'last 10 Chrome versions',
-                        'last 10 Safari versions',
-                        'last 10 Firefox versions',
-                        'last 10 Edge versions',
-                        'last 10 ChromeAndroid versions',
-                        'last 10 iOS versions'
-                      ]
-                }
-              }
-            ]
-          ],
-          plugins: [
-            '@babel/plugin-proposal-optional-chaining',
-            '@babel/plugin-proposal-nullish-coalescing-operator',
-            '@babel/plugin-proposal-logical-assignment-operators',
-            '@babel/plugin-proposal-class-properties', // Addresses a problem handling static class properties.
-            '@babel/plugin-proposal-private-methods' // Enables class private methods.
-          ]
-        })
-        .transform(preprocessify())
-        .bundle((err, buf) => {
-          if (err) {
-            return reject(err)
-          }
+    const prebuiltFile = await fse.readFile(path.join(process.cwd(), reRoutedPath))
+    if (prebuiltFile) return prebuiltFile
+    return browserifyScript(scriptPath, enablePolyfills)
+  } catch (err) {
+    return browserifyScript(scriptPath, enablePolyfills)
+  }
+}
 
-          let content = buf.toString()
-          resolve(content)
-        })
-    })
+function browserifyScript (scriptPath, enablePolyfills) {
+  return new Promise((resolve, reject) => {
+    browserify(scriptPath)
+      .transform('babelify', {
+        envName: 'test',
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              loose: true,
+              targets: {
+                browsers: enablePolyfills
+                  ? ['ie >= 11']
+                  : [
+                      'last 10 Chrome versions',
+                      'last 10 Safari versions',
+                      'last 10 Firefox versions',
+                      'last 10 Edge versions',
+                      'last 10 ChromeAndroid versions',
+                      'last 10 iOS versions'
+                    ]
+              }
+            }
+          ]
+        ],
+        plugins: [
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-logical-assignment-operators',
+          '@babel/plugin-proposal-class-properties', // Addresses a problem handling static class properties.
+          '@babel/plugin-proposal-private-methods' // Enables class private methods.
+        ]
+      })
+      .transform(preprocessify())
+      .bundle((err, buf) => {
+        if (err) {
+          return reject(err)
+        }
+
+        let content = buf.toString()
+        resolve(content)
+      })
   })
 }
 

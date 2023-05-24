9d14ecd8484b27871fb5ad6ff62031d6e5516a79
Jordan Porter <silentsummitsoftware@gmail.com>
2023-05-24 09:48:15 -0400

    add comment disclaimers for session replay [wip] features

--- src/features/session_replay/aggregate/index.js
+++ src/features/session_replay/aggregate/index.js
@@ -5,10 +5,11 @@
 /**
  * @file Records, aggregates, and harvests session replay data.
  *
- * NOTE: This code is under development and dormant. It will not download to instrumented pages or record data.
+ * NOTE: This code is under development and dormant. It will not download to instrumented pages or record any data.
+ * It is not production ready, and is not intended to be imported or implemented in any build of the browser agent until
+ * functionality is validated and a full user experience is curated.
  */
 
-// import { record } from 'rrweb'
 import { drain } from '../../../common/drain/drain'
 import { registerHandler } from '../../../common/event-emitter/register-handler'
 import { HarvestScheduler } from '../../../common/harvest/harvest-scheduler'
--- src/features/session_replay/index.js
+++ src/features/session_replay/index.js
@@ -3,20 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 /**
- * @file Primes the Session Replay feature for lazy loading.
+ * @file The top-level entry point for the eventual Session Replay feature.
  *
  * NOTE: This code is under development and dormant. It will not download to instrumented pages or record any data.
  * It is not production ready, and is not intended to be imported or implemented in any build of the browser agent until
  * functionality is validated and a full user experience is curated.
  */
-import { InstrumentBase } from '../../utils/instrument-base'
-import { FEATURE_NAME } from '../constants'
-
-export class Instrument extends InstrumentBase {
-  static featureName = FEATURE_NAME
-  constructor (agentIdentifier, aggregator, auto = true) {
-    super(agentIdentifier, aggregator, FEATURE_NAME, auto)
-
-    this.importAggregator()
-  }
-}
+export { Instrument as SessionReplay } from './instrument/index'

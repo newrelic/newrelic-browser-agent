734cb5a1ccbbfabde489cf51b5b53d4ca38dffd8
Patrick Housley <phousley@newrelic.com>
2023-05-24 19:27:49 -0500

    chore: threading testing server for wdio

--- tools/wdio/args.mjs
+++ tools/wdio/args.mjs
@@ -1,8 +1,10 @@
 import process from 'process'
 import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
 import loaders from './util/loaders.js'
 
-const args = yargs(process.argv.slice(2))
+process.argvOriginal = [...process.argv].slice(2)
+const args = yargs(hideBin(process.argv))
   .usage('$0 file1[, filen] [options]')
   .example('$0 tests/**/*.js -vb "chrome@39, firefox, ie@>8"')
 

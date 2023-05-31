95c2461fe6f5e8c3b6a7e3a49307676791ae71d5
Ben <bfield@newrelic.com>
2023-05-31 15:27:19 -0700

    Clean up test structure

--- src/common/util/console.test.js
+++ src/common/util/console.test.js
@@ -1,36 +1,34 @@
 import { warn } from './console'
 
-describe('console', () => {
-  beforeEach(() => {
-    console.warn = jest.fn()
+beforeEach(() => {
+  console.warn = jest.fn()
+})
+
+afterEach(() => {
+  jest.restoreAllMocks()
+})
+
+describe('warn', () => {
+  test('should not call console.warn if it is not a function', () => {
+    const spy = jest.spyOn(console, 'warn')
+    console.warn = undefined
+    warn('test message')
+    expect(spy).not.toHaveBeenCalled()
   })
 
-  afterEach(() => {
-    jest.restoreAllMocks()
+  test('should call console.warn with a prefixed message', () => {
+    warn('test message')
+    expect(console.warn).toHaveBeenCalledWith('New Relic: test message')
   })
 
-  describe('warn', () => {
-    it('should not call console.warn if it is not a function', () => {
-      const spy = jest.spyOn(console, 'warn')
-      console.warn = undefined
-      warn('test message')
-      expect(spy).not.toHaveBeenCalled()
-    })
+  test('should call console.warn with secondary argument if provided', () => {
+    const secondary = 'secondary value'
+    warn('test message', secondary)
+    expect(console.warn).toHaveBeenCalledWith(secondary)
+  })
 
-    it('should call console.warn with a prefixed message', () => {
-      warn('test message')
-      expect(console.warn).toHaveBeenCalledWith('New Relic: test message')
-    })
-
-    it('should call console.warn with secondary argument if provided', () => {
-      const secondary = 'secondary value'
-      warn('test message', secondary)
-      expect(console.warn).toHaveBeenCalledWith(secondary)
-    })
-
-    it('should not call console.warn with secondary argument if not provided', () => {
-      warn('test message')
-      expect(console.warn).toHaveBeenCalledTimes(1)
-    })
+  test('should not call console.warn with secondary argument if not provided', () => {
+    warn('test message')
+    expect(console.warn).toHaveBeenCalledTimes(1)
   })
 })

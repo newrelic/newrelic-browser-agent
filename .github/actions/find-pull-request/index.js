79d4125b24a10e870935a8aa98b9c3d692bc350a
Patrick Housley <phousley@newrelic.com>
2023-05-30 19:27:54 -0500

    fix pr comments from codecov

--- .github/actions/find-pull-request/index.js
+++ .github/actions/find-pull-request/index.js
@@ -2,6 +2,7 @@ import process from 'process'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+const prRequired = process.env['PR_REQUIRED']?.toLowerCase() === 'true'
 const octokit = github.getOctokit(process.env['GITHUB_TOKEN'])
 const branchName = process.env['GITHUB_REF'].replace('refs/heads/', '')
 
@@ -13,11 +14,15 @@ const { data: pullRequests } = await octokit.rest.pulls.list({
 })
 
 if (!Array.isArray(pullRequests) || pullRequests.length === 0) {
-  throw new Error(`No pull request found for branch ${branchName} in the newrelic/newrelic-browser-agent repository.`)
+  if (prRequired) {
+    throw new Error(`No pull request found for branch ${branchName} in the newrelic/newrelic-browser-agent repository.`)
+  } else {
+    core.setOutput('results', JSON.stringify({}))
+  }
+} else {
+  core.setOutput('results', JSON.stringify({
+    head: pullRequests[0].head.ref,
+    base: pullRequests[0].base.ref,
+    number: pullRequests[0].number
+  }))
 }
-
-core.setOutput('results', JSON.stringify({
-  head: pullRequests[0].head.ref,
-  base: pullRequests[0].base.ref,
-  number: pullRequests[0].number
-}))

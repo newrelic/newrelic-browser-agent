308443cc432c48c813eb1a078cf36edd124d7f1b
Patrick Housley <phousley@newrelic.com>
2023-05-31 09:41:45 -0500

    fix jest workflow

--- .github/actions/find-pull-request/index.js
+++ .github/actions/find-pull-request/index.js
@@ -7,17 +7,17 @@ const octokit = github.getOctokit(process.env['GITHUB_TOKEN'])
 const branchName = process.env['GITHUB_REF'].replace('refs/heads/', '')
 
 const { data: pullRequests } = await octokit.rest.pulls.list({
-  owner: 'newrelic',
-  repo: 'newrelic-browser-agent',
+  owner: github.context.repo.owner,
+  repo: github.context.repo.repo,
   state: 'open',
-  head: `newrelic/newrelic-browser-agent:${branchName}`
+  head: `${github.context.repo.owner}/${github.context.repo.repo}:${branchName}`
 })
 
 if (!Array.isArray(pullRequests) || pullRequests.length === 0) {
   if (prRequired) {
-    throw new Error(`No pull request found for branch ${branchName} in the newrelic/newrelic-browser-agent repository.`)
+    throw new Error(`No pull request found for branch ${branchName} in the ${github.context.repo.owner}/${github.context.repo.repo} repository.`)
   } else {
-    core.setOutput('results', JSON.stringify({}))
+    core.setOutput('results', null)
   }
 } else {
   core.setOutput('results', JSON.stringify({

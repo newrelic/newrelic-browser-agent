9a23f6b731615e1a283c89926f13d56e9c9a190f
Chunwai Li <cli@newrelic.com>
2023-05-31 09:00:32 -0500

    Add err handling in getSessionReplayMode

--- src/features/session_replay/replay-mode.js
+++ src/features/session_replay/replay-mode.js
@@ -3,6 +3,7 @@ import { ee } from '../../common/event-emitter/contextual-ee'
 import { FEATURE_NAMES } from '../../loaders/features/features'
 import { MODE } from '../../common/session/session-entity'
 import { registerHandler } from '../../common/event-emitter/register-handler'
+import { gosNREUM } from '../../common/window/nreum'
 
 /**
  * Figure out if the Replay feature is running (what mode it's in).
@@ -12,12 +13,14 @@ import { registerHandler } from '../../common/event-emitter/register-handler'
  * @returns Promise that resolves to one of the values in MODE enum
  */
 export async function getSessionReplayMode (agentId, sharedAggregator) {
-  // Should be enabled by configuration and using an agent build that includes it (via checking that the instrument class was initialized).
-  if (getConfigurationValue(agentId, 'session_replay.enabled') && typeof newrelic.initializedAgents[agentId].features.session_replay === 'object') {
-    const srEntitlement = await new Promise(resolve => registerHandler('rumresp-sr', on => resolve(on), FEATURE_NAMES.sessionReplay, ee.get(agentId)))
-    if (srEntitlement === true) {
-      return await sharedAggregator.sessionReplayInitialized // wait for replay to determine which mode it's after running its sampling logic
+  try {
+    // Should be enabled by configuration and using an agent build that includes it (via checking that the instrument class was initialized).
+    if (getConfigurationValue(agentId, 'session_replay.enabled') && typeof gosNREUM().initializedAgents[agentId].features.session_replay === 'object') {
+      const srEntitlement = await new Promise(resolve => registerHandler('rumresp-sr', on => resolve(on), FEATURE_NAMES.sessionReplay, ee.get(agentId)))
+      if (srEntitlement === true) {
+        return await sharedAggregator.sessionReplayInitialized // wait for replay to determine which mode it's after running its sampling logic
+      }
     }
-  }
+  } catch (e) { /* exception ==> off */ }
   return MODE.OFF // at any step of the way s.t. SR cannot be on by implication or is explicitly off
 }

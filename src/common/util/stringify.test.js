11f3b07666302e10e387a61e080dd18fba23c30c
Ben <bfield@newrelic.com>
2023-06-01 07:03:47 -0700

    Remove outer describe

--- src/common/util/stringify.test.js
+++ src/common/util/stringify.test.js
@@ -8,44 +8,42 @@ jest.mock('../event-emitter/contextual-ee', () => ({
   }
 }))
 
-describe('stringify', () => {
-  it('should return a JSON string representation of the value', () => {
-    const obj = { a: 1, b: { nested: true } }
-    const expected = '{"a":1,"b":{"nested":true}}'
+test('should return a JSON string representation of the value', () => {
+  const obj = { a: 1, b: { nested: true } }
+  const expected = '{"a":1,"b":{"nested":true}}'
 
-    const result = stringify(obj)
+  const result = stringify(obj)
 
-    expect(result).toBe(expected)
+  expect(result).toBe(expected)
+})
+
+test('should handle circular references and exclude them from the JSON output', () => {
+  const obj = { a: 1 }
+  obj.b = obj // Create a circular reference
+  const expected = '{"a":1}'
+
+  const result = stringify(obj)
+
+  expect(result).toBe(expected)
+})
+
+test('should handle non-object values and return their string representation', () => {
+  const value = 42
+  const expected = '42'
+
+  const result = stringify(value)
+
+  expect(result).toBe(expected)
+})
+
+test('should emit an "internal-error" event if an error occurs during JSON.stringify', () => {
+  const obj = { a: 1 }
+
+  jest.spyOn(JSON, 'stringify').mockImplementation(() => {
+    throw new Error('message')
   })
 
-  it('should handle circular references and exclude them from the JSON output', () => {
-    const obj = { a: 1 }
-    obj.b = obj // Create a circular reference
-    const expected = '{"a":1}'
+  stringify(obj)
 
-    const result = stringify(obj)
-
-    expect(result).toBe(expected)
-  })
-
-  it('should handle non-object values and return their string representation', () => {
-    const value = 42
-    const expected = '42'
-
-    const result = stringify(value)
-
-    expect(result).toBe(expected)
-  })
-
-  it('should emit an "internal-error" event if an error occurs during JSON.stringify', () => {
-    const obj = { a: 1 }
-
-    jest.spyOn(JSON, 'stringify').mockImplementation(() => {
-      throw new Error('message')
-    })
-
-    stringify(obj)
-
-    expect(mockEmit).toHaveBeenCalledWith('internal-error', expect.any(Array))
-  })
+  expect(mockEmit).toHaveBeenCalledWith('internal-error', expect.any(Array))
 })

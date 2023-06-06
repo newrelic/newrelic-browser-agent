db61ed74611776c501dda4f4ae593e518c171d56
Patrick Housley <phousley@newrelic.com>
2023-06-06 16:33:42 -0500

    fix multiline comments

--- .github/actions/comment-pull-request/index.js
+++ .github/actions/comment-pull-request/index.js
@@ -3,6 +3,22 @@ import { args } from './args.js'
 
 const octokit = github.getOctokit(args.githubToken)
 
+let commentBody = args.comment.split('\n')
+commentBody = commentBody.concat(args.commentTag.split('\n'))
+
+while (commentBody.length > 0 && commentBody[0].trim() === '') {
+  // Do not start the comment with an empty line
+  commentBody.shift()
+}
+
+while (commentBody.length > 0 && commentBody[commentBody.length - 1].trim() === '') {
+  // Do not end the comment with an empty line
+  commentBody.splice(-1)
+}
+
+// Trim surrounding whitespace
+commentBody = commentBody.map(c => c.trim())
+
 let comment
 if (args.commentTag && args.commentTag.toString().trim() !== '') {
   for await (const { data: comments} of octokit.paginate.iterator(
@@ -26,7 +42,7 @@ if (comment) {
     repo: github.context.repo.repo,
     issue_number: args.prNumber,
     comment_id: comment.id,
-    body: args.comment.toString().trim() + '\n' + args.commentTag.toString().trim()
+    body: commentBody.join('\n')
   })
 } else {
   await octokit.rest.issues.createComment({
@@ -34,6 +50,6 @@ if (comment) {
     repo: github.context.repo.repo,
     issue_number: args.prNumber,
     comment_id: comment.id,
-    body: args.comment.toString().trim() + '\n' + args.commentTag.toString().trim()
+    body: commentBody.join('\n')
   })
 }

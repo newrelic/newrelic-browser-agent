276c4f6ee35063e6b22cb2dc4c70729ffb89ca3d
Patrick Housley <patrickhousley@users.noreply.github.com>
2023-06-07 16:15:13 -0500

    feat: remove unused data from stn requests (#570)

--- tests/functional/stn/index.test.js
+++ tests/functional/stn/index.test.js
@@ -8,8 +8,6 @@ const testDriver = require('../../../tools/jil/index')
 let supported = testDriver.Matcher.withFeature('stn')
 
 testDriver.test('posts session traces', supported, function (t, browser, router) {
-  t.plan(5)
-
   let rumPromise = router.expectRum()
   let resourcePromise = router.expectResources()
   let loadPromise = browser.get(router.assetURL('lotsatimers.html')).waitForFeature('loaded')
@@ -17,10 +15,8 @@ testDriver.test('posts session traces', supported, function (t, browser, router)
   Promise.all([resourcePromise, rumPromise, loadPromise]).then(([{ request: { query } }]) => {
     t.ok(+query.st > 1408126770885, `Got start time ${query.st}`)
     t.notok(query.ptid, 'No ptid on first harvest')
-    t.equal(query.ja, '{"aargh":"somanytimers"}', 'custom javascript attributes (on stn first post)')
     return router.expectResources().then(({ request: { query } }) => {
       t.ok(query.ptid, `ptid on second harvest ${query.ptid}`)
-      t.equal(query.ja, undefined, 'no javascript attributes (on stn second post)')
       t.end()
     })
   }).catch(fail)

19961adace56e17a2a12550bc7f7f9e83219555d
Jordan Porter <silentsummitsoftware@gmail.com>
2023-06-09 13:46:31 -0400

    separate out component tests from unit tests

--- jest.component-config.js
+++ jest.component-config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   modulePathIgnorePatterns: ['<rootDir>/temp'],
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/**/?(*.)+(test).[tj]s?(x)'],
+  testMatch: ['<rootDir>/src/**/?(*.)+(component-test).[tj]s?(x)'],
   transform: {
     '\\.m?[jt]sx?$': 'babel-jest'
   },

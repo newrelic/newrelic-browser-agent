057d621e3d094f40fe5f8da9cc97e573b8c706ea
Patrick Housley <phousley@newrelic.com>
2023-06-15 23:21:31 -0500

    stn retry changes

--- tests/specs/stn/retry-harvesting.e2e.js
+++ tests/specs/stn/retry-harvesting.e2e.js
@@ -42,7 +42,7 @@ describe('ins retry harvesting', () => {
   );
 
   [400, 404, 502, 504, 512].forEach(statusCode =>
-    it(`should not send the page action on the next harvest when the first harvest statusCode is ${statusCode}`, async () => {
+    it(`should not send the session trace on the next harvest when the first harvest statusCode is ${statusCode}`, async () => {
       await browser.testHandle.scheduleReply('bamServer', {
         test: testResourcesRequest,
         permanent: true,
@@ -57,26 +57,27 @@ describe('ins retry harvesting', () => {
       ])
 
       // Pause a bit for browsers built-in automated retry logic crap
-      await browser.pause(500)
-      await browser.testHandle.clearScheduledReplies('bamServer')
+      // await browser.pause(500)
+      // await browser.testHandle.clearScheduledReplies('bamServer')
 
-      const ptid = faker.datatype.uuid()
-      await browser.testHandle.scheduleReply('bamServer', {
-        test: testResourcesRequest,
-        permanent: true,
-        body: ptid
-      })
+      // const ptid = faker.datatype.uuid()
+      // await browser.testHandle.scheduleReply('bamServer', {
+      //   test: testResourcesRequest,
+      //   permanent: true,
+      //   body: ptid
+      // })
 
-      const secondResourcesHarvest = await browser.testHandle.expectResources()
-      const [thirdResourcesHarvest] = await Promise.all([
-        browser.testHandle.expectResources(),
-        $('#trigger').click()
-      ])
+      // const secondResourcesHarvest = await browser.testHandle.expectResources()
+      // const [thirdResourcesHarvest] = await Promise.all([
+      //   browser.testHandle.expectResources(),
+      //   $('#trigger').click()
+      // ])
 
       expect(firstResourcesHarvest.reply.statusCode).toEqual(statusCode)
-      expect(secondResourcesHarvest.request.body.res).not.toEqual(expect.arrayContaining(firstResourcesHarvest.request.body.res))
-      expect(secondResourcesHarvest.request.query.ptid).toBeUndefined()
-      expect(thirdResourcesHarvest.request.query.ptid).toEqual(ptid)
+      // expect(secondResourcesHarvest.request.body.res).not.toEqual(expect.arrayContaining(firstResourcesHarvest.request.body.res))
+      // expect(secondResourcesHarvest.request.query.ptid).toBeUndefined()
+      // expect(thirdResourcesHarvest.request.query.ptid).toEqual(ptid)
+      await expect(browser.testHandle.expectResources(10000, true)).resolves.toBeUndefined()
     })
   )
 })

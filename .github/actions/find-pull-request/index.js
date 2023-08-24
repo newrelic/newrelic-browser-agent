d61ad75c528306426bf7c84ce5fcf0cca5291c4c
Patrick Housley <patrickhousley@users.noreply.github.com>
2023-08-24 06:25:06 -0500

    chore: fix pr integration test code coverage (#653)

--- .github/actions/find-pull-request/index.js
+++ .github/actions/find-pull-request/index.js
@@ -3,36 +3,50 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { args } from './args.js'
 
+if (args.githubRef.startsWith('refs/tags/')) {
+  errorResult('Tags cannot have associated pull requests.')
+}
+
 const octokit = github.getOctokit(args.githubToken)
-const branchName = args.githubRef.replace('refs/heads/', '')
+let pullNumber
 
-const { data: pullRequests } = await octokit.rest.pulls.list({
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  state: 'open',
-  head: `${github.context.repo.owner}/${github.context.repo.repo}:${branchName}`
-})
+if (args.githubRef.startsWith('refs/heads/')) {
+  const branchName = args.githubRef.replace('refs/heads/', '')
 
-if (!Array.isArray(pullRequests) || pullRequests.length === 0) {
-  errorResult(`No pull request found for branch ${branchName}.`)
+  const { data: pullRequests } = await octokit.rest.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    state: 'open',
+    head: `${github.context.repo.owner}/${github.context.repo.repo}:${branchName}`
+  })
+
+  if (!Array.isArray(pullRequests) || pullRequests.length === 0) {
+    errorResult(`No pull request found for branch ${branchName}.`)
+  }
+
+  pullNumber = pullRequests[0].number
+}
+
+if (args.githubRef.startsWith('refs/pull/')) {
+  pullNumber = args.githubRef.split('/')[2]
 }
 
 const { data: pullRequest } = await octokit.rest.pulls.get({
   owner: github.context.repo.owner,
   repo: github.context.repo.repo,
-  pull_number: pullRequests[0].number
+  pull_number: pullNumber
 })
 
 if (!(pullRequest?.mergeable)) {
-  errorResult(`Pull request found for branch ${branchName} contains merge conflicts.`)
+  errorResult(`Pull request found for ref ${args.githubRef} contains merge conflicts.`)
 }
 
 core.setOutput('results', JSON.stringify({
-  head_ref: pullRequests[0].head.ref,
-  head_sha: pullRequests[0].head.sha,
-  base_ref: pullRequests[0].base.ref,
-  base_sha: pullRequests[0].base.sha,
-  pr_number: pullRequests[0].number
+  head_ref: pullRequest.head.ref,
+  head_sha: pullRequest.head.sha,
+  base_ref: pullRequest.base.ref,
+  base_sha: pullRequest.base.sha,
+  pr_number: pullRequest.number
 }))
 
 function errorResult(errorMessage) {

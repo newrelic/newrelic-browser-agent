27141b8852abf4a64fe253fba46849ca5abbc8db
Patrick Housley <phousley@newrelic.com>
2023-08-29 15:11:23 -0500

    damnit

--- src/common/session/constants.js
+++ src/common/session/constants.js
@@ -1,5 +1,3 @@
 export const PREFIX = 'NRBA'
-// export const DEFAULT_EXPIRES_MS = 14400000
-// export const DEFAULT_INACTIVE_MS = 1800000
-export const DEFAULT_EXPIRES_MS = 30000
-export const DEFAULT_INACTIVE_MS = 15000
+export const DEFAULT_EXPIRES_MS = 14400000
+export const DEFAULT_INACTIVE_MS = 1800000

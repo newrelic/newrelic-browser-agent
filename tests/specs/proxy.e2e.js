9fa7616287497d3fca7b5bc675474039dc8d406f
Chunwai Li <cli@newrelic.com>
2023-09-01 10:08:13 -0500

    Fix wdio test

--- tests/specs/proxy.e2e.js
+++ tests/specs/proxy.e2e.js
@@ -1,8 +1,10 @@
-import { notIE } from '../../tools/browser-matcher/common-matchers.mjs'
+import { notIE, notSafari, notIOS } from '../../tools/browser-matcher/common-matchers.mjs'
 
 describe('Using proxy servers -', () => {
   // Safari v14- doesn't support this feature either, but that's OoS of existing testing versions.
-  it.withBrowsersMatching(notIE)('setting an assetsPath in info changes where agent fetches its chunks from', async () => {
+  // Furthermore: Safari does not include resource entries for failed script resource, so it's totally excluded.
+
+  it.withBrowsersMatching([notIE, notSafari, notIOS])('setting an assetsPath in info changes where agent fetches its chunks from', async () => {
     const { host, port } = browser.testHandle.assetServerConfig
     /** This is where we expect the agent to fetch its chunk. '/build/' is necessary within the URL because the way our asset server
      * handles each test requests -- see /testing-server/plugins/test-handle/index.js. */
@@ -12,6 +14,7 @@ describe('Using proxy servers -', () => {
     // Expecting a resource fetch to https://bam-test-1.nr-local.net:<asset port>/build/fakepath/nr-spa.min.js for chunk
     await browser.setTimeout({ pageLoad: 10000 }) // not expecting RUM to be called, etc.
     await browser.url(url)
+    await browser.pause(500) // give it a bit of time to process
     let resources = await browser.execute(function () {
       return performance.getEntriesByType('resource')
     })

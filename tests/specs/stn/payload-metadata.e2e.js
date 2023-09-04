0db8cc520bc20ddd91e2d30b99c1dd8be7acd2ee
Jordan Porter <silentsummitsoftware@gmail.com>
2023-09-03 19:10:34 -0600

    add test

---
+++ tests/specs/stn/payload-metadata.e2e.js
@@ -0,0 +1,16 @@
+describe('STN Payload metadata checks', () => {
+  it('adds node count and timestamp metadata query attrs', async () => {
+    await browser.url(await browser.testHandle.assetURL('stn/instrumented.html'))
+      .then(() => browser.waitForAgentLoad())
+
+    // n == node count
+    const resources = await browser.testHandle.expectResources()
+    expect(Number(resources.request.query.n)).toEqual(resources.request.body.res.length)
+
+    // fts == first timestamp
+    const ms = resources.request.query.fts - resources.request.query.st
+    const firstNode = resources.request.body.res.reduce((acc, next) => (!acc || next.s < acc) ? next.s : acc, undefined)
+    expect(ms).toBeGreaterThanOrEqual(0)
+    expect(ms).toEqual(firstNode)
+  })
+})

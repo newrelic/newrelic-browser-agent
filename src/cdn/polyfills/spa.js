d54dfdd0dbc6b0beb96b5e79cf10e840933c9dbf
Jordan Porter <silentsummitsoftware@gmail.com>
2023-09-24 13:00:25 -0600

    fix relative path error

--- src/cdn/polyfills/spa.js
+++ src/cdn/polyfills/spa.js
@@ -4,7 +4,7 @@
  */
 
 import '../polyfills.js'
-import { Agent } from '../loaders/agent'
+import { Agent } from '../../loaders/agent'
 
 import { Instrument as InstrumentPageViewEvent } from '../../features/page_view_event/instrument'
 import { Instrument as InstrumentPageViewTiming } from '../../features/page_view_timing/instrument'

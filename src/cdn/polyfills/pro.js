a15b6e123d2f9ab827bd59a3cc3f0dfd8a98fa6e
Jordan Porter <insomniacrampage@hotmail.com>
2023-09-25 11:38:49 -0600

    chore: Add more control over the polyfill builds (#731)

--- src/cdn/polyfills/pro.js
+++ src/cdn/polyfills/pro.js
@@ -1,7 +1,28 @@
 /**
- * @file Creates a version of the "Pro" agent loader with [core-js]{@link https://github.com/zloirock/core-js}
+ * @file Creates a version of the "PRO" agent loader with [core-js]{@link https://github.com/zloirock/core-js}
  * polyfills for pre-ES6 browsers and IE 11.
  */
 
 import '../polyfills.js'
-import '../pro'
+import { Agent } from '../../loaders/agent'
+
+import { Instrument as InstrumentPageViewEvent } from '../../features/page_view_event/instrument'
+import { Instrument as InstrumentPageViewTiming } from '../../features/page_view_timing/instrument'
+import { Instrument as InstrumentMetrics } from '../../features/metrics/instrument'
+import { Instrument as InstrumentErrors } from '../../features/jserrors/instrument'
+import { Instrument as InstrumentXhr } from '../../features/ajax/instrument'
+import { Instrument as InstrumentSessionTrace } from '../../features/session_trace/instrument'
+import { Instrument as InstrumentPageAction } from '../../features/page_action/instrument'
+
+new Agent({
+  features: [
+    InstrumentPageViewEvent,
+    InstrumentPageViewTiming,
+    InstrumentSessionTrace,
+    InstrumentXhr,
+    InstrumentMetrics,
+    InstrumentPageAction,
+    InstrumentErrors
+  ],
+  loaderType: 'pro-polyfills'
+})

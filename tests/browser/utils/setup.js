cd7644f2ae8052f57ed0c59e37f9d6f3cc7ae010
Chunwai Li <cli@newrelic.com>
2023-10-24 13:32:05 -0500

    fix unit test setup for real

--- tests/browser/utils/setup.js
+++ tests/browser/utils/setup.js
@@ -1,9 +1,11 @@
 const { ee } = require('../../../src/common/event-emitter/contextual-ee')
-const { gosNREUM } = require('../../../src/common/window/nreum')
+const { gosNREUM, gosNREUMInitializedAgents } = require('../../../src/common/window/nreum')
 const { Aggregator } = require('../../../src/common/aggregate/aggregator')
 const { configure } = require('../../../src/loaders/configure/configure')
 export function setup (agentIdentifier = (Math.random() + 1).toString(36).substring(7)) {
-  const api = configure({ agentIdentifier }, {}, 'browser-test', true)
+  const fakeAgent = { agentIdentifier }
+  gosNREUMInitializedAgents(agentIdentifier, fakeAgent)
+  const api = configure(fakeAgent, {}, 'browser-test', true)
   const nr = gosNREUM()
   const aggregator = new Aggregator({ agentIdentifier, ee })
   const baseEE = ee.get(agentIdentifier)

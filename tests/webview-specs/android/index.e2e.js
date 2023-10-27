e12fbe02e26bd9e9bc334f7a7d5a115878c2cbec
Patrick Housley <phousley@newrelic.com>
2023-10-27 08:13:31 -0500

    chore: add webview testing

---
+++ tests/webview-specs/android/index.e2e.js
@@ -0,0 +1,43 @@
+import { onlyAndroid } from '../../../tools/browser-matcher/common-matchers.mjs'
+
+describe.withBrowsersMatching(onlyAndroid)('android webview', () => {
+  it('should load the agent and send back data', async () => {
+    // Load up the test page
+    const url = await driver.testHandle.assetURL('instrumented.html')
+    const addressBar = await $('android=new UiSelector().resourceId("com.webview.myapplication:id/activity_main_textview")')
+    await addressBar.clearValue()
+
+    // // Setup expects and submit the url
+    const [rumResult, resourcesResult, spaResult] = await Promise.all([
+      driver.testHandle.expectRum(),
+      driver.testHandle.expectResources(),
+      driver.testHandle.expectInteractionEvents(),
+      addressBar.setValue(url)
+    ])
+
+    expect(rumResult.request.body).toEqual('')
+    expect(rumResult.request.query).toEqual(expect.objectContaining({
+      ref: url.slice(0, url.indexOf('?')),
+      t: 'Unnamed Transaction'
+    }))
+    expect(resourcesResult.request.query).toEqual(expect.objectContaining({
+      ref: url.slice(0, url.indexOf('?')),
+      t: 'Unnamed Transaction'
+    }))
+    expect(resourcesResult.request.body.res.length).toBeGreaterThanOrEqual(1)
+    expect(spaResult.request.query).toEqual(expect.objectContaining({
+      ref: url.slice(0, url.indexOf('?')),
+      t: 'Unnamed Transaction'
+    }))
+    expect(spaResult.request.body).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        category: 'Initial page load',
+        type: 'interaction',
+        trigger: 'initialPageLoad',
+        initialPageURL: url.slice(0, url.indexOf('?')),
+        newURL: url.slice(0, url.indexOf('?')),
+        oldURL: url.slice(0, url.indexOf('?'))
+      })
+    ]))
+  })
+})

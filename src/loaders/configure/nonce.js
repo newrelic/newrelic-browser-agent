fc28a02438c7f548a8d22eaf86ef8a853b3cb9a7
Jordan Porter <silentsummitsoftware@gmail.com>
2023-11-09 09:52:32 -0700

    fix nonce for ie tests

--- src/loaders/configure/nonce.js
+++ src/loaders/configure/nonce.js
@@ -5,15 +5,6 @@ __webpack_require__.nc = (() => {
     if (document.currentScript && document.currentScript.nonce) {
       return document.currentScript.nonce
     }
-
-    // Fallback for browsers that do not support document.currentScript (IE11)
-    const scriptTags = document.querySelectorAll('script')
-    for (let i = 0; i < scriptTags.length; i++) {
-      const scriptTag = scriptTags[i]
-      if (scriptTag.nonce) {
-        return scriptTag.nonce
-      }
-    }
   } catch (ex) {
     // Swallow error and proceed like nonce is not defined
   }
--- tests/specs/csp.e2e.js
+++ tests/specs/csp.e2e.js
@@ -1,7 +1,8 @@
+import { notIE } from '../../tools/browser-matcher/common-matchers.mjs'
 import { faker } from '@faker-js/faker'
 
 describe('Content Security Policy', () => {
-  it('should support a nonce script element', async () => {
+  it.withBrowsersMatching(notIE)('should support a nonce script element', async () => {
     const nonce = faker.datatype.uuid()
     await Promise.all([
       browser.testHandle.expectRum(),

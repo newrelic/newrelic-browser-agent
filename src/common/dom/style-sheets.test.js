07fb4f08a35839a4ddf3cd37ff1e295394d987b0
Jordan Porter <silentsummitsoftware@gmail.com>
2023-12-18 09:56:41 -0700

    add check for clone

--- src/common/dom/style-sheets.test.js
+++ src/common/dom/style-sheets.test.js
@@ -5,24 +5,27 @@
 import { reinjectStylesheetAsAnonymous } from './style-sheets'
 
 let cssScript
+const cloner = jest.fn(createStylesheet)
 
 function createStylesheet () {
   const css = document.createElement('link')
   css.src = 'https://res.cloudinary.com/beleza-na-web/raw/upload/blz/11.102.0/main/assets/boticariostore/css/showcase.css'
   css.ref = 'stylesheet'
   css.type = 'text/css'
-  css.cloneNode = jest.fn(createStylesheet)
+  css.cloneNode = cloner
   return css
 }
 
 describe('reinject stylesheets as anonymous', () => {
   beforeEach(() => {
+    jest.clearAllMocks()
     cssScript = createStylesheet()
     document.head.prepend(cssScript)
   })
 
-  it('should inject the new node and remove the old node', async () => {
+  it('should clone the old node | inject the new node | remove the old node', async () => {
     reinjectStylesheetAsAnonymous(cssScript)
+    expect(cloner).toHaveBeenCalledTimes(1)
     expect(document.querySelectorAll('link').length).toEqual(1)
     expect(document.querySelector('link')).toMatchObject({
       crossOrigin: 'anonymous',

a4a20420a7872567f945b3ffcaeb8f113a25b5f3
Patrick Housley <phousley@newrelic.com>
2024-01-29 14:04:05 -0600

    chore: disable SR and ST for experiments

--- .github/actions/build-ab/templates/experiments.js
+++ .github/actions/build-ab/templates/experiments.js
@@ -3,6 +3,9 @@ window.NREUM.loader_config.applicationID = '{{{args.abAppId}}}'
 window.NREUM.loader_config.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.info.applicationID = '{{{args.abAppId}}}'
 window.NREUM.info.licenseKey = '{{{args.abLicenseKey}}}'
+window.NREUM.init.session_replay.enabled = false
+window.NREUM.init.session_trace.enabled = false
+window.NREUM.init.feature_flags = ['soft_nav']
 
 {{#if experimentScripts}}
 {{#each experimentScripts}}

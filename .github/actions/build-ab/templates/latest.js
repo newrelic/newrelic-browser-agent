282e9388cea2bb301f3e47a61ec924be17466286
Chunwai Li <cli@newrelic.com>
2024-02-01 11:03:44 -0600

    Add soft_nav ff to latest template

--- .github/actions/build-ab/templates/latest.js
+++ .github/actions/build-ab/templates/latest.js
@@ -3,5 +3,6 @@ window.NREUM.loader_config.applicationID = '{{{args.abAppId}}}'
 window.NREUM.loader_config.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.info.applicationID = '{{{args.abAppId}}}'
 window.NREUM.info.licenseKey = '{{{args.abLicenseKey}}}'
+window.NREUM.init.feature_flags = ['soft_nav']
 
 {{{latestScript}}}

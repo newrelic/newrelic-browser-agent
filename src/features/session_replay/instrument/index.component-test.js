aff8f1d1afbde98dc3285e60a37d0681866c7bc0
Jordan Porter <silentsummitsoftware@gmail.com>
2024-02-27 11:20:12 -0700

    test cleanup

--- src/features/session_replay/instrument/index.component-test.js
+++ src/features/session_replay/instrument/index.component-test.js
@@ -1,4 +1,4 @@
-import { Instrument as SpaInstrument } from './index'
+import { Instrument as SRInstrument } from './index'
 import { Aggregator } from '../../../common/aggregate/aggregator'
 import { getConfigurationValue, originals } from '../../../common/config/config'
 import { PREFIX, DEFAULT_KEY, MODE } from '../../../common/session/constants'
@@ -45,27 +45,22 @@ beforeEach(() => {
   jest.resetModules()
 })
 
-// test('Without session and preload flag, replay queues agg import', () => {
-//   new SpaInstrument(agentIdentifier, new Aggregator({}))
-//   expect(typeof importAggregatorFn).toEqual('function')
-// })
-
 describe('Replay', () => { // this is moreso a test of the SR-specific logic within importAggregator() of instrument-base.js
   test('does import with all pre-req settings on', async () => {
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'session_replay.preload') // not testing preload behavior yet
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
-    expect(spa.recorder).toBeUndefined() // should not have set preload recorder with flag off and no existing session!
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
+    expect(sr.recorder).toBeUndefined() // should not have set preload recorder with flag off and no existing session!
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(true)
   })
 
   test('does not import if MutationObserver is missing', async () => {
     originals.MO = undefined
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'session_replay.preload')
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(false)
 
     originals.MO = true // restore this for other test use
@@ -73,17 +68,17 @@ describe('Replay', () => { // this is moreso a test of the SR-specific logic wit
 
   test('does not import if cookies_enabled is false', async () => {
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'session_replay.preload' && setting !== 'privacy.cookies_enabled')
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(false)
   })
 
   test('does not import if session_trace is disabled', async () => {
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'session_replay.preload' && setting !== 'session_trace.enabled')
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(false)
   })
 
@@ -93,9 +88,9 @@ describe('Replay', () => { // this is moreso a test of the SR-specific logic wit
       setupAgentSession: jest.fn()
     }))
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'session_replay.preload')
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(false)
   })
 })
@@ -107,33 +102,33 @@ describe('Preload early records', () => {
 
   test('with flag enabled and if session dne yet', async () => {
     getConfigurationValue.mockImplementation(() => true)
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
     await new Promise(process.nextTick)
-    expect(spa.recorder.startRecording).toHaveBeenCalled()
+    expect(sr.recorder.startRecording).toHaveBeenCalled()
   })
 
   test('when replay already on in existing session, even if preload flag disabled', async () => {
     localStorage.setItem(`${PREFIX}_${DEFAULT_KEY}`, JSON.stringify({ sessionReplayMode: MODE.FULL }))
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'session_replay.preload')
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
     await new Promise(process.nextTick)
-    expect(spa.recorder.startRecording).toHaveBeenCalled()
+    expect(sr.recorder.startRecording).toHaveBeenCalled()
   })
 
   test('when replay already on in existing session, even if flag enabled but a pre-req is not', async () => {
     localStorage.setItem(`${PREFIX}_${DEFAULT_KEY}`, JSON.stringify({ sessionReplayMode: MODE.ERROR }))
     getConfigurationValue.mockImplementation((_, setting) => setting !== 'privacy.cookies_enabled')
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
     await new Promise(process.nextTick)
-    expect(spa.recorder.startRecording).toHaveBeenCalled()
+    expect(sr.recorder.startRecording).toHaveBeenCalled()
   })
 
   test('if replay is off in existing session, but all required flags are enabled', async () => {
     localStorage.setItem(`${PREFIX}_${DEFAULT_KEY}`, JSON.stringify({ sessionReplayMode: MODE.OFF }))
     getConfigurationValue.mockImplementation(() => true)
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
     await new Promise(process.nextTick)
-    expect(spa.recorder.startRecording).toHaveBeenCalled()
+    expect(sr.recorder.startRecording).toHaveBeenCalled()
   })
 })
 
@@ -147,29 +142,28 @@ describe('Preload recording stops if', () => {
       __esModule: true,
       setupAgentSession: jest.fn(() => { throw new Error('RIP') })
     }))
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
     await new Promise(process.nextTick) // since the startRecording -> importAggregator chain is async
-    expect(spa.recorder.startRecording).toHaveBeenCalled()
+    expect(sr.recorder.startRecording).toHaveBeenCalled()
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(false)
-    expect(spa.recorder.stopRecording).toHaveBeenCalled()
-    expect(spa.featAggregate).toBeUndefined() // aggregate also shouldn't have been imported if session entity fails
+    expect(sr.recorder.stopRecording).toHaveBeenCalled()
+    expect(sr.featAggregate).toBeUndefined() // aggregate also shouldn't have been imported if session entity fails
   })
 
   test('replay aggregate fails to initialize', async () => {
     let aggConstructor = jest.fn(() => { throw new Error('RIP') })
-    jest.doMock('../aggregate', () => ({
+    jest.doMock('../aggregate/index.js', () => ({
       __esModule: true,
       Aggregate: aggConstructor
     }))
-    const spa = new SpaInstrument(agentIdentifier, new Aggregator({}))
+    const sr = new SRInstrument(agentIdentifier, new Aggregator({}))
     await new Promise(process.nextTick)
-    expect(spa.recorder.startRecording).toHaveBeenCalled()
+    expect(sr.recorder.startRecording).toHaveBeenCalled()
 
-    const loaded = await spa.onAggregateImported
+    const loaded = await sr.onAggregateImported
     expect(loaded).toEqual(false)
-    expect(spa.recorder.stopRecording).toHaveBeenCalled()
-    // expect(aggConstructor).toHaveBeenCalled() -- can't get this spy to capture
+    expect(sr.recorder.stopRecording).toHaveBeenCalled()
   })
 })

6dd09c505af87b3a1b08330362eca46951ea22ed
Patrick Housley <patrickhousley@users.noreply.github.com>
2024-04-18 12:08:40 -0500

    fix: JSEerrors harvest hasReplay decoration (#986)

--- tests/specs/session-replay/adjacent-payloads.e2e.js
+++ tests/specs/session-replay/adjacent-payloads.e2e.js
@@ -1,34 +1,56 @@
 import { notIE } from '../../../tools/browser-matcher/common-matchers.mjs'
 import { config, decodeAttributes } from './helpers'
 
-describe('errors', () => {
-  beforeEach(async () => {
-    await browser.enableSessionReplay()
-  })
+describe.withBrowsersMatching(notIE)('Adjacent Payloads', () => {
+  describe('JSErrors', () => {
+    beforeEach(async () => {
+      await browser.enableSessionReplay()
+    })
 
-  afterEach(async () => {
-    await browser.destroyAgentSession()
-  })
+    afterEach(async () => {
+      await browser.destroyAgentSession()
+    })
 
-  it.withBrowsersMatching(notIE)('error timestamp should be contained within replay timestamp', async () => {
-    await browser.url(await browser.testHandle.assetURL('rrweb-instrumented.html', config({
-      session_replay: { error_sampling_rate: 100, sampling_rate: 0 }
-    })))
-      .then(() => browser.waitForAgentLoad())
+    it('error timestamp should be contained within replay timestamp', async () => {
+      await browser.url(await browser.testHandle.assetURL('rrweb-instrumented.html', config({
+        session_replay: { error_sampling_rate: 100, sampling_rate: 0 }
+      })))
+        .then(() => browser.waitForAgentLoad())
 
-    // this issue was seen when some time had passed from agg load time and was running in error mode
-    await browser.pause(5000)
+      // this issue was seen when some time had passed from agg load time and was running in error mode
+      await browser.pause(5000)
 
-    const [errorPayload, blobPayload] = await Promise.all([
-      browser.testHandle.expectErrors(10000),
-      browser.testHandle.expectBlob(10000),
-      browser.execute(function () {
-        newrelic.noticeError(new Error('test'))
-      })
-    ])
+      const [errorPayload, blobPayload] = await Promise.all([
+        browser.testHandle.expectErrors(10000),
+        browser.testHandle.expectBlob(10000),
+        browser.execute(function () {
+          newrelic.noticeError(new Error('test'))
+        })
+      ])
 
-    const errorTimestamp = errorPayload.request.body.err[0].params.firstOccurrenceTimestamp
-    const SRTimestamp = decodeAttributes(blobPayload.request.query.attributes)['replay.firstTimestamp']
-    expect(errorTimestamp).toBeGreaterThan(SRTimestamp)
+      const errorTimestamp = errorPayload.request.body.err[0].params.firstOccurrenceTimestamp
+      const SRTimestamp = decodeAttributes(blobPayload.request.query.attributes)['replay.firstTimestamp']
+      expect(errorTimestamp).toBeGreaterThan(SRTimestamp)
+      expect(errorPayload.request.body.err[0].params.hasReplay).toEqual(true)
+    })
+
+    it('error should have hasReplay removed when preloaded but not autostarted', async () => {
+      await browser.url(await browser.testHandle.assetURL('rrweb-instrumented.html', config({
+        session_replay: { preload: true, error_sampling_rate: 0, sampling_rate: 100, autoStart: false }
+      })))
+        .then(() => browser.waitForAgentLoad())
+
+      // this issue was seen when some time had passed from agg load time and was running in error mode
+      await browser.pause(5000)
+
+      const [errorPayload] = await Promise.all([
+        browser.testHandle.expectErrors(10000),
+        browser.execute(function () {
+          newrelic.noticeError(new Error('test'))
+        })
+      ])
+
+      expect(errorPayload.request.body.err[0].params.hasReplay).toBeUndefined()
+    })
   })
 })

9c29e3e8155c52ac75146b5bde995380340295b3
Chunwai Li <cli@newrelic.com>
2024-04-24 16:18:07 -0500

    Fix set config

--- src/common/config/state/configurable.js
+++ src/common/config/state/configurable.js
@@ -12,8 +12,9 @@ export function getModeledObject (obj, model) {
     const target = Object.keys(output).length === 0 ? obj : output
     for (let key in target) {
       if (obj[key] === undefined) continue
-      if (obj[key] === null) { output[key] = null; continue }
       try {
+        if (obj[key] === null) { output[key] = null; continue }
+
         if (Array.isArray(obj[key]) && Array.isArray(model[key])) output[key] = Array.from(new Set([...obj[key], ...model[key]]))
         else if (typeof obj[key] === 'object' && typeof model[key] === 'object') output[key] = getModeledObject(obj[key], model[key])
         else output[key] = obj[key]

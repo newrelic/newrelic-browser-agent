7fdd578e05065fbf861ff7ef5b11eef5afa41aa3
Chunwai Li <cli@newrelic.com>
2024-04-26 12:29:13 -0500

    Finish conversion of ajax-events.browser.js

--- tests/components/ajax/aggregate.test.js
+++ tests/components/ajax/aggregate.test.js
@@ -2,7 +2,9 @@ import { ee } from '../../../src/common/event-emitter/contextual-ee'
 import { Aggregator } from '../../../src/common/aggregate/aggregator'
 import { FEATURE_NAMES } from '../../../src/loaders/features/features'
 import { getNREUMInitializedAgent } from '../../../src/common/window/nreum'
+import { getInfo } from '../../../src/common/config/config'
 import * as hMod from '../../../src/common/event-emitter/handle'
+import qp from '@newrelic/nr-querypack'
 
 window.fetch = jest.fn(() => Promise.resolve())
 window.Request = jest.fn()
@@ -31,8 +33,11 @@ jest.spyOn(hMod, 'handle')
 const agentIdentifier = 'abcdefg'
 
 describe('Ajax aggregate', () => {
-  let ajaxAggregate, context
+  let ajaxAggregate, context, mockCurrentInfo
   beforeAll(async () => {
+    mockCurrentInfo = { jsAttributes: {} }
+    getInfo.mockReturnValue(mockCurrentInfo)
+
     const ajaxInstrument = new Ajax(agentIdentifier, new Aggregator({ agentIdentifier, ee }))
     await expect(ajaxInstrument.onAggregateImported).resolves.toEqual(true)
     ajaxAggregate = ajaxInstrument.featAggregate
@@ -42,6 +47,7 @@ describe('Ajax aggregate', () => {
     { // params
       method: 'PUT',
       status: 200,
+      host: 'https://example.com',
       hostname: 'example.com',
       pathname: '/pathname'
     },
@@ -51,7 +57,7 @@ describe('Ajax aggregate', () => {
       cbTime: 5
     },
     0, // startTime
-    10, // endTime
+    30, // endTime
     'XMLHttpRequest' // type
   ]
   beforeEach(() => {
@@ -127,10 +133,113 @@ describe('Ajax aggregate', () => {
   })
 
   describe('prepareHarvest', () => {
+    test('correctly serializes an AjaxRequest events payload', () => {
+      const expected = {
+        type: 'ajax',
+        start: 0,
+        end: 30,
+        callbackEnd: 30,
+        callbackDuration: 0,
+        domain: 'https://example.com',
+        path: '/pathname',
+        method: 'PUT',
+        status: 200,
+        requestedWith: 'XMLHttpRequest',
+        requestBodySize: 128,
+        responseBodySize: 256,
+        nodeId: '0',
+        guid: null,
+        traceId: null,
+        timestamp: null
+      }
 
+      ajaxAggregate.ee.emit('xhr', ajaxArguments, context)
+
+      const expectedCustomAttributes = {
+        customStringAttribute: 'customStringAttribute',
+        customNumAttribute: 2,
+        customBooleanAttribute: true,
+        nullCustomAttribute: null,
+        undefinedCustomAttribute: undefined // will be treated as null
+      }
+      mockCurrentInfo.jsAttributes = expectedCustomAttributes
+
+      const serializedPayload = ajaxAggregate.prepareHarvest({ retry: false })
+      // serializedPayload from ajax comes back as an array of bodies now, so we just need to decode each one and flatten
+      // this decoding does not happen elsewhere in the app so this only needs to happen here in this specific test
+      const decodedEvents = serializedPayload.map(sp => qp.decode(sp.body.e))
+
+      decodedEvents.forEach(payload => {
+        payload.forEach(event => {
+          validateCustomAttributeValues(expectedCustomAttributes, event.children)
+          delete event.children
+
+          expect(event).toEqual(expected) // event attributes serialized correctly
+        })
+      })
+
+      mockCurrentInfo.jsAttributes = {} // reset jsattributes for other tests
+    })
+
+    test('correctly serializes a very large AjaxRequest events payload', () => {
+      for (let callNo = 0; callNo < 10; callNo++) ajaxAggregate.ee.emit('xhr', ajaxArguments, context)
+
+      const expectedCustomAttributes = {
+        customStringAttribute: 'customStringAttribute',
+        customNumAttribute: 2,
+        customBooleanAttribute: true,
+        undefinedCustomAttribute: undefined
+      }
+      mockCurrentInfo.jsAttributes = expectedCustomAttributes
+
+      const maxPayloadSize = (ajaxAggregate.MAX_PAYLOAD_SIZE = 500)
+      const serializedPayload = ajaxAggregate.prepareHarvest({ retry: false })
+      const decodedEvents = serializedPayload.map(sp => qp.decode(sp.body.e))
+
+      expect(decodedEvents.length).toBeGreaterThan(1) // large payload of AJAX Events are broken into multiple chunks
+      expect(serializedPayload.every(sp => !exceedsSizeLimit(sp))).toBeTruthy() // each chunks is less than the maxPayloadSize
+
+      decodedEvents.forEach(payload => {
+        payload.forEach(event => {
+          validateCustomAttributeValues(expectedCustomAttributes, event.children) // Custom attributes are accounted for in chunked AJAX payloads
+        })
+      })
+      mockCurrentInfo.jsAttributes = {}
+
+      function exceedsSizeLimit (payload) {
+        return payload.length * 2 > maxPayloadSize
+      }
+    })
+
+    test('correctly exits if maxPayload is too small', () => {
+      for (let callNo = 0; callNo < 10; callNo++) ajaxAggregate.ee.emit('xhr', ajaxArguments, context)
+
+      ajaxAggregate.MAX_PAYLOAD_SIZE = 10 // this is too small for any AJAX payload to fit in
+      const serializedPayload = ajaxAggregate.prepareHarvest({ retry: false })
+      expect(serializedPayload.length).toEqual(0) // payload that are each too small for limit will be dropped
+    })
   })
 })
 
-// function exceedsSizeLimit (payload, maxPayloadSize) {
-//   return payload.length * 2 > maxPayloadSize
-// }
+function validateCustomAttributeValues (expectedCustomAttributes, childrenNodes) {
+  expect(childrenNodes.length).toEqual(Object.keys(expectedCustomAttributes).length)
+  childrenNodes.forEach(attribute => {
+    switch (attribute.type) {
+      case 'stringAttribute':
+      case 'doubleAttribute':
+        expect(expectedCustomAttributes[attribute.key]).toEqual(attribute.value)
+        break
+      case 'trueAttribute':
+        expect(expectedCustomAttributes[attribute.key]).toEqual(true)
+        break
+      case 'falseAttribute':
+        expect(expectedCustomAttributes[attribute.key]).toEqual(false)
+        break
+      case 'nullAttribute':
+        expect(expectedCustomAttributes[attribute.key]).toBeNil()
+        break
+      default:
+        throw new Error('unexpected custom attribute type')
+    }
+  })
+}

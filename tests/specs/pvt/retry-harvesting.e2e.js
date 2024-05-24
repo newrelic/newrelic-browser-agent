df4e344dd830bc65908d5c96fcb575fdc38eb0fb
Jordan Porter <silentsummitsoftware@gmail.com>
2024-05-24 13:27:01 -0600

    add matching statuscode case tests

--- tests/specs/pvt/retry-harvesting.e2e.js
+++ tests/specs/pvt/retry-harvesting.e2e.js
@@ -1,33 +1,70 @@
 import { testTimingEventsRequest } from '../../../tools/testing-server/utils/expect-tests'
 
 describe('pvt harvesting', () => {
-  it('timings are retried when collector returns 429', async () => {
-    await browser.testHandle.scheduleReply('bamServer', {
-      test: testTimingEventsRequest,
-      permanent: true,
-      statusCode: 429
-    })
-    let url = await browser.testHandle.assetURL('instrumented.html', {
-      init: {
-        page_view_timing: {
-          enabled: true,
-          initialHarvestSeconds: 2,
-          harvestTimeSeconds: 2
-        },
-        harvest: {
-          tooManyRequestsDelay: 5
+  ;[408, 429, 500, 503].forEach(statusCode => {
+    it('timings are retried when collector returns ' + statusCode, async () => {
+      await browser.testHandle.scheduleReply('bamServer', {
+        test: testTimingEventsRequest,
+        permanent: true,
+        statusCode
+      })
+      let url = await browser.testHandle.assetURL('instrumented.html', {
+        init: {
+          page_view_timing: {
+            enabled: true,
+            initialHarvestSeconds: 2,
+            harvestTimeSeconds: 2
+          },
+          harvest: {
+            tooManyRequestsDelay: 5
+          }
         }
-      }
+      })
+      const [firstTimingsHarvest] = await Promise.all([
+        browser.testHandle.expectTimings(),
+        browser.url(url).then(() => browser.waitForAgentLoad())
+      ])
+
+      await browser.testHandle.clearScheduledReplies('bamServer')
+      const secondTimingsHarvest = await browser.testHandle.expectTimings(10000)
+
+      expect(firstTimingsHarvest.reply.statusCode).toEqual(statusCode)
+      expect(secondTimingsHarvest.request.body).toEqual(expect.arrayContaining(firstTimingsHarvest.request.body))
     })
-    const [firstTimingsHarvest] = await Promise.all([
-      browser.testHandle.expectTimings(),
-      browser.url(url).then(() => browser.waitForAgentLoad())
-    ])
+  })
 
-    await browser.testHandle.clearScheduledReplies('bamServer')
-    const secondTimingsHarvest = await browser.testHandle.expectTimings(10000)
+  ;[400, 404, 502, 504, 512].forEach(statusCode => {
+    it('timings are NOT retried when collector returns ' + statusCode, async () => {
+      await browser.testHandle.scheduleReply('bamServer', {
+        test: testTimingEventsRequest,
+        permanent: true,
+        statusCode
+      })
+      let url = await browser.testHandle.assetURL('instrumented.html', {
+        init: {
+          page_view_timing: {
+            enabled: true,
+            initialHarvestSeconds: 2,
+            harvestTimeSeconds: 2
+          },
+          harvest: {
+            tooManyRequestsDelay: 5
+          }
+        }
+      })
+      const [firstTimingsHarvest] = await Promise.all([
+        browser.testHandle.expectTimings(),
+        browser.url(url).then(() => browser.waitForAgentLoad())
+      ])
 
-    expect(firstTimingsHarvest.reply.statusCode).toEqual(429)
-    expect(secondTimingsHarvest.request.body).toEqual(expect.arrayContaining(firstTimingsHarvest.request.body))
+      await browser.testHandle.clearScheduledReplies('bamServer')
+      const [secondTimingsHarvest] = await Promise.all([
+        browser.testHandle.expectTimings(),
+        $('body').click()
+          .then(() => browser.url(url).then(() => browser.waitForAgentLoad()))
+      ])
+
+      expect(secondTimingsHarvest.request.body).not.toEqual(expect.arrayContaining(firstTimingsHarvest.request.body))
+    })
   })
 })

bec022b57ffa2cb23353272b667727f631e76b63
Jordan Porter <insomniacrampage@hotmail.com>
2024-05-24 13:10:52 -0700

    fix: Prevent session reset triggers from harvesting session trace data (#1050)

--- tests/specs/session-trace/session-pages.e2e.js
+++ tests/specs/session-trace/session-pages.e2e.js
@@ -121,6 +121,76 @@ describe.withBrowsersMatching(notIE)('Session Replay Across Pages', () => {
     await browser.testHandle.expectTrace(10000, true)
   })
 
+  it('should not report harvest if sessionId changes', async () => {
+    await browser.testHandle.scheduleReply('bamServer', {
+      test: testRumRequest,
+      body: JSON.stringify({ st: 1, sts: 1, err: 1, ins: 1, spa: 1, sr: 0, loaded: 1 })
+    })
+    await browser.url(await browser.testHandle.assetURL('instrumented.html', stConfig()))
+      .then(() => browser.waitForAgentLoad())
+
+    const { request: page1Contents } = await browser.testHandle.expectTrace(10000)
+    const { localStorage: { value: session } } = await browser.getAgentSessionInfo()
+
+    testExpectedTrace({ data: page1Contents, session })
+
+    await Promise.all([
+      browser.testHandle.expectTrace(10000, true), // should not harvest again if the session id changes mid-lifecycle
+      browser.execute(function () {
+        Object.values(NREUM.initializedAgents)[0].runtime.session.state.value = 'session_id_changed'
+      })
+    ])
+  })
+
+  it('should not report harvest if session resets', async () => {
+    await browser.testHandle.scheduleReply('bamServer', {
+      test: testRumRequest,
+      body: JSON.stringify({ st: 1, sts: 1, err: 1, ins: 1, spa: 1, sr: 0, loaded: 1 })
+    })
+    await browser.url(await browser.testHandle.assetURL('instrumented.html', stConfig()))
+      .then(() => browser.waitForAgentLoad())
+
+    const { request: page1Contents } = await browser.testHandle.expectTrace(10000)
+    const { localStorage: { value: session } } = await browser.getAgentSessionInfo()
+
+    testExpectedTrace({ data: page1Contents, session })
+
+    await Promise.all([
+      browser.testHandle.expectTrace(10000, true), // should not harvest again if the session id changes mid-lifecycle
+      browser.execute(function () {
+        Object.values(NREUM.initializedAgents)[0].runtime.session.reset()
+      })
+    ])
+  })
+
+  // As of 06/26/2023 test fails in Safari, though tested behavior works in a live browser (revisit in NR-138940).
+  it.withBrowsersMatching([supportsMultipleTabs, notSafari])('should not report harvest if session resets on another page', async () => {
+    await browser.destroyAgentSession()
+    await browser.testHandle.scheduleReply('bamServer', {
+      test: testRumRequest,
+      body: JSON.stringify({ st: 1, sts: 1, err: 1, ins: 1, spa: 1, sr: 0, loaded: 1 })
+    })
+    let url = await browser.testHandle.assetURL('instrumented.html', stConfig())
+    await browser.url(url).then(() => browser.waitForAgentLoad())
+    await browser.testHandle.expectTrace(10000)
+    // got a payload, now open a new tab and reset the session in tab B
+    const newTab = await browser.createWindow('tab')
+    await browser.switchToWindow(newTab.handle)
+    await browser.testHandle.scheduleReply('bamServer', {
+      test: testRumRequest,
+      body: JSON.stringify({ st: 1, sts: 1, err: 1, ins: 1, spa: 1, sr: 0, loaded: 1 })
+    })
+    await browser.url(await browser.testHandle.assetURL('rrweb-instrumented.html', stConfig()))
+      .then(() => browser.waitForAgentLoad())
+
+    await browser.resetAgentSession()
+
+    // go back to tab A and see if we get another harvest
+    await browser.closeWindow()
+    await browser.switchToWindow((await browser.getWindowHandles())[0])
+    await browser.testHandle.expectTrace(10000, true)
+  })
+
   // As of 06/26/2023 test fails in Safari, though tested behavior works in a live browser (revisit in NR-138940).
   it.withBrowsersMatching([supportsMultipleTabs, notSafari])('catches mode transition from other pages in the session', async () => {
     await browser.destroyAgentSession()

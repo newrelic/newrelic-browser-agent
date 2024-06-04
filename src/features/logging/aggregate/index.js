c8dfa50c27dfd09b6ea1524f74d7af52da216eb7
Jordan Porter <silentsummitsoftware@gmail.com>
2024-06-04 10:31:49 -0600

    fix tests

--- src/features/logging/aggregate/index.js
+++ src/features/logging/aggregate/index.js
@@ -1,4 +1,4 @@
-import { getRuntime } from '../../../common/config/config'
+import { getInfo, getRuntime } from '../../../common/config/config'
 import { registerHandler } from '../../../common/event-emitter/register-handler'
 import { AggregateBase } from '../../utils/aggregate-base'
 import { FEATURE_NAME, LOGGING_EVENT_EMITTER_TYPES } from '../constants'
@@ -6,11 +6,18 @@ import { Log } from '../shared/log'
 
 export class Aggregate extends AggregateBase {
   static featureName = FEATURE_NAME
+  #agentRuntime
+  #agentInfo
   constructor (agentIdentifier, aggregator) {
     super(agentIdentifier, aggregator, FEATURE_NAME)
 
-    this.logs = []
-    this.agentRuntime = getRuntime(this.agentIdentifier)
+    /** held logs before sending */
+    this.bufferedLogs = []
+    /** held logs during sending, for retries */
+    this.outgoingLogs = []
+
+    this.#agentRuntime = getRuntime(this.agentIdentifier)
+    this.#agentInfo = getInfo(this.agentIdentifier)
 
     this.waitForFlags([]).then(() => {
       /** emitted by instrument class (wrapped loggers) or the api methods directly */
@@ -20,13 +27,37 @@ export class Aggregate extends AggregateBase {
   }
 
   handleLog (timestamp, message, attributes, level) {
-    this.logs.push(
+    this.bufferedLogs.push(
       new Log(
-        this.agentRuntime.timeKeeper.convertRelativeTimestamp(timestamp),
+        this.#agentRuntime.timeKeeper.convertRelativeTimestamp(timestamp),
         message,
         attributes,
         level
       )
     )
+  }
+
+  preparePayload () {
+    this.outgoingLogs.push(...this.bufferedLogs.splice(0))
+    return {
+      common: {
+        attributes: {
+          entityGuid: (this.#agentRuntime.appMetadata?.agents?.[0] || {}).entityGuid,
+          session: {
+            id: this.#agentRuntime?.session?.state.value || '0', // The session ID that we generate and keep across page loads
+            hasReplay: this.#agentRuntime?.session?.state.sessionReplayMode === 1, // True if a session replay recording is running
+            hasTrace: this.#agentRuntime?.session?.state.sessionTraceMode === 1, // True if a session trace recording is running
+            pageTraceId: this.#agentRuntime.ptid // The trace ID if a session trace is recording
+          },
+          agent: {
+            appId: this.#agentInfo.applicationID, // Application ID from info object
+            standalone: this.#agentInfo.sa, // Whether the app is C+P or APM injected
+            version: this.#agentRuntime.version, // the browser agent version
+            distribution: this.#agentRuntime.distMethod // How is the agent being loaded on the page
+          }
+        }
+      },
+      logs: this.outgoingLogs
+    }
   }
 }
---
+++ src/features/logging/index.js
@@ -0,0 +1,1 @@
+export { Instrument as Logging } from './instrument/index'

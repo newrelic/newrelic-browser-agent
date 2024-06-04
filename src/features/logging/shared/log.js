df3a85e9cddf27f2cd62a5f6c3221f353619c34b
Jordan Porter <silentsummitsoftware@gmail.com>
2024-06-03 22:44:11 -0600

    more comments

--- src/features/logging/shared/log.js
+++ src/features/logging/shared/log.js
@@ -2,7 +2,15 @@ import { globalScope } from '../../../common/constants/runtime'
 import { cleanURL } from '../../../common/url/clean-url'
 
 export class Log {
+  /**
+   *
+   * @param {number} timestamp - Unix timestamp
+   * @param {string} message - message string
+   * @param {{[key: string]: *}} attributes - other attributes
+   * @param {enum} level - Log level
+   */
   constructor (timestamp, message, attributes, level) {
+    /** @type {long} */
     this.timestamp = timestamp
     this.message = message
     this.attributes = attributes

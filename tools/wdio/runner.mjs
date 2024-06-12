47b1a9c120133fdfc5e69dfbdea5ae9843978c0c
Chunwai Li <cli@newrelic.com>
2024-06-12 14:25:30 -0500

    Revert "debugging REVERT THIS LATER"
    This reverts commit 06ae5f1a2caf930c3b0e19f1f0c8f60b2c1324fe.

--- tools/wdio/runner.mjs
+++ tools/wdio/runner.mjs
@@ -25,7 +25,6 @@ const wdioConfig = deepmerge(
   specsConfig(),
   args.lt ? lambdaTestConfig() : sauceConfig()
 )
-console.log(wdioConfig)
 const configFilePath = path.join(
   path.resolve(__dirname, '../../node_modules/.cache/wdio'),
   `wdio.conf_${crypto.randomBytes(16).toString('hex')}.mjs`

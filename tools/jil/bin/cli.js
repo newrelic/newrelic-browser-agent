ae900826062478808b52267b0737ffab4e0e9b17
Patrick Housley <phousley@newrelic.com>
2024-06-20 12:33:55 -0500

    removing jil

--- tools/jil/bin/cli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-/*
- * Copyright 2020 New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-require('../runner')
--- tools/jil/index.js
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2020 New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-const runner = require('./runner')
-module.exports = runner

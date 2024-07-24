c3ea9b91197473833e72a57d554e97e0b1e828fd
Jordan Porter <silentsummitsoftware@gmail.com>
2024-07-23 20:26:07 -0600

    add generic events to test builds

--- tools/test-builds/browser-agent-wrapper/src/custom-agent-spa.js
+++ tools/test-builds/browser-agent-wrapper/src/custom-agent-spa.js
@@ -1,23 +1,29 @@
 import { Agent } from '@newrelic/browser-agent/loaders/agent'
 import { Ajax } from '@newrelic/browser-agent/features/ajax'
+import { GenericEvents } from '@newrelic/browser-agent/features/generic_events'
 import { JSErrors } from '@newrelic/browser-agent/features/jserrors'
 import { Metrics } from '@newrelic/browser-agent/features/metrics'
 import { PageAction } from '@newrelic/browser-agent/features/page_action'
 import { PageViewEvent } from '@newrelic/browser-agent/features/page_view_event'
 import { PageViewTiming } from '@newrelic/browser-agent/features/page_view_timing'
 import { SessionTrace } from '@newrelic/browser-agent/features/session_trace'
+import { SessionReplay } from '@newrelic/browser-agent/features/session_replay'
 import { Spa } from '@newrelic/browser-agent/features/spa'
+import { SoftNav } from '@newrelic/browser-agent/features/soft_navigations'
 
 window.agent = new Agent({
   features: [
     Ajax,
+    GenericEvents,
     JSErrors,
     Metrics,
     PageAction,
     PageViewEvent,
     PageViewTiming,
     SessionTrace,
-    Spa
+    SessionReplay,
+    Spa,
+    SoftNav
   ],
-  loaderType: 'spa'
+  loaderType: 'pro'
 })

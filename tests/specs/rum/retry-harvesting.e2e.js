27a6677d711ec549e75c293ee6ffb2d25f7af579
Patrick Housley <phousley@newrelic.com>
2024-08-12 16:13:39 -0500

    chore: Migrating rum specs to use network capture

--- tests/specs/rum/retry-harvesting.e2e.js
+++ tests/specs/rum/retry-harvesting.e2e.js
@@ -1,7 +1,35 @@
-import { testRumRequest } from '../../../tools/testing-server/utils/expect-tests'
+import { testAjaxEventsRequest, testAjaxTimeSlicesRequest, testBlobReplayRequest, testBlobTraceRequest, testErrorsRequest, testInsRequest, testInteractionEventsRequest, testRumRequest, testTimingEventsRequest } from '../../../tools/testing-server/utils/expect-tests'
 
 describe('rum retry harvesting', () => {
-  [400, 404, 408, 429, 500, 502, 503, 504, 512].forEach(statusCode => {
+  let rumCapture
+  let timingEventsCapture
+  let ajaxEventsCapture
+  let ajaxMetricsCapture
+  let traceCapture
+  let insightsCapture
+  let interactionEventsCapture
+  let errorMetricsCapture
+  let replaysCapture
+
+  beforeEach(async () => {
+    [rumCapture, timingEventsCapture, ajaxEventsCapture, ajaxMetricsCapture, traceCapture, insightsCapture, interactionEventsCapture, errorMetricsCapture, replaysCapture] = await browser.testHandle.createNetworkCaptures('bamServer', [
+      { test: testRumRequest },
+      { test: testTimingEventsRequest },
+      { test: testAjaxEventsRequest },
+      { test: testAjaxTimeSlicesRequest },
+      { test: testBlobTraceRequest },
+      { test: testInsRequest },
+      { test: testInteractionEventsRequest },
+      { test: testErrorsRequest },
+      { test: testBlobReplayRequest }
+    ])
+  })
+
+  afterEach(async () => {
+    await browser.destroyAgentSession(browser.testHandle)
+  })
+
+  ;[400, 404, 408, 429, 500, 502, 503, 504, 512].forEach(statusCode => {
     it(`should not retry rum and should not continue harvesting when request statusCode is ${statusCode}`, async () => {
       await browser.testHandle.scheduleReply('bamServer', {
         test: testRumRequest,
@@ -10,52 +38,89 @@ describe('rum retry harvesting', () => {
         body: ''
       })
 
-      const allPayloads = Promise.all([
-        browser.testHandle.expectTrace(10000, true),
-        browser.testHandle.expectInteractionEvents(10000, true),
-        browser.testHandle.expectTimings(10000, true),
-        browser.testHandle.expectAjaxTimeSlices(10000, true),
-        browser.testHandle.expectAjaxEvents(10000, true),
-        browser.testHandle.expectIns(10000, true),
-        browser.testHandle.expectRum()
+      let [
+        rumHarvests,
+        timingEventsHarvests,
+        ajaxEventsHarvests,
+        ajaxMetricsHarvests,
+        insightsHarvests,
+        interactionEventsHarvests,
+        traceHarvests,
+        errorMetricsHarvests,
+        replaysHarvests
+      ] = await Promise.all([
+        rumCapture.waitForResult({ timeout: 10000 }),
+        timingEventsCapture.waitForResult({ timeout: 10000 }),
+        ajaxEventsCapture.waitForResult({ timeout: 10000 }),
+        ajaxMetricsCapture.waitForResult({ timeout: 10000 }),
+        insightsCapture.waitForResult({ timeout: 10000 }),
+        interactionEventsCapture.waitForResult({ timeout: 10000 }),
+        traceCapture.waitForResult({ timeout: 10000 }),
+        errorMetricsCapture.waitForResult({ timeout: 10000 }),
+        replaysCapture.waitForResult({ timeout: 10000 }),
+        browser.url(await browser.testHandle.assetURL('obfuscate-pii.html'))
+          .then(() => $('a').click())
+          .then(() => browser.execute(function () {
+            newrelic.noticeError(new Error('hippo hangry'))
+            newrelic.addPageAction('DummyEvent', { free: 'tacos' })
+          }))
       ])
-      await browser.pause(500)
-      await browser.url(await browser.testHandle.assetURL('obfuscate-pii.html')).then(() => $('a').click())
-      const [
-        resourcesResults,
-        interactionResults,
-        timingsResults,
-        ajaxSliceResults,
-        ajaxEventsResults,
-        insResults,
-        rumResults
-      ] = await allPayloads
 
-      // Uncomment this code to reproduce the issue described in https://issues.newrelic.com/browse/NEWRELIC-9348
-      // Leave uncommented once that ticket is worked
-      // await browser.pause(500)
+      if (statusCode === 408) {
+        // Browsers automatically retry requests with status code 408
+        expect(rumHarvests.length).toBeLessThan(5)
+      } else {
+        expect(rumHarvests.length).toEqual(1)
+      }
+      expect(rumHarvests[0].reply.statusCode).toEqual(statusCode)
 
-      // await browser.execute(function () {
-      //   newrelic.noticeError(new Error('hippo hangry'))
-      //   newrelic.addPageAction('DummyEvent', { free: 'tacos' })
-      // })
+      expect(timingEventsHarvests.length).toEqual(0)
+      expect(ajaxEventsHarvests.length).toEqual(0)
+      expect(ajaxMetricsHarvests.length).toEqual(0)
+      expect(insightsHarvests.length).toEqual(0)
+      expect(interactionEventsHarvests.length).toEqual(0)
+      expect(traceHarvests.length).toEqual(0)
+      expect(errorMetricsHarvests.length).toEqual(0)
+      expect(replaysHarvests.length).toEqual(0)
 
-      // await Promise.all([
-      //   browser.testHandle.expectTimings(10000, true),
-      //   browser.testHandle.expectAjaxEvents(10000, true),
-      //   browser.testHandle.expectMetrics(10000, true),
-      //   browser.testHandle.expectErrors(10000, true),
-      //   browser.testHandle.expectTrace(10000, true),
-      //   browser.url(await browser.testHandle.assetURL('/'))
-      // ])
+      ;[
+        rumHarvests,
+        timingEventsHarvests,
+        ajaxEventsHarvests,
+        ajaxMetricsHarvests,
+        insightsHarvests,
+        interactionEventsHarvests,
+        traceHarvests,
+        errorMetricsHarvests,
+        replaysHarvests
+      ] = await Promise.all([
+        rumCapture.waitForResult({ timeout: 10000 }),
+        timingEventsCapture.waitForResult({ timeout: 10000 }),
+        ajaxEventsCapture.waitForResult({ timeout: 10000 }),
+        ajaxMetricsCapture.waitForResult({ timeout: 10000 }),
+        insightsCapture.waitForResult({ timeout: 10000 }),
+        interactionEventsCapture.waitForResult({ timeout: 10000 }),
+        traceCapture.waitForResult({ timeout: 10000 }),
+        errorMetricsCapture.waitForResult({ timeout: 10000 }),
+        replaysCapture.waitForResult({ timeout: 10000 }),
+        browser.url(await browser.testHandle.assetURL('/'))
+      ])
 
-      expect(rumResults.reply.statusCode).toEqual(statusCode)
-      expect(resourcesResults).toBeUndefined()
-      expect(interactionResults).toBeUndefined()
-      expect(timingsResults).toBeUndefined()
-      expect(ajaxSliceResults).toBeUndefined()
-      expect(ajaxEventsResults).toBeUndefined()
-      expect(insResults).toBeUndefined()
+      if (statusCode === 408) {
+        // Browsers automatically retry requests with status code 408
+        expect(rumHarvests.length).toBeLessThan(5)
+      } else {
+        expect(rumHarvests.length).toEqual(1)
+      }
+
+      expect(timingEventsHarvests.length).toEqual(0)
+      expect(ajaxEventsHarvests.length).toEqual(0)
+      expect(ajaxMetricsHarvests.length).toEqual(0)
+      expect(insightsHarvests.length).toEqual(0)
+      expect(interactionEventsHarvests.length).toEqual(0)
+      expect(traceHarvests.length).toEqual(0)
+      expect(errorMetricsHarvests.length).toEqual(0)
+      expect(replaysHarvests.length).toEqual(0)
     })
   })
 })

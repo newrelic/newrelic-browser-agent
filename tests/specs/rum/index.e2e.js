d6d35360ef1da4d31fe664fc37341a07e0c39b81
Patrick Housley <patrickhousley@users.noreply.github.com>
2024-08-13 11:29:47 -0500

    chore: Migrating rum specs to use network capture (#1144)

--- tests/specs/rum/index.e2e.js
+++ tests/specs/rum/index.e2e.js
@@ -1,5 +1,5 @@
 import { checkRumBody, checkRumPerf, checkRumQuery } from '../../util/basic-checks'
-import { testAssetRequest } from '../../../tools/testing-server/utils/expect-tests'
+import { testAssetRequest, testRumRequest } from '../../../tools/testing-server/utils/expect-tests'
 import { detailedCheckRum } from '../../util/detailed-checks'
 import { supportsFirstContentfulPaint, supportsFirstPaint } from '../../../tools/browser-matcher/common-matchers.mjs'
 
@@ -17,67 +17,75 @@ const loader_config = {
 }
 
 describe('basic pve capturing', () => {
-  ['same-origin', 'cross-origin'].forEach(page => {
-    it(`should send rum when ${page} page loads in an iframe`, async () => {
-      const rumPromise = browser.testHandle.expectRum()
-      await browser.pause(1000)
-      await browser.url(await browser.testHandle.assetURL(`iframe/${page}.html`))
-      const rumResults = await rumPromise
+  let rumCapture
 
-      checkRumQuery(rumResults.request)
-      checkRumBody(rumResults.request)
+  beforeEach(async () => {
+    rumCapture = await browser.testHandle.createNetworkCaptures('bamServer', { test: testRumRequest })
+  })
+
+  ;['same-origin', 'cross-origin'].forEach(page => {
+    it(`should send rum when ${page} page loads in an iframe`, async () => {
+      const [[rumHarvest]] = await Promise.all([
+        rumCapture.waitForResult({ totalCount: 1 }),
+        browser.url(await browser.testHandle.assetURL(`iframe/${page}.html`))
+      ])
+
+      checkRumQuery(rumHarvest.request)
+      checkRumBody(rumHarvest.request)
     })
   })
 
   it('should capture page load timings', async () => {
-    const rumPromise = browser.testHandle.expectRum()
     await browser.testHandle.scheduleReply('assetServer', {
       test: testAssetRequest,
       permanent: false,
       delay: 500
     })
-    await browser.url(await browser.testHandle.assetURL('64kb-dom.html'))
-    const rumResults = await rumPromise
 
-    checkRumQuery(rumResults.request)
-    checkRumBody(rumResults.request)
-    expect(parseInt(rumResults.request.query.be, 10)).toBeGreaterThanOrEqual(500)
-    expect(parseInt(rumResults.request.query.fe, 10)).toBeGreaterThanOrEqual(100)
-    expect(parseInt(rumResults.request.query.dc, 10)).toBeGreaterThanOrEqual(100)
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('64kb-dom.html'))
+    ])
+
+    checkRumQuery(rumHarvest.request)
+    checkRumBody(rumHarvest.request)
+    expect(parseInt(rumHarvest.request.query.be, 10)).toBeGreaterThanOrEqual(500)
+    expect(parseInt(rumHarvest.request.query.fe, 10)).toBeGreaterThanOrEqual(100)
+    expect(parseInt(rumHarvest.request.query.dc, 10)).toBeGreaterThanOrEqual(100)
   })
 
   /** equivalent to former no-body.test.js */
   it('reports RUM with no body', async () => {
-    const rumPromise = browser.testHandle.expectRum()
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('no-body.html', { config: { account: loader_config.account } }))
-    const rumResults = await rumPromise
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('no-body.html', { config: { account: loader_config.account } }))
+    ])
 
-    checkRumQuery(rumResults.request)
-    detailedCheckRum(rumResults.request, { query: { ac: 'test_account' }, body: { ja: { no: 'body' } } })
+    checkRumQuery(rumHarvest.request)
+    detailedCheckRum(rumHarvest.request, { query: { ac: 'test_account' }, body: { ja: { no: 'body' } } })
   })
 
   /** equivalent to former paint-timing.test.js */
   it.withBrowsersMatching([supportsFirstPaint, supportsFirstContentfulPaint])('reports paint timings', async () => {
-    const rumPromise = browser.testHandle.expectRum()
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { account: loader_config.account } }))
-    const rumResults = await rumPromise
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { account: loader_config.account } }))
+    ])
 
-    expect(Number(rumResults.request.query.fp)).toBeGreaterThan(0)
-    expect(Number(rumResults.request.query.fcp)).toBeGreaterThan(0)
+    expect(Number(rumHarvest.request.query.fp)).toBeGreaterThan(0)
+    expect(Number(rumHarvest.request.query.fcp)).toBeGreaterThan(0)
   })
 
   /** equivalent to former unconfigured-on-load.test.js */
   it('should not receive RUM call if not configured', async () => {
-    const rumPromise = browser.testHandle.expectRum(10000, true)
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('unconfigured-on-load.html'))
-    await rumPromise
-  })
-})
+    const [rumHarvests] = await Promise.all([
+      rumCapture.waitForResult({ timeout: 10000 }),
+      browser.url(await browser.testHandle.assetURL('unconfigured-on-load.html'))
+    ])
 
-describe('APM Decorations', () => {
+    expect(rumHarvests.length).toEqual(0)
+  })
+
   // equivalent to former data.test.js
   it('should capture detailed APM decorations', async () => {
     const expected = {
@@ -95,46 +103,48 @@ describe('APM Decorations', () => {
       },
       body: { ja: { foo: 'bar' } }
     }
-    const rumPromise = browser.testHandle.expectRum()
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('rum-data.html', { config: loader_config }))
-    const rumResults = await rumPromise
-    detailedCheckRum(rumResults.request, expected) // equivalent to former data.test.js
-    checkRumPerf(rumResults.request) // equivalent to former nav-timing.test.js, former perf.test.js
-    expect(+rumResults.request.query.dc).toBeGreaterThanOrEqual(0)
-    expect(+rumResults.request.query.fe).toBeGreaterThanOrEqual(+rumResults.request.query.dc)
+
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('rum-data.html', { config: loader_config }))
+    ])
+
+    detailedCheckRum(rumHarvest.request, expected) // equivalent to former data.test.js
+    checkRumPerf(rumHarvest.request) // equivalent to former nav-timing.test.js, former perf.test.js
+    expect(+rumHarvest.request.query.dc).toBeGreaterThanOrEqual(0)
+    expect(+rumHarvest.request.query.fe).toBeGreaterThanOrEqual(+rumHarvest.request.query.dc)
   })
 
   /** equivalent to former transaction-name.test.js */
   it('should report a transactionName without a tNamePlain', async () => {
-    const rumPromise = browser.testHandle.expectRum()
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { transactionName: 'abc' } }))
-    const rumResults = await rumPromise
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { transactionName: 'abc' } }))
+    ])
 
-    expect(rumResults.request.query.to).toEqual('abc') // has correct obfuscated transactionName
-    expect(rumResults.request.query.t).toBeUndefined() // tNamePlain excluded
+    expect(rumHarvest.request.query.to).toEqual('abc') // has correct obfuscated transactionName
+    expect(rumHarvest.request.query.t).toBeUndefined() // tNamePlain excluded
   })
 
   /** equivalent to former transaction-name.test.js */
   it('should report a tNamePlain without a transactionName', async () => {
-    const rumPromise = browser.testHandle.expectRum()
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { tNamePlain: 'abc' } }))
-    const rumResults = await rumPromise
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { tNamePlain: 'abc' } }))
+    ])
 
-    expect(rumResults.request.query.t).toEqual('abc') // has correct tNamePlain
-    expect(rumResults.request.query.to).toBeUndefined() // transactionName excluded
+    expect(rumHarvest.request.query.t).toEqual('abc') // has correct tNamePlain
+    expect(rumHarvest.request.query.to).toBeUndefined() // transactionName excluded
   })
 
   /** equivalent to former transaction-name.test.js */
   it('should honor transactionName if both tNamePlain and transactionName are supplied', async () => {
-    const rumPromise = browser.testHandle.expectRum()
-    await browser.pause(500)
-    await browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { transactionName: 'abc', tNamePlain: 'def' } }))
-    const rumResults = await rumPromise
+    const [[rumHarvest]] = await Promise.all([
+      rumCapture.waitForResult({ totalCount: 1 }),
+      browser.url(await browser.testHandle.assetURL('instrumented.html', { config: { transactionName: 'abc', tNamePlain: 'def' } }))
+    ])
 
-    expect(rumResults.request.query.to).toEqual('abc') // should honor obfuscated if both are defined
-    expect(rumResults.request.query.t).toBeUndefined() // tNamePlain excluded
+    expect(rumHarvest.request.query.to).toEqual('abc') // should honor obfuscated if both are defined
+    expect(rumHarvest.request.query.t).toBeUndefined() // tNamePlain excluded
   })
 })

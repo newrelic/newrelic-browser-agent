1b2448498cf285f36530f2107cb0403401958b1f
Jordan Porter <insomniacrampage@hotmail.com>
2024-10-11 08:46:53 -0600

    feat: Include logging feature in micro agent loader (#1210)

--- tests/specs/npm/micro-agent.e2e.js
+++ tests/specs/npm/micro-agent.e2e.js
@@ -1,13 +1,14 @@
 /* globals MicroAgent */
 
-import { testErrorsRequest, testInsRequest, testRumRequest } from '../../../tools/testing-server/utils/expect-tests'
+import { testErrorsRequest, testInsRequest, testLogsRequest, testRumRequest } from '../../../tools/testing-server/utils/expect-tests'
 
 describe('micro-agent', () => {
   it('Smoke Test - Can send distinct payloads of all relevant data types to 2 distinct app IDs', async () => {
-    const [rumCapture, errorsCapture, insightsCapture] = await browser.testHandle.createNetworkCaptures('bamServer', [
+    const [rumCapture, errorsCapture, insightsCapture, logsCapture] = await browser.testHandle.createNetworkCaptures('bamServer', [
       { test: testRumRequest },
       { test: testErrorsRequest },
-      { test: testInsRequest }
+      { test: testInsRequest },
+      { test: testLogsRequest }
     ])
     await browser.url(await browser.testHandle.assetURL('test-builds/browser-agent-wrapper/micro-agent.html'))
 
@@ -30,19 +31,24 @@ describe('micro-agent', () => {
       // each payload in this test is decorated with data that matches its appId for ease of testing
       window.agent1.addPageAction('1', { val: 1 })
       window.agent2.addPageAction('2', { val: 2 })
+
+      // each payload in this test is decorated with data that matches its appId for ease of testing
+      window.agent1.log('1')
+      window.agent2.log('2')
     })
-    const [rumHarvests, errorsHarvests, insightsHarvests] = await Promise.all([
+    const [rumHarvests, errorsHarvests, insightsHarvests, logsHarvest] = await Promise.all([
       rumCapture.waitForResult({ totalCount: 2 }),
       errorsCapture.waitForResult({ totalCount: 2 }),
-      insightsCapture.waitForResult({ totalCount: 2 })
+      insightsCapture.waitForResult({ totalCount: 2 }),
+      logsCapture.waitForResult({ totalCount: 2 })
     ])
 
     // these props will get set to true once a test has matched it
     // if it gets tried again, the test will fail, since these should all
     // only have one distinct matching payload
     const tests = {
-      1: { rum: false, err: false, pa: false },
-      2: { rum: false, err: false, pa: false }
+      1: { rum: false, err: false, pa: false, log: false },
+      2: { rum: false, err: false, pa: false, log: false }
     }
 
     // each type of test should check that:
@@ -62,6 +68,12 @@ describe('micro-agent', () => {
       expect(ranOnce(query.a, 'pa')).toEqual(true)
       const data = body.ins[0]
       expect(payloadMatchesAppId(query.a, data.val, data.actionName, data.customAttr)).toEqual(true)
+    })
+
+    logsHarvest.forEach(({ request: { query, body } }) => {
+      const data = JSON.parse(body)[0]
+      expect(ranOnce(data.common.attributes.appId, 'log')).toEqual(true)
+      expect(payloadMatchesAppId(data.common.attributes.appId, data.logs[0].message)).toEqual(true)
     })
 
     function payloadMatchesAppId (appId, ...props) {

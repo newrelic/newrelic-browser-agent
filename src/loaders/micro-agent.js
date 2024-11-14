1ede50779ab56a3bbf6de29f900439bdb5636475
Chunwai Li <cli@newrelic.com>
2024-11-14 13:14:06 -0600

    Fix session missing when initializing multiple microagents

--- src/loaders/micro-agent.js
+++ src/loaders/micro-agent.js
@@ -34,6 +34,47 @@ export class MicroAgent extends MicroAgentBase {
     configure(this, { ...options, runtime: { isolatedBacklog: true } }, options.loaderType || 'micro-agent')
     Object.assign(this, this.api) // the APIs should be available at the class level for micro-agent
 
+    /**
+     * Starts a set of agent features if not running in "autoStart" mode
+     * {@link https://docs.newrelic.com/docs/browser/new-relic-browser/browser-apis/start/}
+     * @param {string|string[]} [featureNames] The feature name(s) to start.  If no name(s) are passed, all features will be started
+     */
+    this.start = (featureNames) => {
+      try {
+        if (featureNames === undefined || (Array.isArray(featureNames) && featureNames.length === 0)) featureNames = nonAutoFeatures
+        else if (typeof featureNames === 'string') featureNames = [featureNames]
+
+        if (featureNames.some(f => !nonAutoFeatures.includes(f))) warn(37, nonAutoFeatures)
+        const enabledFeatures = getEnabledFeatures(this.agentIdentifier)
+
+        try {
+          // a biproduct of doing this is that the "session manager" is automatically handled through importing this feature
+          this.features.page_view_event = new PVE(this)
+        } catch (err) {
+          warn(24, err)
+        }
+
+        this.features.page_view_event.onAggregateImported.then(() => {
+          /* The following features do not import an "instrument" file, meaning they are only hooked up to the API.
+          Since the missing instrument-base class handles drain-gating (racing behavior) and PVE handles some setup, these are chained until after PVE has finished initializing
+          so as to avoid the race condition of things like session and sharedAggregator not being ready by features that uses them right away. */
+          nonAutoFeatures.forEach(f => {
+            if (enabledFeatures[f] && featureNames.includes(f)) {
+              import(/* webpackChunkName: "lazy-feature-loader" */ '../features/utils/lazy-feature-loader').then(({ lazyFeatureLoader }) => {
+                return lazyFeatureLoader(f, 'aggregate')
+              }).then(({ Aggregate }) => {
+                this.features[f] = new Aggregate(this)
+              }).catch(err => warn(25, err))
+            }
+          })
+        })
+        return true
+      } catch (err) {
+        warn(26, err)
+        return false
+      }
+    }
+
     this.start(nonAutoFeatures.filter(featureName => !!this.init[featureName].autoStart))
   }
 
@@ -43,47 +84,6 @@ export class MicroAgent extends MicroAgentBase {
       init: this.init,
       loader_config: this.loader_config,
       runtime: this.runtime
-    }
-  }
-
-  /**
-   * Starts a set of agent features if not running in "autoStart" mode
-   * {@link https://docs.newrelic.com/docs/browser/new-relic-browser/browser-apis/start/}
-   * @param {string|string[]} [featureNames] The feature name(s) to start.  If no name(s) are passed, all features will be started
-   */
-  start (featureNames) {
-    try {
-      if (featureNames === undefined || (Array.isArray(featureNames) && featureNames.length === 0)) featureNames = nonAutoFeatures
-      else if (typeof featureNames === 'string') featureNames = [featureNames]
-
-      if (featureNames.some(f => !nonAutoFeatures.includes(f))) warn(37, nonAutoFeatures)
-      const enabledFeatures = getEnabledFeatures(this.agentIdentifier)
-
-      try {
-        // a biproduct of doing this is that the "session manager" is automatically handled through importing this feature
-        this.features.page_view_event = new PVE(this)
-      } catch (err) {
-        warn(24, err)
-      }
-
-      this.features.page_view_event.onAggregateImported.then(() => {
-        /* The following features do not import an "instrument" file, meaning they are only hooked up to the API.
-        Since the missing instrument-base class handles drain-gating (racing behavior) and PVE handles some setup, these are chained until after PVE has finished initializing
-        so as to avoid the race condition of things like session and sharedAggregator not being ready by features that uses them right away. */
-        nonAutoFeatures.forEach(f => {
-          if (enabledFeatures[f] && featureNames.includes(f)) {
-            import(/* webpackChunkName: "lazy-feature-loader" */ '../features/utils/lazy-feature-loader').then(({ lazyFeatureLoader }) => {
-              return lazyFeatureLoader(f, 'aggregate')
-            }).then(({ Aggregate }) => {
-              this.features[f] = new Aggregate(this)
-            }).catch(err => warn(25, err))
-          }
-        })
-      })
-      return true
-    } catch (err) {
-      warn(26, err)
-      return false
     }
   }
 }

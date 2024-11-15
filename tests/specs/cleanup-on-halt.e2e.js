4202e32d0140d52f506a79b2a13b1ba2d5eeeb70
Pik Tang <ptang@newrelic.com>
2024-11-15 10:53:06 -0800

    chore: Check logging for memory leak

--- tests/specs/cleanup-on-halt.e2e.js
+++ tests/specs/cleanup-on-halt.e2e.js
@@ -5,7 +5,7 @@ describe('Memory leaks', () => {
   it('does not occur on ee backlog when RUM flags are 0', async () => {
     await browser.testHandle.scheduleReply('bamServer', {
       test: testRumRequest,
-      body: JSON.stringify(rumFlags({ st: 0, sr: 0, err: 0, ins: 0, spa: 0, loaded: 1 }))
+      body: JSON.stringify(rumFlags({ st: 0, sr: 0, err: 0, ins: 0, spa: 0, log: 0, loaded: 1 }))
     })
     // This test relies on features to call deregisterDrain when not enabled by flags which in turn should clear their backlogs.
 
@@ -22,6 +22,7 @@ describe('Memory leaks', () => {
     expect(backlog).toEqual(expect.objectContaining({
       ajax: 0, // ajax does not rely on any flags anyways so it's always drained
       jserrors: 0,
+      logging: 0,
       metrics: 0,
       generic_events: 0,
       page_view_event: 0, // no handler

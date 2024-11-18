e151e88f4390ea8fa608c63a89bdcb97cf0590ce
Grady Ellison <gellison@newrelic.com>
2024-11-18 13:34:06 -0800

    chore: Port tests for soft nav from old spa tests

---
+++ tests/specs/soft_navigations/retry-harvesting.e2e.js
@@ -0,0 +1,70 @@
+import { testInteractionEventsRequest } from '../../../tools/testing-server/utils/expect-tests'
+
+describe('retry harvesting', () => {
+  const config = { loader: 'spa', init: { feature_flags: ['soft_nav'] } }
+  let interactionsCapture
+
+  beforeEach(async () => {
+    interactionsCapture = await browser.testHandle.createNetworkCaptures('bamServer', { test: testInteractionEventsRequest })
+  })
+
+  ;[408, 429, 500, 503].forEach(statusCode =>
+    it(`should send the interaction on the next harvest when the first harvest statusCode is ${statusCode}`, async () => {
+      await browser.testHandle.scheduleReply('bamServer', {
+        test: testInteractionEventsRequest,
+        permanent: true,
+        statusCode
+      })
+
+      const [firstInteractionEventHarvest] = await Promise.all([
+        interactionsCapture.waitForResult({ totalCount: 1 }),
+        browser.url(await browser.testHandle.assetURL('instrumented.html', config))
+          .then(() => browser.waitForAgentLoad())
+      ])
+
+      // Pause a bit for browsers built-in automated retry logic crap
+      await browser.pause(500)
+      await browser.testHandle.clearScheduledReplies('bamServer')
+
+      const [secondInteractionEventHarvest] = await Promise.all([
+        interactionsCapture.waitForResult({ totalCount: 2 }),
+        await browser.execute(function () {
+          newrelic.interaction().setName('interaction1').save().end()
+        })
+      ])
+
+      expect(firstInteractionEventHarvest[0].reply.statusCode).toEqual(statusCode)
+      expect(secondInteractionEventHarvest[1].request.body).toEqual(expect.arrayContaining(firstInteractionEventHarvest[0].request.body))
+    })
+  )
+
+  ;[400, 404, 502, 504, 512].forEach(statusCode =>
+    it(`should not send the page action on the next harvest when the first harvest statusCode is ${statusCode}`, async () => {
+      await browser.testHandle.scheduleReply('bamServer', {
+        test: testInteractionEventsRequest,
+        permanent: true,
+        statusCode
+      })
+
+      const [firstInteractionEventHarvest] = await Promise.all([
+        interactionsCapture.waitForResult({ totalCount: 1 }),
+        browser.url(await browser.testHandle.assetURL('instrumented.html', config))
+          .then(() => browser.waitForAgentLoad())
+      ])
+
+      // Pause a bit for browsers built-in automated retry logic crap
+      await browser.pause(500)
+      await browser.testHandle.clearScheduledReplies('bamServer')
+
+      const [secondInteractionEventHarvest] = await Promise.all([
+        interactionsCapture.waitForResult({ totalCount: 2 }),
+        await browser.execute(function () {
+          newrelic.interaction().setName('interaction1').save().end()
+        })
+      ])
+
+      expect(firstInteractionEventHarvest[0].reply.statusCode).toEqual(statusCode)
+      expect(secondInteractionEventHarvest[1].request.body).not.toEqual(expect.arrayContaining(firstInteractionEventHarvest[0].request.body))
+    })
+  )
+})

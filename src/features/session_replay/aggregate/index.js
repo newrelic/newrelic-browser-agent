dffc37452e4dce5acf9852107a71253ce38d4d0c
Pik Tang <ptang@newrelic.com>
2024-11-25 15:20:08 -0800

    chore: Cleanup extra arg

--- src/features/session_replay/aggregate/index.js
+++ src/features/session_replay/aggregate/index.js
@@ -147,16 +147,14 @@ export class Aggregate extends AggregateBase {
       this.scheduler.startTimer(this.harvestTimeSeconds)
       this.syncWithSessionManager({ sessionReplayMode: this.mode })
     } else {
-      this.initializeRecording(false, true, true)
+      this.initializeRecording(false, true)
     }
   }
 
   /**
-   * Evaluate entitlements and sampling before starting feature mechanics, importing and configuring recording library, and setting storage state
-   * @param {boolean} entitlements - the true/false state of the "sr" flag from RUM response
-   * @param {boolean} errorSample - the true/false state of the error sampling decision
-   * @param {boolean} fullSample - the true/false state of the full sampling decision
-   * @param {boolean} ignoreSession - whether to force the method to ignore the session state and use just the sample flags
+   * Evaluate entitlements (which already accounts for sampling) before starting feature mechanics, importing and configuring recording library, and setting storage state
+   * @param {boolean} srMode - the true/false state of the "sr" flag from RUM response
+   * @param {boolean} ignoreSession - whether to force the method to ignore the session state and use just the "sr" flag
    * @returns {void}
    */
   async initializeRecording (srMode, ignoreSession) {

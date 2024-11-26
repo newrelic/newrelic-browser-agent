e127f026fd13c18fc320ebd4da9f9c4aa0f7fd77
Chunwai Li <cli@newrelic.com>
2024-11-26 16:07:21 -0600

    Finished remaining spa e2e tests

--- src/features/soft_navigations/constants.js
+++ src/features/soft_navigations/constants.js
@@ -3,7 +3,8 @@ import { FEATURE_NAMES } from '../../loaders/features/features'
 export const INTERACTION_TRIGGERS = [
   'click', // e.g. user clicks link or the page back/forward buttons
   'keydown', // e.g. user presses left and right arrow key to switch between displayed photo gallery
-  'submit' // e.g. user clicks submit butotn or presses enter while editing a form field
+  'submit', // e.g. user clicks submit butotn or presses enter while editing a form field
+  'popstate' // history api is used to navigate back and forward
 ]
 export const API_TRIGGER_NAME = 'api'
 export const IPL_TRIGGER_NAME = 'initialPageLoad'

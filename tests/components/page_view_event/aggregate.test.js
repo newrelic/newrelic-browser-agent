4abf1009f090b84101cfe0294c2b45d504dc3ee0
Chunwai Li <cli@newrelic.com>
2025-01-09 17:16:26 -0600

    Add PVE comp jest test

---
+++ tests/components/page_view_event/aggregate.test.js
@@ -0,0 +1,32 @@
+import { setupAgent } from '../setup-agent'
+import { Instrument as PageViewEvent } from '../../../src/features/page_view_event/instrument'
+
+let mainAgent, pveAggregate
+
+beforeAll(async () => {
+  mainAgent = setupAgent()
+  mainAgent.info.errorBeacon = undefined // this prevents Harvester from actually running its `send` method
+})
+beforeEach(async () => {
+  const pveInstrument = new PageViewEvent(mainAgent)
+  await new Promise(process.nextTick)
+  pveAggregate = pveInstrument.featAggregate
+})
+
+test('PageViewEvent does not throw on Harvester driven processes', () => {
+  expect(pveAggregate.blocked).toEqual(false)
+  expect(() => pveAggregate.makeHarvestPayload(true)).not.toThrow()
+
+  // The following both don't send anything since PVE buffer is meant to stay empty, so they return false.
+  expect(mainAgent.runtime.harvester.triggerHarvestFor(pveAggregate)).toEqual(false) // mimics what the harvester does on interval
+  expect(mainAgent.runtime.harvester.triggerHarvestFor(pveAggregate, { isFinalHarvest: true })).toEqual(false) // mimics what the harvester does on EoL
+
+  expect(mainAgent.runtime.harvester.triggerHarvestFor(pveAggregate, {
+    directSend: {
+      targetApp: 'someApp',
+      payload: 'blah'
+    },
+    needResponse: true,
+    sendEmptyBody: true
+  })).toEqual(true) // mimics the manual trigger in PVE `sendRum`; this should return true as it actually tries to "send"
+})

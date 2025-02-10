9b2756f520cb946192e72d138e14fb4fca75a7b8
Grady Ellison <ellisong1990@gmail.com>
2025-02-10 11:19:24 -0800

    feat: Re-implement wrap-websocket into agent (#1342)

--- src/common/wrap/wrap-websocket.js
+++ src/common/wrap/wrap-websocket.js
@@ -27,51 +27,35 @@ export function wrapWebSocket (sharedEE) {
     }
   }
 
-  Object.defineProperty(WrappedWebSocket, 'name', {
-    value: 'WebSocket'
-  })
+  class WrappedWebSocket extends WebSocket {
+    static name = 'WebSocket'
 
-  function WrappedWebSocket () {
-    const ws = new originals.WS(...arguments)
-    const socketId = generateRandomHexString(6)
-    const report = reporter(socketId)
-    report('new')
+    constructor (...args) {
+      super(...args)
+      const socketId = generateRandomHexString(6)
+      this.report = reporter(socketId)
+      this.report('new')
 
-    const events = ['message', 'error', 'open', 'close']
-    /** add event listeners */
-    events.forEach(evt => {
-      ws.addEventListener(evt, function (e) {
-        report(ADD_EVENT_LISTENER_TAG, { eventType: evt, event: e })
-      })
-    })
-
-    /** could also observe the on-events for runtime processing, but not implemented yet */
-
-    /** observe the static method send, but noteably not close, as that is currently observed with the event listener */
-    ;['send'].forEach(wrapStaticProperty)
-
-    function wrapStaticProperty (prop) {
-      const originalProp = ws[prop]
-      if (originalProp) {
-        Object.defineProperty(proxiedProp, 'name', {
-          value: prop
+      const events = ['message', 'error', 'open', 'close']
+      /** add event listeners */
+      events.forEach(evt => {
+        this.addEventListener(evt, function (e) {
+          this.report(ADD_EVENT_LISTENER_TAG, { eventType: evt, event: e })
         })
-        function proxiedProp () {
-          report(prop, ...arguments)
-          try {
-            return originalProp.apply(this, arguments)
-          } catch (err) {
-            report(prop + '-err', ...arguments)
-            // rethrow error so we don't effect execution by observing.
-            throw err
-          }
-        }
-        ws[prop] = proxiedProp
-      }
+      })
     }
 
-    return ws
+    send (...args) {
+      this.report('send', ...args)
+      try {
+        return super.send(...args)
+      } catch (err) {
+        this.report('send-err', ...args)
+        throw err
+      }
+    }
   }
+
   globalScope.WebSocket = WrappedWebSocket
   return sharedEE
 }

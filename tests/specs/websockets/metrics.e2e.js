9a7c2f24839614e894a4c491194b222c771d272a
Jordan Porter <silentsummitsoftware@gmail.com>
2025-02-12 18:52:47 -0700

    tests must apply to ios and safari matchers

--- tests/specs/websockets/metrics.e2e.js
+++ tests/specs/websockets/metrics.e2e.js
@@ -1,10 +1,10 @@
 const { notIOS, notSafari } = require('../../../tools/browser-matcher/common-matchers.mjs')
 const { testSupportMetricsRequest, testErrorsRequest } = require('../../../tools/testing-server/utils/expect-tests')
 
-describe('WebSocket supportability metrics', () => {
-  /** Safari and iOS safari are blocked from connecting to the websocket protocol on LT, which throws socket errors instead of connecting and capturing the expected payloads.
-   *  validated that this works locally for these envs */
-  it.withBrowsersMatching([notSafari, notIOS])('should capture expected SMs', async () => {
+/** NOTE: Safari and iOS safari are blocked from connecting to the websocket protocol (on LT!), which throws socket errors instead of connecting and capturing the expected payloads.
+   *  validated that this works locally for these envs.  Any websocket changes must be validated manually for these envs */
+describe.withBrowsersMatching([notSafari, notIOS])('WebSocket supportability metrics', () => {
+  it('should capture expected SMs', async () => {
     const supportabilityMetricsRequest = await browser.testHandle.createNetworkCaptures('bamServer', { test: testSupportMetricsRequest })
     const url = await browser.testHandle.assetURL('websockets.html')
 

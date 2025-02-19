450c47aaac05df4697b87951501783524ec7d4b5
Chunwai Li <cli@newrelic.com>
2025-02-19 11:06:30 -0600

    Removing websocket wrapping & SM

--- src/features/metrics/instrument/index.js
+++ src/features/metrics/instrument/index.js
@@ -3,22 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { handle } from '../../../common/event-emitter/handle'
-import { WEBSOCKET_TAG, wrapWebSocket } from '../../../common/wrap/wrap-websocket'
+// import { handle } from '../../../common/event-emitter/handle'
+// import { WEBSOCKET_TAG, wrapWebSocket } from '../../../common/wrap/wrap-websocket'
 import { InstrumentBase } from '../../utils/instrument-base'
-import { FEATURE_NAME, WATCHABLE_WEB_SOCKET_EVENTS } from '../constants'
+import { FEATURE_NAME } from '../constants'
 
 export class Instrument extends InstrumentBase {
   static featureName = FEATURE_NAME
   constructor (agentRef, auto = true) {
     super(agentRef, FEATURE_NAME, auto)
-    wrapWebSocket(this.ee)
+    // wrapWebSocket(this.ee) - feb'25 : removing wrapping again to avoid integration issues
 
-    WATCHABLE_WEB_SOCKET_EVENTS.forEach((suffix) => {
-      this.ee.on(WEBSOCKET_TAG + suffix, (...args) => {
-        handle('buffered-' + WEBSOCKET_TAG + suffix, [...args], undefined, this.featureName, this.ee)
-      })
-    })
+    // WATCHABLE_WEB_SOCKET_EVENTS.forEach((suffix) => {
+    //   this.ee.on(WEBSOCKET_TAG + suffix, (...args) => {
+    //     handle('buffered-' + WEBSOCKET_TAG + suffix, [...args], undefined, this.featureName, this.ee)
+    //   })
+    // })
 
     this.importAggregator(agentRef)
   }

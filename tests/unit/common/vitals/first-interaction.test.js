c146a4e2af634e21757a5cc55917e11d1e68ee33
Pik Tang <ptang@newrelic.com>
2025-02-24 08:14:10 -0800

    chore: Add unit test for first interaction

---
+++ tests/unit/common/vitals/first-interaction.test.js
@@ -0,0 +1,106 @@
+beforeEach(() => {
+  jest.resetModules()
+  jest.resetAllMocks()
+  jest.clearAllMocks()
+})
+
+const inpAttribution = {
+  interactionType: 'pointer',
+  interactionTarget: 'button',
+  interactionTime: 8853.8,
+  inputDelay: 0,
+  nextPaintTime: 200,
+  processingDuration: 0,
+  presentationDelay: 0,
+  loadState: 'complete'
+}
+const getFreshImport = async (codeToRun) => {
+  jest.doMock('web-vitals/attribution', () => ({
+    onINP: jest.fn(cb => cb({ value: 8, attribution: inpAttribution, id: 'ruhroh' }))
+  }))
+  await import('../../../../src/common/vitals/interaction-to-next-paint')
+  const { firstInteraction } = await import('../../../../src/common/vitals/first-interaction')
+  codeToRun(firstInteraction)
+}
+
+describe('fi (first interaction)', () => {
+  test('reports fi', (done) => {
+    getFreshImport((metric) => {
+      metric.subscribe(({ value, attrs }) => {
+        console.log('Value: ', value)
+        expect(value).toEqual(8853)
+        done()
+      })
+    })
+  })
+
+  test('Does NOT report values if initiallyHidden', (done) => {
+    jest.doMock('../../../../src/common/constants/runtime', () => ({
+      __esModule: true,
+      initiallyHidden: true,
+      isBrowserScope: true
+    }))
+
+    getFreshImport(metric => {
+      metric.subscribe(() => {
+        console.log('should not have reported')
+        expect(1).toEqual(2)
+      })
+      setTimeout(done, 1000)
+    })
+  })
+
+  test('does NOT report if not browser scoped', (done) => {
+    jest.doMock('../../../../src/common/constants/runtime', () => ({
+      __esModule: true,
+      initiallyHidden: false,
+      isBrowserScope: false
+    }))
+
+    getFreshImport(metric => {
+      metric.subscribe(({ value, attrs }) => {
+        console.log('should not have reported...')
+        expect(1).toEqual(2)
+      })
+      setTimeout(done, 1000)
+    })
+  })
+
+  test('multiple subs get same value', done => {
+    jest.doMock('../../../../src/common/constants/runtime', () => ({
+      __esModule: true,
+      initiallyHidden: false,
+      isBrowserScope: true
+    }))
+    let witness = 0
+    getFreshImport(metric => {
+      metric.subscribe(({ value }) => {
+        expect(value).toEqual(8853)
+        witness++
+      })
+      metric.subscribe(({ value }) => {
+        expect(value).toEqual(8853)
+        witness++
+        if (witness === 2) done()
+      })
+    })
+  })
+
+  test('reports only once', (done) => {
+    jest.doMock('../../../../src/common/constants/runtime', () => ({
+      __esModule: true,
+      initiallyHidden: false,
+      isBrowserScope: true
+    }))
+    let triggered = 0
+    getFreshImport(metric => metric.subscribe(({ value }) => {
+      triggered++
+      expect(value).toEqual(8853)
+      expect(triggered).toEqual(1)
+      setTimeout(() => {
+        expect(triggered).toEqual(1)
+        done()
+      }, 1000)
+    }))
+  })
+})

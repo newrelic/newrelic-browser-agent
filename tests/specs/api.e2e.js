d5d773b1ad63337825ab7448fff2127e1c42c7cf
Pik Tang <ptang@newrelic.com>
2025-04-01 12:56:32 -0700

    fix: Make stronger assertions

--- tests/specs/api.e2e.js
+++ tests/specs/api.e2e.js
@@ -51,20 +51,69 @@ describe('newrelic api', () => {
     expect(result).toEqual(true)
   })
 
-  it('should work as expected within `newrelic` event listeners', async () => {
-    const logsCapture = await browser.testHandle.createNetworkCaptures('bamServer', { test: testLogsRequest })
+  it('should fire `newrelic` event after RUM call', async () => {
+    const rumCapture = await browser.testHandle.createNetworkCaptures('bamServer', { test: testRumRequest })
+
     await browser.testHandle.scheduleReply('bamServer', {
       test: testRumRequest,
       body: JSON.stringify(rumFlags({ log: LOGGING_MODE.INFO }))
     })
 
-    const [logsHarvests] = await Promise.all([
-      logsCapture.waitForResult({ timeout: 10000 }),
+    const [rumHarvests] =
+      await Promise.all([
+        rumCapture.waitForResult({ timeout: 10000 }),
+        browser.url(await browser.testHandle.assetURL('event-listener-newrelic.html'))
+          .then(() => browser.waitForAgentLoad())
+          .then(() => browser.waitUntil(
+            () => browser.execute(function () {
+              return window?.newrelicEventTime
+            }),
+            {
+              timeout: 10000,
+              timeoutMsg: 'Timeout while waiting on `newrelic` event'
+            }
+          ))
+      ])
+
+    expect(rumHarvests.length).toEqual(1)
+    const rumResult = JSON.parse(rumHarvests[0].reply.body)
+
+    const newrelicEventTime = await browser.execute(function () {
+      return window.newrelicEventTime
+    })
+
+    expect(newrelicEventTime > rumResult.app.nrServerTime).toBe(true)
+  })
+
+  it('should work as expected within `newrelic` event listeners', async () => {
+    const [logsCapture, errorsCapture] = await browser.testHandle.createNetworkCaptures('bamServer', [
+      { test: testLogsRequest },
+      { test: testErrorsRequest }
+    ])
+    await browser.testHandle.scheduleReply('bamServer', {
+      test: testRumRequest,
+      body: JSON.stringify(rumFlags({ log: LOGGING_MODE.INFO }))
+    })
+
+    const [logsHarvests, errorsHarvests] =
+    await Promise.all([
+      logsCapture.waitForResult({ totalCount: 1, timeout: 10000 }),
+      errorsCapture.waitForResult({ totalCount: 1, timeout: 10000 }),
       browser.url(await browser.testHandle.assetURL('event-listener-newrelic.html'))
         .then(() => browser.waitForAgentLoad())
     ])
 
+    expect(errorsHarvests.length).toEqual(1)
+    expect(errorsHarvests[0].request.query.ct).toEqual('http://custom.transaction/some-page')
+    const actualErrors = errorsHarvests[0].request.body.err
+    expect(actualErrors.length).toEqual(1)
+    expect(actualErrors[0].params.message).toEqual('error 1')
+
     expect(logsHarvests.length).toEqual(1)
+    const logsPayload = JSON.parse(logsHarvests[0].request.body)
+    expect(logsPayload[0].common.attributes['application.version']).toEqual('1.0.0')
+    expect(logsPayload[0].common.attributes.foo).toEqual('bar')
+    expect(logsPayload[0].logs[0].message).toEqual('test message')
   })
 
   describe('setPageViewName()', () => {

4fde667e114c5eb29f7bedddf57df2dd8dc00254
Jordan Porter <silentsummitsoftware@gmail.com>
2025-04-16 08:02:10 -0600

    bundler tools idea

---
+++ tools/bundler-tools/bundler-tools.js
@@ -0,0 +1,27 @@
+const webpackCacheGroup = (asyncChunkName = 'new-relic-browser-agent', matcher) => {
+  if (!matcher) {
+    matcher = (module, { chunkGraph }) => {
+      if (!module.resource) return false
+      if (!module.resource.match(/node_modules[\\/](@newrelic[\\/]browser-agent|web-vitals)[\\/]/)) return false
+      return chunkGraph.getModuleChunks(module).filter(chunk => {
+        return !['recorder', 'compressor'].includes(chunk.name)
+      }).length > 0
+    }
+  }
+
+  return {
+    [asyncChunkName]: {
+      name: asyncChunkName,
+      enforce: true,
+      reuseExistingChunk: true,
+      test: matcher,
+      chunks: 'async'
+    }
+  }
+}
+
+module.exports = {
+  webpackCacheGroup
+}
+
+// roll-up, vite, esbuild etc.

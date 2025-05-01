8e7498e47a4666996f4837a53b02e8c313a48380
Jordan Porter <insomniacrampage@hotmail.com>
2025-05-01 15:58:29 -0600

    feat: API Code Splitting (#1433)
    Co-authored-by: Chunwai Li <cli@newrelic.com>

---
+++ src/loaders/api/wrapLogger.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2020-2025 New Relic, Inc. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { wrapLogger } from '../../common/wrap/wrap-logger'
+import { LOG_LEVELS } from '../../features/logging/constants'
+import { WRAP_LOGGER } from './constants'
+import { setupAPI } from './sharedHandlers'
+
+export function setupWrapLoggerAPI (agent) {
+  setupAPI(WRAP_LOGGER, (parent, functionName, { customAttributes = {}, level = LOG_LEVELS.INFO } = {}) => {
+    wrapLogger(agent.ee, parent, functionName, { customAttributes, level })
+  }, agent)
+}

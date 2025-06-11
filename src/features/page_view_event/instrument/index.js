def89c4ab5850a3d8f8dc03b424a473bd5eb4bb5
Grady Ellison <ellisong1990@gmail.com>
2025-06-11 12:56:33 -0700

    feat: Add more inspection events (#1494)

--- src/features/page_view_event/instrument/index.js
+++ src/features/page_view_event/instrument/index.js
@@ -6,11 +6,17 @@ import { handle } from '../../../common/event-emitter/handle'
 import { setupSetPageViewNameAPI } from '../../../loaders/api/setPageViewName'
 import { InstrumentBase } from '../../utils/instrument-base'
 import * as CONSTANTS from '../constants'
+import { SESSION_EVENTS } from '../../../common/session/constants'
+import { dispatchGlobalEvent } from '../../../common/dispatch/global-event'
+import { onDOMContentLoaded, onPopstateChange, onWindowLoad } from '../../../common/window/load'
 
 export class Instrument extends InstrumentBase {
   static featureName = CONSTANTS.FEATURE_NAME
   constructor (agentRef) {
     super(agentRef, CONSTANTS.FEATURE_NAME)
+
+    /** setup inspection events for window lifecycle */
+    this.setupInspectionEvents(agentRef.agentIdentifier)
 
     /** feature specific APIs */
     setupSetPageViewNameAPI(agentRef)
@@ -19,6 +25,41 @@ export class Instrument extends InstrumentBase {
     this.ee.on('api-send-rum', (attrs, target) => handle('send-rum', [attrs, target], undefined, this.featureName, this.ee))
 
     this.importAggregator(agentRef, () => import(/* webpackChunkName: "page_view_event-aggregate" */ '../aggregate'))
+  }
+
+  setupInspectionEvents (agentIdentifier) {
+    const dispatch = (evt, name) => {
+      if (!evt) return
+      dispatchGlobalEvent({
+        agentIdentifier,
+        timeStamp: evt.timeStamp,
+        loaded: evt.target.readyState === 'complete',
+        type: 'window',
+        name,
+        data: evt.target.location + ''
+      })
+    }
+
+    onDOMContentLoaded((evt) => {
+      dispatch(evt, 'DOMContentLoaded')
+    })
+
+    onWindowLoad((evt) => {
+      dispatch(evt, 'load')
+    })
+
+    onPopstateChange((evt) => {
+      dispatch(evt, 'navigate')
+    })
+
+    this.ee.on(SESSION_EVENTS.UPDATE, (_, data) => {
+      dispatchGlobalEvent({
+        agentIdentifier,
+        type: 'lifecycle',
+        name: 'session',
+        data
+      })
+    })
   }
 }
 

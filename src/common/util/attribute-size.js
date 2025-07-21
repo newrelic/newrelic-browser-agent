4411dafbf2d1abf2559b2656d04ad3208f02f148
Jordan Porter <silentsummitsoftware@gmail.com>
2025-07-21 12:45:34 -0600

    remove pointless warning

--- src/common/util/attribute-size.js
+++ src/common/util/attribute-size.js
@@ -2,11 +2,9 @@
  * Copyright 2020-2025 New Relic, Inc. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import { warn } from './console'
 import { stringify } from './stringify'
 
 export function trackObjectAttributeSize (parent, object) {
-  if (!parent) warn(64)
   const originalAttribute = parent[object] ??= {}
   const output = { bytes: Object.keys(originalAttribute).reduce((acc, key) => acc + key.length + stringify(originalAttribute[key]).length, 0) }
   // proxy attribute to calculate its size when changed

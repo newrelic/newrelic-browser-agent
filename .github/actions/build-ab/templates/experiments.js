4a399c3cb133dd61817c17b964b7913c57095d81
Jordan Porter <silentsummitsoftware@gmail.com>
2025-07-29 11:38:53 -0600

    allow traces for experiment

--- .github/actions/build-ab/templates/experiments.js
+++ .github/actions/build-ab/templates/experiments.js
@@ -5,7 +5,7 @@ window.NREUM.info.applicationID = '{{{args.abAppId}}}'
 window.NREUM.info.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.init.proxy = {} // Proxy won't work for experiments
 window.NREUM.init.session_replay.enabled = false // disabled for now to not double wrap the page which can cause extra processing burden
-window.NREUM.init.session_trace.enabled = false // disabled for now to not double wrap the page which can cause extra processing burden
+window.NREUM.init.session_trace.enabled = true // disabled for now to not double wrap the page which can cause extra processing burden
 window.NREUM.init.feature_flags = ['ajax_metrics_deny_list','soft_nav']
 window.NREUM.init.user_actions = {elementAttributes: ['id', 'className', 'tagName', 'type', 'ariaLabel', 'alt', 'title']}
 

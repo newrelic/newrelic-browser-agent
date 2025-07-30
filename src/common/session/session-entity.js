a113bb0838e0bcee116158f3296c02a1fbcfcb12
Grady Ellison <gellison@newrelic.com>
2025-07-30 13:52:59 -0700

    fix: Persist consent value across session resets

--- src/common/session/session-entity.js
+++ src/common/session/session-entity.js
@@ -33,7 +33,7 @@ const model = {
   serverTimeDiff: null, // set by TimeKeeper; "undefined" value will not be stringified and stored but "null" will
   custom: {},
   numOfResets: 0,
-  consent: false
+  consent: false // set by consent() API call
 }
 
 export class SessionEntity {
@@ -78,7 +78,10 @@ export class SessionEntity {
 
   setup ({ value = generateRandomHexString(16), expiresMs = DEFAULT_EXPIRES_MS, inactiveMs = DEFAULT_INACTIVE_MS, numOfResets = 0 }) {
     /** Ensure that certain properties are preserved across a reset if already set */
-    const persistentAttributes = { serverTimeDiff: this.state.serverTimeDiff || model.serverTimeDiff }
+    const persistentAttributes = {
+      serverTimeDiff: this.state.serverTimeDiff || model.serverTimeDiff,
+      consent: this.state.consent || model.consent
+    }
     this.state = {}
     this.sync({ ...model, ...persistentAttributes })
 

aca15df0a0a67d61973d08244123f4bd4b80ea4d
Pik Tang <ptang@newrelic.com>
2025-09-22 10:15:44 -0700

    chore: Add 'user_frustrations' ff

--- .github/actions/build-ab/templates/experiments.js
+++ .github/actions/build-ab/templates/experiments.js
@@ -6,7 +6,7 @@ window.NREUM.info.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.init.proxy = {} // Proxy won't work for experiments
 window.NREUM.init.session_replay.enabled = true // feature is enabled, but the app settings will have sampling at 0.  We can proactively enable SR for certain test cases through app settings
 window.NREUM.init.session_trace.enabled = true // feature is enabled, but the app settings will have sampling at 0.  We can proactively enable SR for certain test cases through app settings
-window.NREUM.init.feature_flags = ['ajax_metrics_deny_list','soft_nav']
+window.NREUM.init.feature_flags = ['ajax_metrics_deny_list','soft_nav', 'user_frustrations']
 window.NREUM.init.user_actions = {elementAttributes: ['id', 'className', 'tagName', 'type', 'ariaLabel', 'alt', 'title']}
 
 {{#if experimentScripts}}

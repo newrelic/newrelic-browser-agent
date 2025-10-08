04e5b5ab84fbd6347c517a98d5fdb6205c8615a2
Jordan Porter <silentsummitsoftware@gmail.com>
2025-10-08 09:11:24 -0600

    fix comment

--- src/common/config/runtime.js
+++ src/common/config/runtime.js
@@ -29,7 +29,7 @@ const RuntimeModel = {
   harvester: undefined,
   isolatedBacklog: false,
   isRecording: false, // true when actively recording, false when paused or stopped
-  earliestViableSR: 0, // relative timestamp of the earliest viable session replay data buffered
+  earliestViableSR: 0, // absolute timestamp of the earliest viable session replay data buffered on the current page. updates as the SR buffer evolves
   srHarvestedAt: undefined, // absolute timestamp of the FIRST SR harvest success time
   loaderType: undefined,
   maxBytes: 30000,

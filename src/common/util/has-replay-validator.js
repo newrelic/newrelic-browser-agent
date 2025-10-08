3b2cafe95c192bb05a070b373c4dd9db32946f38
Jordan Porter <silentsummitsoftware@gmail.com>
2025-10-08 09:14:12 -0600

    fix comment

--- src/common/util/has-replay-validator.js
+++ src/common/util/has-replay-validator.js
@@ -33,25 +33,25 @@ export function hasReplayValidator (agentRef, timestamp, harvestOpts = {}) {
   if (eventIsOutsideReplayWindow || srIsNotRecording) {
     // is event in the valid SR buffer window? (for handling error mode cases)
     // if it is, is SR recording right now? (if it isnt, we prefer to take the less risky option of assigning false negatives over false positives)
-    // If neither valid nor recording, this event cant reasonably be related to a valid harvest, so just delete hasReplay and harvest it now.
+    // If neither valid nor recording, this event cant reasonably be related to a valid harvest, so do not add hasReplay and harvest it now.
     return { shouldAdd: false, shouldHold: false }
   } else {
     if (srIsRecordingButHasNeverHarvested) {
       // timestamp is in valid window and a replay is actively recording
       if (harvestOpts.isFinalHarvest) {
-      // If this is the final harvest and we still don't have a successful SR harvest, we should just delete the hasReplay flag and harvest it to
-      // not drop data and not assign false positives.
+      // If this is the final harvest and we still don't have a successful SR harvest, we should not add the hasReplay flag and harvest it now
+      // to not drop data and not assign false positives.
         return { shouldAdd: false, shouldHold: false }
       } else {
         // We now suspect a replay will harvest eventually. Because the attribution seems valid, and we are waiting on a valid harvest
         // we should put the event back in the aggregator for later harvesting, to be checked again at the next harvest point.
         // if error mode doesnt harvest and "resets" its look-back buffer, the attribute will be dropped.  If full mode doesnt harvest it will
-        // time-out and shut down.  If either harvests successfully, we will re-evaluate the event next time, keep the hasReplay attribute and harvest.
+        // time-out and shut down.  If either harvests successfully, when we re-evaluate the event the next cycle, it will keep the hasReplay attribute and harvest.
         return { shouldAdd: false, shouldHold: true }
       }
     }
   }
   // If we reach this point, the event is in a valid window and we can assume the session replay was active,
-  // so it can have its hasReplay flag added and be harvested as is.
+  // so it can have its hasReplay flag added and be harvested.
   return { shouldAdd: true, shouldHold: false }
 }

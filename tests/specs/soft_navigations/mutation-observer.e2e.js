613a34d86fd38f5e87182ad8c0d4dcb8a627e49e
Pik Tang <ptang@newrelic.com>
2025-11-24 13:35:28 -0800

    chore: Make test loader type explicit
    It actually runs as 'spa' loader type, but it's not obvious.

--- tests/specs/soft_navigations/mutation-observer.e2e.js
+++ tests/specs/soft_navigations/mutation-observer.e2e.js
@@ -8,8 +8,12 @@ describe('MutationObserver', () => {
   })
 
   it('should not throw an error when a UI event is triggered before the body exists', async () => {
-    await browser.url(await browser.testHandle.assetURL('mutation-observer-test.html'))
+    const loaderType = await browser.url(await browser.testHandle.assetURL('mutation-observer-test.html', { loader: 'spa' }))
       .then(() => browser.waitForAgentLoad())
+      .then(() => browser.execute(function () {
+        return Object.values(NREUM.initializedAgents)[0].runtime.loaderType
+      }))
+    expect(loaderType).toBe('spa')
     const errorResult = await errorsCapture.waitForResult({ timeout: 10000 })
     expect(errorResult.length).toBe(0)
   })

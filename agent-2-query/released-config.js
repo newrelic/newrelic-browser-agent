09e85db6f00f8998660e0ae72349d785ee884869
Jordan Porter <silentsummitsoftware@gmail.com>
2025-12-01 11:38:02 -0700

    set up test files uploads

---
+++ agent-2-query/released-config.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+// eslint-disable-file
+window.localStorage.clear()
+window.NREUM || (NREUM = {})
+NREUM.init = {
+  session_replay: {
+    enabled: true,
+    block_selector: '',
+    mask_text_selector: '*',
+    sampling_rate: 100.0,
+    error_sampling_rate: 100.0,
+    mask_all_inputs: true,
+    collect_fonts: true,
+    inline_images: false,
+    inline_stylesheet: true,
+    fix_stylesheets: true,
+    preload: false,
+    mask_input_options: {}
+  },
+  distributed_tracing: {
+    enabled: true
+  },
+  performance: {
+    capture_marks: true, capture_measures: true
+  },
+  browser_consent_mode: {
+    enabled: false
+  },
+  privacy: {
+    cookies_enabled: true
+  },
+  ajax: {
+    deny_list: ['staging-bam-cell.nr-data.net']
+  }
+}
+NREUM.feature_flags = ['soft_nav', 'websockets']
+NREUM.info = { beacon: 'staging-bam-cell.nr-data.net', errorBeacon: 'staging-bam-cell.nr-data.net', licenseKey: 'NRJS-1d0fe8ebf651d9c0af2', applicationID: '275822177', sa: 1 }

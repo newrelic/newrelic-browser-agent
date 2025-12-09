da2ef2f50af05085dc5806d422d411deec2d2ba5
Jordan Porter <insomniacrampage@hotmail.com>
2025-12-09 13:24:36 -0700

    feat: Improve agent compatibility with ChatGPT connector apps (#1640)
    Co-authored-by: ptang-nr <ptang@newrelic.com>

--- src/common/constants/runtime.js
+++ src/common/constants/runtime.js
@@ -83,3 +83,5 @@ export const ffVersion = (() => {
  * @type {number}
  */
 export const originTime = Date.now() - now()
+
+export const supportsNavTimingL2 = () => typeof PerformanceNavigationTiming !== 'undefined' && globalScope?.performance?.getEntriesByType('navigation')?.length > 0

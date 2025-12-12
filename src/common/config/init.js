503f10761ab1b90cc52b583736b894e087fd5598
Jordan Porter <silentsummitsoftware@gmail.com>
2025-12-12 09:49:30 -0700

    use constant

--- src/common/config/init.js
+++ src/common/config/init.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { FEATURE_FLAGS } from '../../features/generic_events/constants'
+import { CAPTURE_PAYLOAD_SETTINGS } from '../../features/ajax/constants'
 import { isValidSelector } from '../dom/query-selector'
 import { DEFAULT_EXPIRES_MS, DEFAULT_INACTIVE_MS } from '../session/constants'
 import { warn } from '../util/console'
@@ -47,7 +48,7 @@ const InitModelFn = () => {
     }
   }
   return {
-    ajax: { deny_list: undefined, block_internal: true, enabled: true, autoStart: true, capture_payloads: 'off' },
+    ajax: { deny_list: undefined, block_internal: true, enabled: true, autoStart: true, capture_payloads: CAPTURE_PAYLOAD_SETTINGS.OFF },
     api: {
       get allow_registered_children () { return hiddenState.feature_flags.includes(FEATURE_FLAGS.REGISTER) || hiddenState.experimental.allow_registered_children },
       set allow_registered_children (val) { hiddenState.experimental.allow_registered_children = val },

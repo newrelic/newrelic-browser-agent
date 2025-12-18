7d2b1a6fb06b0c2817cce7ee88243c8effc81dd1
Pik Tang <ptang@newrelic.com>
2025-12-18 10:08:56 -0800

    fix: Prevent dupe MicroAgent payloads
    Due to handlers being registered once per micro-agent drain.

--- tests/specs/npm/micro-agent.e2e.js
+++ tests/specs/npm/micro-agent.e2e.js
@@ -15,19 +15,9 @@ describe('micro-agent', () => {
       { test: testLogsRequest }
     ])
     await browser.url(await browser.testHandle.assetURL('test-builds/browser-agent-wrapper/micro-agent.html'))
+      .then(() => browser.waitForAgentLoad())
 
     await browser.execute(function () {
-      var opts = {
-        info: NREUM.info,
-        init: NREUM.init
-      }
-      window.agent1 = new MicroAgent({ ...opts, info: { ...opts.info, applicationID: 1 } })
-      window.agent2 = new MicroAgent({ ...opts, info: { ...opts.info, applicationID: 2 } })
-
-      // each payload in this test is decorated with data that matches its appId for ease of testing
-      window.agent1.setCustomAttribute('customAttr', '1')
-      window.agent2.setCustomAttribute('customAttr', '2')
-
       // each payload in this test is decorated with data that matches its appId for ease of testing
       window.agent1.noticeError('1')
       window.agent2.noticeError('2')
@@ -41,10 +31,10 @@ describe('micro-agent', () => {
       window.agent2.log('2')
     })
     const [rumHarvests, errorsHarvests, insightsHarvests, logsHarvest] = await Promise.all([
-      rumCapture.waitForResult({ totalCount: 2 }),
-      errorsCapture.waitForResult({ totalCount: 2 }),
-      insightsCapture.waitForResult({ totalCount: 2 }),
-      logsCapture.waitForResult({ totalCount: 2, timeout: 15000 })
+      rumCapture.waitForResult({ totalCount: 2, timeout: 10000 }),
+      errorsCapture.waitForResult({ totalCount: 2, timeout: 10000 }),
+      insightsCapture.waitForResult({ totalCount: 2, timeout: 10000 }),
+      logsCapture.waitForResult({ totalCount: 2, timeout: 10000 })
     ])
 
     // these props will get set to true once a test has matched it
@@ -64,11 +54,13 @@ describe('micro-agent', () => {
     })
 
     errorsHarvests.forEach(({ request: { query, body } }) => {
+      expect(body.err.length).toEqual(1) // ensure only one error per appId
       expect(ranOnce(query.a, 'err')).toEqual(true)
       expect(payloadMatchesAppId(query.a, body.err[0].params.message)).toEqual(true)
     })
 
     insightsHarvests.forEach(({ request: { query, body } }) => {
+      expect(body.ins.length).toEqual(1) // ensure only one page action per appId
       expect(ranOnce(query.a, 'pa')).toEqual(true)
       const data = body.ins[0]
       expect(payloadMatchesAppId(query.a, data.val, data.actionName, data.customAttr)).toEqual(true)
@@ -76,6 +68,7 @@ describe('micro-agent', () => {
 
     logsHarvest.forEach(({ request: { query, body } }) => {
       const data = JSON.parse(body)[0]
+      expect(data.logs.length).toEqual(1) // ensure only one log per appId
       expect(ranOnce(data.common.attributes.appId, 'log')).toEqual(true)
       expect(payloadMatchesAppId(data.common.attributes.appId, data.logs[0].message)).toEqual(true)
     })
@@ -101,12 +94,27 @@ describe('micro-agent', () => {
       { test: testLogsRequest }
     ])
     await browser.url(await browser.testHandle.assetURL('test-builds/rollup-micro-agent/index.html'))
+      .then(() => browser.waitForAgentLoad())
+
+    await browser.execute(function () {
+      // each payload in this test is decorated with data that matches its appId for ease of testing
+      window.agent1.noticeError('1')
+      window.agent2.noticeError('2')
+
+      // each payload in this test is decorated with data that matches its appId for ease of testing
+      window.agent1.addPageAction('1', { val: 1 })
+      window.agent2.addPageAction('2', { val: 2 })
+
+      // each payload in this test is decorated with data that matches its appId for ease of testing
+      window.agent1.log('1')
+      window.agent2.log('2')
+    })
 
     const [rumHarvests, errorsHarvests, insightsHarvests, logsHarvest] = await Promise.all([
-      rumCapture.waitForResult({ totalCount: 2 }),
-      errorsCapture.waitForResult({ totalCount: 2 }),
-      insightsCapture.waitForResult({ totalCount: 2 }),
-      logsCapture.waitForResult({ totalCount: 2, timeout: 15000 })
+      rumCapture.waitForResult({ totalCount: 2, timeout: 10000 }),
+      errorsCapture.waitForResult({ totalCount: 2, timeout: 10000 }),
+      insightsCapture.waitForResult({ totalCount: 2, timeout: 10000 }),
+      logsCapture.waitForResult({ totalCount: 2, timeout: 10000 })
     ])
 
     // these props will get set to true once a test has matched it
@@ -126,11 +134,13 @@ describe('micro-agent', () => {
     })
 
     errorsHarvests.forEach(({ request: { query, body } }) => {
+      expect(body.err.length).toEqual(1) // ensure only one error per appId
       expect(ranOnce(query.a, 'err')).toEqual(true)
       expect(payloadMatchesAppId(query.a, body.err[0].params.message)).toEqual(true)
     })
 
     insightsHarvests.forEach(({ request: { query, body } }) => {
+      expect(body.ins.length).toEqual(1) // ensure only one page action per appId
       expect(ranOnce(query.a, 'pa')).toEqual(true)
       const data = body.ins[0]
       expect(payloadMatchesAppId(query.a, data.val, data.actionName, data.customAttr)).toEqual(true)
@@ -138,6 +148,7 @@ describe('micro-agent', () => {
 
     logsHarvest.forEach(({ request: { query, body } }) => {
       const data = JSON.parse(body)[0]
+      expect(data.logs.length).toEqual(1) // ensure only one log per appId
       expect(ranOnce(data.common.attributes.appId, 'log')).toEqual(true)
       expect(payloadMatchesAppId(data.common.attributes.appId, data.logs[0].message)).toEqual(true)
     })

ca36b1c98ebf1b232812e5dc66a3098dcd37316f
Jordan Porter <silentsummitsoftware@gmail.com>
2026-03-17 09:15:26 -0600

    remove unused header constant

--- src/common/constants/agent-constants.js
+++ src/common/constants/agent-constants.js
@@ -8,7 +8,6 @@ export const IDEAL_PAYLOAD_SIZE = 16000
 export const MAX_PAYLOAD_SIZE = 1000000
 export const DEFAULT_KEY = 'NR_CONTAINER_AGENT'
 export const SESSION_ERROR = 'SESSION_ERROR'
-export const NEW_RELIC_MFE_ID_HEADER = 'newrelic-mfe-id'
 
 export const SUPPORTS_REGISTERED_ENTITIES = {
   [FEATURE_NAMES.logging]: true,

a3e43fd494b6b2699f4d0cef19600e635e9c5511
Pik Tang <ptang@newrelic.com>
2026-03-27 12:15:56 -0700

    chore: Cleanup stale lint ignore patterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,8 @@ module.exports = {
     'build/**/*',
     'coverage/**/*',
     '**/node_modules/**/*',
-    'tests/assets/frameworks/**/*',
-    'tests/assets/js/internal/**/*',
-    'tests/assets/js/vendor/**/*',
-    'tests/assets/scripts/**/*',
     'tests/assets/test-builds/**/*',
-    'tests/assets/modular/js-errors/js/vendor/**/*',
-    'tests/dts/**/*',
-
-    // Ignore old JIL test code since they are being migrated to WDIO
-    'tests/browser/**/*',
-    'tests/functional/**/*'
+    'tests/dts/**/*'
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {

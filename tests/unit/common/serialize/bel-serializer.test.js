353dc03c668ff5d3e4ccfd6e25b2108fa272f342
Grady Ellison <gellison@newrelic.com>
2026-07-09 12:19:23 -0700

    feat: Do not obfuscate attribute keys

---
+++ tests/unit/common/serialize/bel-serializer.test.js
@@ -0,0 +1,26 @@
+import { addCustomAttributes, getAddStringContext } from '../../../../src/common/serialize/bel-serializer.js'
+import { Obfuscator } from '../../../../src/common/util/obfuscate.js'
+
+describe('addCustomAttributes in bel-serializer', () => {
+  let addString
+
+  beforeEach(() => {
+    const rules = [{
+      regex: /hello/g,
+      replacement: 'foobar'
+    },
+    {
+      regex: /world/g,
+      replacement: 'foobar'
+    }]
+    const obfuscator = new Obfuscator({ init: { obfuscate: rules } })
+    addString = getAddStringContext(obfuscator, undefined)
+  })
+
+  test('should obfuscate attribute values but not attribute keys', () => {
+    const attrs = { hello: 'world' }
+    const attrParts = addCustomAttributes(attrs, addString)
+
+    expect(attrParts).toEqual([[5, "'hello,'foobar"]])
+  })
+})

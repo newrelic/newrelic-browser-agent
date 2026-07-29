be26bca32bf87c5ae197363e2773734ac22dd60e
Jordan Porter <insomniacrampage@hotmail.com>
2026-07-29 17:35:53 -0600

    Update src/interfaces/registered-iframe-entity.js
    Co-authored-by: Chunwai Li <cli@newrelic.com>

--- src/interfaces/registered-iframe-entity.js
+++ src/interfaces/registered-iframe-entity.js
@@ -416,7 +416,7 @@ export class RegisteredIframeEntity {
   }
 
   /**
-   * Adds a user-defined identifier string to subsequent events on the page for the registered taret.
+   * Adds a user-defined identifier string to subsequent events on the page for the registered target.
    * {@link https://docs.newrelic.com/docs/browser/new-relic-browser/browser-apis/setuserid/}
    * @param {string|null} value A string identifier for the end-user, useful for tying all browser events to specific users. The value parameter does not have to be unique. If IDs should be unique, the caller is responsible for that validation. Passing a null value unsets any existing user ID.
    * @param {boolean} [resetSession=false] Optional param. Should not be used from a registered entity context. To reset a session when updating user id, must be initiated by the main agent.
